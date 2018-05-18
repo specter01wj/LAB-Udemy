78cd90f0fd61e04f5752d69ef596370f4df6e98b
specter01wj <specter01wj@gmail.com>
2018-05-18 01:06:34 -0400

    05/17/2018 v1

---
+++ JAVA/Complete Java Masterclass/sec_4/s4_1/src/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Tim!");
+
+        int myFirstNum = (10 + 5) + (2 * 10);
+        System.out.println(myFirstNum);
+        System.out.println("myFirstNum");
+
+    }
+}
