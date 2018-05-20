6a3d01fcce4e423c2a921164943957a40b1a8047
specter01wj <specter01wj@gmail.com>
2018-05-19 23:55:35 -0400

    05/19/2018 v1

---
+++ JAVA/Complete Java Masterclass/sec_4/s4_2/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int myMinValue = -2147483648;
+        int myMaxValue = 2147483647;
+
+
+
+    }
+}
