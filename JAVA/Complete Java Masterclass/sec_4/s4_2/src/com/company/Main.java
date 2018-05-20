8634c215db561d19240285f0d491da2f1cb12a21
specter01wj <specter01wj@gmail.com>
2018-05-20 00:21:45 -0400

    05/19/2018 v1

--- JAVA/Complete Java Masterclass/sec_4/s4_2/src/com/company/Main.java
+++ JAVA/Complete Java Masterclass/sec_4/s4_2/src/com/company/Main.java
@@ -4,10 +4,24 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        int myMinValue = -2147483648;
+        // int 2exp31
+        int myMinValue = -2_147_483_648;
         int myMaxValue = 2147483647;
+        int myTotal = (myMinValue/2);
+
+        //byte 2exp7
+        byte myByteValue = 127;
+        byte myNewByteValue = (byte)(myByteValue/2);
+
+        //short 2exp15
+        short myShortValue = -32768;
+        short myNewShortValue = (short) (myShortValue/2);
+
+        //long 2exp63
+        long myLongValue = 9_223_372_036_854_775_807L;
 
 
+        System.out.println(myNewByteValue);
 
     }
 }
