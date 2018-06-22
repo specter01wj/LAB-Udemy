52ada53edf8947576df9eb366c009ecbe2e1424f
specter01wj <specter01wj@gmail.com>
2018-06-22 16:14:39 -0400

    06/22/2018 v1

---
+++ JAVA/Complete Java Masterclass/sec_4/s4_4/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        char myChar = '\u00a9';
+        System.out.println("Unicode: " + myChar);
+
+        boolean myBoolean = false;
+        boolean isMale = true;
+
+        char regiSym = '\u00ae';
+        System.out.println("Regi Sym: " + regiSym);
+
+    }
+}
