6af237c49394f46bcd88c16bdf31782373fe993c
specter01wj <specter01wj@gmail.com>
2018-06-25 02:26:07 -0400

    06/24/2018 v1

--- JAVA/Complete Java Masterclass/sec_5/s5_1/src/s5_1.java
+++ JAVA/Complete Java Masterclass/sec_5/s5_1/src/s5_1.java
@@ -5,13 +5,13 @@ public class s5_1 {
     public static void main(String[] args) {
         // a mile is equal to 1.609344 kilometers
         double kilometers = (100 * 1.609344);
+        System.out.println("double kilo: " + kilometers);
         int highScore = 50;
 
         if(highScore == 50) {
             System.out.println("This is an expression");
-
         }
-        
+
         // In the following code that I will type below, write down what parts of the code
         // are expressions.
         int score = 100;
