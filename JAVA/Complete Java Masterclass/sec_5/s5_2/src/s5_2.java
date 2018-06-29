6dcbeae52811564c93e0d67978fad17548b6f629
specter01wj <specter01wj@gmail.com>
2018-06-29 15:30:59 -0400

    06/29/2018 v1

---
+++ JAVA/Complete Java Masterclass/sec_5/s5_2/src/s5_2.java
@@ -0,0 +1,27 @@
+
+
+public class s5_2 {
+
+    public static void main(String[] args) {
+
+        int myVariable = 50;
+
+        if (myVariable == 50) {
+            System.out.println("Printed");
+        }
+
+
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test");
+
+        System.out.println("This is" +
+                " another" +
+                " still more.");
+
+        int anotherVariable = 50;
+        myVariable--;
+        System.out.println("This is another one");
+    }
+
+}
