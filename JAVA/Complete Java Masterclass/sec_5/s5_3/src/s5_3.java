32cb588a3767da24e49c70e7e83c54cb34e74d97
specter01wj <specter01wj@gmail.com>
2018-06-29 16:21:53 -0400

    06/29/2018 v1

---
+++ JAVA/Complete Java Masterclass/sec_5/s5_3/src/s5_3.java
@@ -0,0 +1,30 @@
+
+
+public class s5_3 {
+
+    public static void main(String[] args) {
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        if(gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Your final score was " + finalScore);
+        }
+
+
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        if(gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
+        }
+
+
+    }
+
+}
