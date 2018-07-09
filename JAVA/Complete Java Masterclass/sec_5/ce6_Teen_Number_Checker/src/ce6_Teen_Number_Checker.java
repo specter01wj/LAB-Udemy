16aeb26788ba4b7c7191d98bf9cecb1217e9e470
specter01wj <specter01wj@gmail.com>
2018-07-09 11:31:15 -0400

    07/09/2018 v1

--- JAVA/Complete Java Masterclass/sec_5/ce6_Teen_Number_Checker/src/ce6_Teen_Number_Checker.java
+++ JAVA/Complete Java Masterclass/sec_5/ce6_Teen_Number_Checker/src/ce6_Teen_Number_Checker.java
@@ -5,7 +5,7 @@ public class ce6_Teen_Number_Checker {
     public static void main(String[] args) {
 
         int input1 = 23, input2 = 17, input3 = 29;
-        boolean output = hasEqualSum(input1, input2, input3);
+        boolean output = hasTeen(input1, input2, input3);
         System.out.println("input1: " + input1 +
                 "\ninput2: " + input2 +
                 "\ninput3: " + input3 + "\noutput: " + output);
@@ -18,7 +18,7 @@ public class ce6_Teen_Number_Checker {
      * @param n: An int
      * @return: A boolean
      */
-    public static boolean hasEqualSum(int num1, int num2, int num3) {
+    public static boolean hasTeen(int num1, int num2, int num3) {
         // write your code here
         return (num1 >= 13 && num1 <= 19) || (num2 >= 13 && num2 <= 19) || (num3 >= 13 && num3 <= 19);
     }
