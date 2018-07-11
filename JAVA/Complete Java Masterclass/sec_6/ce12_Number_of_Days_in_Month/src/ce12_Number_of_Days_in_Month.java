d84b5574bc98404b6b605118482abd0176d6ae93
specter01wj <specter01wj@gmail.com>
2018-07-11 01:10:33 -0400

    07/11/2018 v1

--- JAVA/Complete Java Masterclass/sec_6/ce12_Number_of_Days_in_Month/src/ce12_Number_of_Days_in_Month.java
+++ JAVA/Complete Java Masterclass/sec_6/ce12_Number_of_Days_in_Month/src/ce12_Number_of_Days_in_Month.java
@@ -5,57 +5,50 @@ public class ce12_Number_of_Days_in_Month {
     public static void main(String[] args) {
 
         int input = 6;
-        printNumberInWord(input);
-        //System.out.println("input: " + input + "\noutput: " + output);
+        int output = getDaysInMonth(input, 2003);
+        System.out.println("input: " + input + "\noutput: " + output);
 
     }
 
     /**
      * @param n: An integer
-     * @return: void
+     * @return: A boolean
      */
-    public static void main(String[] args) {
-
-        int input = 6;
-        printNumberInWord(input);
-        //System.out.println("input: " + input + "\noutput: " + output);
-
-    }
-
-    /**
-     * @param n: An integer
-     * @return: void
-     */
-    public class NumberOfDaysInMonth {
-        public static boolean isLeapYear(int year) {
-            if (year < 1 || year > 9999) {
-                return false;
-            } else if (year % 100 == 0 && year % 400 != 0) {
-                return false;
-            } else if (year % 4 == 0) {
+    public static boolean isLeapYear(int year) {
+        // write your code here
+        if (year >= 1 && year <= 9999) {
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
                 return true;
             } else {
                 return false;
             }
+        } else {
+            return false;
         }
-        public static int getDaysInMonth(int month, int year) {
-            if (month < 1 || month > 12 || year < 1 || year > 9999) {
+    }
+
+    /**
+     * @param n: An integer
+     * @param n: An integer
+     * @return: An Integer
+     */
+    public static int getDaysInMonth(int month, int year) {
+        if (month < 1 || month > 12 || year < 1 || year > 9999) {
+            return -1;
+        }
+        switch (month) {
+            case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                return 31;
+            case 2:
+                if (isLeapYear(year)) {
+                    return 29;
+                } else {
+                    return 28;
+                }
+            case 4: case 6: case 9: case 11:
+                return 30;
+            default:
                 return -1;
-            }
-            switch (month) {
-                case 1: case 3: case 5: case 7: case 8: case 10: case 12:
-                    return 31;
-                case 2:
-                    if (NumberOfDaysInMonth.isLeapYear(year)) {
-                        return 29;
-                    } else {
-                        return 28;
-                    }
-                case 4: case 6: case 9: case 11:
-                    return 30;
-                default:
-                    return -1;
-            }
         }
     }
 
