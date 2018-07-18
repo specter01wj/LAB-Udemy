7cf48c00053fda572744c8970d0173634b9ce68a
specter01wj <specter01wj@gmail.com>
2018-07-18 11:56:02 -0400

    07/18/2018 v1

--- JAVA/Complete Java Masterclass/sec_7/s7_2/src/s7_2.java
+++ JAVA/Complete Java Masterclass/sec_7/s7_2/src/s7_2.java
@@ -5,7 +5,8 @@ public class s7_2 {
     public static void main(String[] args) {
         Car porsche = new Car();
         Car holden = new Car();
-        porsche.setModel("911");
+        porsche.setModel("commodore");
+        //System.out.println(porsche.model);
         System.out.println("Model is " + porsche.getModel());
     }
 
