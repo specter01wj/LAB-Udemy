a5508624cf0522cdc7eab0b6614d91c53a6a7b41
specter01wj <specter01wj@gmail.com>
2018-07-19 23:58:42 -0400

    07/14/2018 v1

--- JAVA/Complete Java Masterclass/sec_7/s7_4/src/s7_4.java
+++ JAVA/Complete Java Masterclass/sec_7/s7_4/src/s7_4.java
@@ -1,2 +1,22 @@
+
+
 public class s7_4 {
+
+    public static void main(String[] args) {
+  
+        VipPerson person1 = new VipPerson();
+        System.out.println(person1.getName());
+
+        VipPerson person2 = new VipPerson("Bob", 25000.00);
+        System.out.println(person2.getName());
+
+        VipPerson person3 = new VipPerson("Tim", 100.00, "tim@email.com");
+        System.out.println(person3.getName());
+        System.out.println(person3.getEmailAddress());
+
+
+
+
+    }
+
 }
