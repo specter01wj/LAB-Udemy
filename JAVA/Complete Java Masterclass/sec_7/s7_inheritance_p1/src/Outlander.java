a5c9d78d4f754f3ffa7bb8aa492184a21e3c0226
specter01wj <specter01wj@gmail.com>
2018-07-23 02:02:36 -0400

    07/22/2018 v1

---
+++ JAVA/Complete Java Masterclass/sec_7/s7_inheritance_p1/src/Outlander.java
@@ -0,0 +1,12 @@
+
+
+public class Outlander extends Car {
+
+    private int roadServiceMonths;
+
+    public Outlander(int roadServiceMonths) {
+        super("Outlander", "4WD", 5, 5, 6, false);
+        this.roadServiceMonths = roadServiceMonths;
+    }
+
+}
---
+++ JAVA/Complete Java Masterclass/sec_7/s7_inheritance_p1/src/s7_inheritance_p1.java
@@ -0,0 +1,11 @@
+
+
+public class s7_inheritance_p1 {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
