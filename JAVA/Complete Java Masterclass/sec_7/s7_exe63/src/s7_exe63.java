770bdd048a01ef16bb1cf057f793a5ab18c7d8f2
specter01wj <specter01wj@gmail.com>
2018-07-23 00:10:34 -0400

    07/22/2018 v1

--- JAVA/Complete Java Masterclass/sec_7/s7_exe63/src/s7_exe63.java
+++ JAVA/Complete Java Masterclass/sec_7/s7_exe63/src/s7_exe63.java
@@ -5,6 +5,12 @@ public class s7_exe63 {
     public static void main(String[] args) {
 
         Calculator.printSum(5, 10);
+        //Calculator cal = new Calculator();
+        //System.out.println(cal.printSum(5, 15));
+
+        Dog rex = new Dog();
+        rex.bark();
+
         printHello();
 
     }
@@ -18,8 +24,17 @@ public class s7_exe63 {
 
 class Calculator {
 
-    public static void printSum(int a, int b) {
+    public static int printSum(int a, int b) {
         System.out.println("sum = " + (a + b));
+        return a + b;
+    }
+
+}
+
+class Dog {
+
+    public void bark() {
+        System.out.println("woof!");
     }
 
 }
