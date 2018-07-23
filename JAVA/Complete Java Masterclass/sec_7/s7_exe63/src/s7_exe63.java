6e4eff122b05ca843fff2abe04c9dfde71cce276
specter01wj <specter01wj@gmail.com>
2018-07-22 23:58:25 -0400

    07/22/2018 v1

---
+++ JAVA/Complete Java Masterclass/sec_7/s7_exe63/src/s7_exe63.java
@@ -0,0 +1,25 @@
+
+
+public class s7_exe63 {
+
+    public static void main(String[] args) {
+
+        Calculator.printSum(5, 10);
+        printHello();
+
+    }
+
+    public static void printHello() {
+        System.out.println("Hello!");
+    }
+
+}
+
+
+class Calculator {
+
+    public static void printSum(int a, int b) {
+        System.out.println("sum = " + (a + b));
+    }
+
+}
