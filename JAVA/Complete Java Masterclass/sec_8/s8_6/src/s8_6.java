f4cded6aef6208aaae7b6e33b1d163ba128cf08e
specter01wj <specter01wj@gmail.com>
2018-07-29 21:24:59 -0400

    07/29/2018 v1

---
+++ JAVA/Complete Java Masterclass/sec_8/s8_6/src/s8_6.java
@@ -0,0 +1,16 @@
+
+
+public class s8_6 {
+
+    public static void main(String[] args) {
+
+        Hamburger hamburger = new Hamburger("Basic", "Sausage", 3.56, "White");
+        double price = hamburger.itemizeHamburger();
+        hamburger.addHamburgerAddition1("Tomato", 0.27);
+        hamburger.addHamburgerAddition2("Lettuce", 0.75);
+        hamburger.addHamburgerAddition3("Cheese", 1.13);
+        System.out.println("Total Burger price is " + hamburger.itemizeHamburger());
+
+    }
+
+}
