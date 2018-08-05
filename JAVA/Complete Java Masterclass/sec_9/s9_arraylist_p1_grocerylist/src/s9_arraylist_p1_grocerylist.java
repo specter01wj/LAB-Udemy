0aa19304ab8f5e1ee8c6a65466dd86d06227bf10
specter01wj <specter01wj@gmail.com>
2018-08-04 22:41:26 -0400

    08/04/2018 v1

--- JAVA/Complete Java Masterclass/sec_9/s9_arraylist_p1_grocerylist/src/s9_arraylist_p1_grocerylist.java
+++ JAVA/Complete Java Masterclass/sec_9/s9_arraylist_p1_grocerylist/src/s9_arraylist_p1_grocerylist.java
@@ -5,25 +5,42 @@ public class s9_arraylist_p1_grocerylist {
 
     private static ArrayList<String> groceryList = new ArrayList<String>();
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         addGroceryItem("Apple");
         addGroceryItem("Melon");
+        addGroceryItem("Banana");
+
+        printGroceryList();
+
+
+        modifyGroceryItem(1, "James");
+        printGroceryList();
+
+        removeGroceryItem(0);
         printGroceryList();
     }
 
-    public static void addGroceryItem(String item){
+    public static void addGroceryItem(String item) {
         groceryList.add(item);
     }
 
-    public static void printGroceryList(){
+    public static void printGroceryList() {
         //int[] int1 = new int[5];
         System.out.println("Youu have " + groceryList.size() + " item in your list!");
-        for(int i=0;i<groceryList.size();i++){
-            System.out.println((i+1) + ". "+groceryList.get(i));
+        for (int i = 0; i < groceryList.size(); i++) {
+            System.out.println((i + 1) + ". " + groceryList.get(i));
         }
         //System.out.println(Arrays.toString(groceryList));
     }
 
-    public static 
+    public static void modifyGroceryItem(int pos, String newItem) {
+        groceryList.set(pos, newItem);
+        System.out.println("Grocery item " + (pos+1) + " has been modified!");
+    }
+
+    public static void removeGroceryItem(int pos){
+        String theItem = groceryList.get(pos);
+        groceryList.remove(pos);
+    }
 
 }
