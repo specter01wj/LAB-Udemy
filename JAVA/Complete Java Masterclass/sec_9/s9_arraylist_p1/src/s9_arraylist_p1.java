bd512f5c6e872e627d83671cf26687d14b97e9d6
specter01wj <specter01wj@gmail.com>
2018-08-04 21:50:43 -0400

    08/04/2018 v1

--- JAVA/Complete Java Masterclass/sec_9/s9_arraylist_p1/src/s9_arraylist_p1.java
+++ JAVA/Complete Java Masterclass/sec_9/s9_arraylist_p1/src/s9_arraylist_p1.java
@@ -6,25 +6,40 @@ public class s9_arraylist_p1 {
     private static Scanner s = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int[] baseData = new int[10];
+        int[] baseData = new int[3];
 
-        System.out.println("Enter 10 ints:");
+        System.out.println("Enter 3 ints:");
+        getInput(baseData);
+        printArray(baseData);
+        resizeArray(baseData);
+
+        System.out.println("Enter 5 ints:");
         getInput(baseData);
         printArray(baseData);
 
     }
 
     private static void getInput(int[] data) {
-        for(int i = 0;i<data.length; i++){
+        for (int i = 0; i < data.length; i++) {
             data[i] = s.nextInt();
         }
     }
 
     private static void printArray(int[] arr) {
-        for(int i =0;i<arr.length;i++){
+        for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
         System.out.println();
+    }
+
+    private static void resizeArray(int[] arr) {
+        int[] orig = arr;
+
+        arr = new int[5];
+        for (int i = 0; i < orig.length; i++) {
+            arr[i] = orig[i];
+        }
+        orig[1]=110;
     }
 
 
