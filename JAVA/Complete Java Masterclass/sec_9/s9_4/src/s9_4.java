8cf8f4b1359b3726486600a9b014ea97ee7f7569
specter01wj <specter01wj@gmail.com>
2018-08-05 20:44:50 -0400

    08/05/2018 v1

---
+++ JAVA/Complete Java Masterclass/sec_9/s9_4/src/s9_4.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class s9_4 {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+
+}
