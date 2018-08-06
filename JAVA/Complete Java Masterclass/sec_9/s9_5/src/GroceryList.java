c459e3bd8b1d94803dfc22ca076efdfba8517ac0
specter01wj <specter01wj@gmail.com>
2018-08-06 01:23:45 -0400

    08/05/2018 v1

--- JAVA/Complete Java Masterclass/sec_9/s9_5/src/GroceryList.java
+++ JAVA/Complete Java Masterclass/sec_9/s9_5/src/GroceryList.java
@@ -17,29 +17,33 @@ public class GroceryList {
 
     public void printGroceryList() {
         System.out.println("You have " + groceryList.size() + " items in your grocery list");
-        for(int i=0; i< groceryList.size(); i++) {
-            System.out.println((i+1) + ". " + groceryList.get(i));
+        for (int i = 0; i < groceryList.size(); i++) {
+            System.out.println((i + 1) + ". " + groceryList.get(i));
         }
     }
 
     public void modifyGroceryItem(String currentItem, String newItem) {
         int position = findItem(currentItem);
-        if(position >= 0) {
+        if (position >= 0) {
             modifyGroceryItem(position, newItem);
+        } else {
+            System.out.println("Item cannot find!");
         }
 
     }
+
     private void modifyGroceryItem(int position, String newItem) {
         groceryList.set(position, newItem);
-        System.out.println("Grocery item " + (position+1) + " has been modified.");
+        System.out.println("Grocery item " + (position + 1) + " has been modified.");
     }
 
     public void removeGroceryItem(String item) {
         int position = findItem(item);
-        if(position >= 0) {
+        if (position >= 0) {
             removeGroceryItem(position);
         }
     }
+
     private void removeGroceryItem(int position) {
         groceryList.remove(position);
     }
@@ -50,7 +54,7 @@ public class GroceryList {
 
     public boolean onFile(String searchItem) {
         int position = findItem(searchItem);
-        if(position >=0) {
+        if (position >= 0) {
             return true;
         }
 
