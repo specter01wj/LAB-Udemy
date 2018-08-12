6171bd2a9ccecb2237e0ad8c6154187f72eec7d0
specter01wj <specter01wj@gmail.com>
2018-08-12 02:49:11 -0400

    08/11/2018 v1

---
+++ JAVA/Complete Java Masterclass/sec_9/s9_autobox_p2/src/Bank.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Bank {
+
+    private String name;
+    private ArrayList<Branch> branches;
+
+    public Bank(String name) {
+        this.name = name;
+        this.branches = new ArrayList<Branch>();
+    }
+
+    public boolean addBranch(String branchName) {
+        if (findBranch(branchName) == null) {
+            this.branches.add(new Branch(branchName));
+            return true;
+        }
+        return false;
+    }
+
+
+}
---
+++ JAVA/Complete Java Masterclass/sec_9/s9_autobox_p2/src/s9_autobox_p2.java
@@ -0,0 +1,9 @@
+
+
+public class s9_autobox_p2 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
