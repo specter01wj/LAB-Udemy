36d7f25559c7050c989ff687ff821bc07df33538
specter01wj <specter01wj@gmail.com>
2018-08-11 22:49:40 -0400

    08/11/2018 v1

--- JAVA/Complete Java Masterclass/sec_9/s9_autobox_p1/src/Branch.java
+++ JAVA/Complete Java Masterclass/sec_9/s9_autobox_p1/src/Branch.java
@@ -3,6 +3,16 @@ import java.util.ArrayList;
 
 public class Branch {
 
+    private String name;
+    private ArrayList<Customer> customers;
+
+    public Branch(String name) {
+        this.name = name;
+        this.customers = new ArrayList<Customer>();
+    }
+
+
+    
 
 
 }
