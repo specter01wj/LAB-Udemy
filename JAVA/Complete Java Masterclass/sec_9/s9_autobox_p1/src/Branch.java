c233dcbb0caea22e64abc771315b7738e542dc53
specter01wj <specter01wj@gmail.com>
2018-08-11 23:00:46 -0400

    08/11/2018 v1

--- JAVA/Complete Java Masterclass/sec_9/s9_autobox_p1/src/Branch.java
+++ JAVA/Complete Java Masterclass/sec_9/s9_autobox_p1/src/Branch.java
@@ -16,13 +16,30 @@ public class Branch {
     }
 
     public boolean newCustomer(String customerName, double initialAmount) {
-        if(findCustomer(customerName) == null) {
+        if (findCustomer(customerName) == null) {
             this.customers.add(new Customer(customerName, initialAmount));
             return true;
         }
         return false;
     }
 
-    
+    public boolean addCustomerTransaction(String customerName, double amount) {
+        Customer existingCustomer = findCustomer(customerName);
+        if (existingCustomer != null) {
+            existingCustomer.addTransaction(amount);
+            return true;
+        }
+        return false;
+    }
+
+    private Customer findCustomer(String customerName) {
+        for (int i = 0; i < this.customers.size(); i++) {
+            Customer checkedCustomer = this.customers.get(i);
+            if (checkedCustomer.getName().equals(customerName)) {
+                return checkedCustomer;
+            }
+        }
+        return null;
+    }
 
 }
