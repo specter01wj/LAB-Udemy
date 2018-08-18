36ff8b66efe369bf5b3ba6039fca96deadb38052
specter01wj <specter01wj@gmail.com>
2018-08-18 00:59:14 -0400

    08/17/2018 v1

---
+++ JAVA/The Complete Java Developer Course/sec_3/s3_14/src/s3_14/s3_14.java
@@ -0,0 +1,14 @@
+package s3_14;
+
+public class s3_14 {
+	
+	public static void main(String[] args) {
+		
+		int n1 = 5, n2 = 10, n3 = 4;
+		int sum = (n1-- + n2) * n3;
+		
+		System.out.println("Sum: " + sum);
+		
+	}
+
+}
