7091cb2985506fc5816b002404d3553d32917227
specter01wj <specter01wj@gmail.com>
2018-08-19 21:29:34 -0400

    08/19/2018 v1

--- JAVA/The Complete Java Developer Course/sec_4/s4_18/src/s4_18/s4_18.java
+++ JAVA/The Complete Java Developer Course/sec_4/s4_18/src/s4_18/s4_18.java
@@ -7,11 +7,13 @@ public class s4_18 {
 	static Scanner reader = new Scanner(System.in);
 	
 	public static void main(String[] args) {
-		
+		System.out.print("Enter grade: ");
 		int grade = reader.nextInt();
 		
 		if(grade >= 90){
 			System.out.println("Grade A!");
+		} else if(grade < 90 && grade >=60) {
+			System.out.println("Grade B!");
 		} else {
 			System.out.println("Grade F!");
 		}
