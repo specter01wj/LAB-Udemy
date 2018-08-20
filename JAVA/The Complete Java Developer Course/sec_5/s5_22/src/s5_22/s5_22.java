743a70d6910e5bd033d07fd03a62a3675aa04059
specter01wj <specter01wj@gmail.com>
2018-08-20 00:55:13 -0400

    08/19/2018 v1

--- JAVA/The Complete Java Developer Course/sec_5/s5_22/src/s5_22/s5_22.java
+++ JAVA/The Complete Java Developer Course/sec_5/s5_22/src/s5_22/s5_22.java
@@ -1,5 +1,16 @@
 package s5_22;
 
 public class s5_22 {
+	
+	public static void main(String[] args) {
+		
+		for(int i=1; i < 30; i++) {
+			if(i % 4 == 0){
+				System.out.println("count is: " + i);
+			}
+		}
+		
+		
+	}
 
 }
