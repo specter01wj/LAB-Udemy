4de37d87040f6101f6e46582d3b46cb7da723e9e
specter01wj <specter01wj@gmail.com>
2018-08-21 18:04:55 -0400

    08/21/2018 v1

---
+++ JAVA/The Complete Java Developer Course/sec_5/s5_23/src/s5_23/s5_23.java
@@ -0,0 +1,16 @@
+package s5_23;
+
+public class s5_23 {
+	
+	public static void main(String[] args) {
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println("*");
+			for(int j = i; j>= 0; j--) {
+				System.out.print("-");
+			}
+		}
+		
+	}
+
+}
