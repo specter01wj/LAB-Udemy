f45d8923933eb8285801ca1d82f2f236d6b933fb
specter01wj <specter01wj@gmail.com>
2018-08-22 13:05:53 -0400

    08/22/2018 v1

---
+++ JAVA/The Complete Java Developer Course/sec_5/s5_24/src/s5_24/s5_24.java
@@ -0,0 +1,18 @@
+package s5_24;
+
+public class s5_24 {
+	
+	public static void main(String[] args) {
+		
+		int i = 1;
+		while(i<100) {
+			if(i % 4 == 0) {
+				System.out.println("by 4 is: " + i);
+			}
+			i++;
+		}
+		System.out.println("App done!");
+		
+	}
+
+}
