88041909d29c372f9d2bd2dc315bb073cb5d378b
specter01wj <specter01wj@gmail.com>
2018-08-22 16:56:49 -0400

    08/22/2018 v1

---
+++ JAVA/The Complete Java Developer Course/sec_5/s5_25/src/s5_25/s5_25.java
@@ -0,0 +1,19 @@
+package s5_25;
+
+public class s5_25 {
+	
+	public static void main(String[] args) {
+		
+		for(int i = 0; i <= 100; i++) {
+			if( i == 50 ) break;
+			
+			if( i % 2 == 0) continue;
+			
+			System.out.println("output: " + i);
+			
+		}
+		
+		
+	}
+
+}
