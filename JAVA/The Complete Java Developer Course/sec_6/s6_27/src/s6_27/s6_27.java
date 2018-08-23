c330e216ea8c82f481e553662cadf28883c5f7ce
specter01wj <specter01wj@gmail.com>
2018-08-23 01:00:53 -0400

    08/21/2018 v1

---
+++ JAVA/The Complete Java Developer Course/sec_6/s6_27/src/s6_27/s6_27.java
@@ -0,0 +1,20 @@
+package s6_27;
+
+public class s6_27 {
+	
+	public static void main(String[] args) {
+		
+		String[] jobs = new String[5];
+		jobs[0] = "dev";
+		jobs[1] = "tester";
+		jobs[2] = "qa";
+		jobs[3] = "manager";
+		jobs[4] = "admin";
+		
+		for(int i = 0; i < 5; i++) {
+			System.out.println(jobs[i]);
+		}
+		
+	}
+
+}
