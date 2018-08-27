9c2c0b1156eafef52589da95c5e7ae6defdccf9b
specter01wj <specter01wj@gmail.com>
2018-08-26 21:39:43 -0400

    08/26/2018 v1

---
+++ JAVA/The Complete Java Developer Course/sec_6/s6_30/src/s6_30/s6_30.java
@@ -0,0 +1,21 @@
+package s6_30;
+
+public class s6_30 {
+	
+	public static void main(String[] args) {
+		
+		String name = "University of Maryland!";
+		
+		for(int i=0; i<name.length();i++){
+			System.out.print(name.charAt(i));
+		}
+		for(int i=name.length()-1; i>=0;i--){
+			System.out.print(name.charAt(i));
+		}
+		System.out.println("");
+		
+		System.out.println("print split: ");
+		
+	}
+
+}
