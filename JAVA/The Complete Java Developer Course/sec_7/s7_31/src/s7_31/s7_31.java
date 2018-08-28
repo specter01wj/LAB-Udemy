2cf0e76ae2a5d4efbe965df8e1bf3bf4fec7778f
specter01wj <specter01wj@gmail.com>
2018-08-28 01:23:02 -0400

    08/27/2018 v1

--- JAVA/The Complete Java Developer Course/sec_7/s7_31/src/s7_31/s7_31.java
+++ JAVA/The Complete Java Developer Course/sec_7/s7_31/src/s7_31/s7_31.java
@@ -11,9 +11,17 @@ public class s7_31 {
 		}
 	}
 	
+	float sum(float n1, float n2){
+		float sum = n1 + n2;
+		return sum;
+	}
+	
 	public static void main(String[] args) {
 		
 		new s7_31().Draw();
+		
+		float r1 = new s7_31().sum(9, 13);
+		System.out.println("r1 = " + r1);
 		
 	}
 
