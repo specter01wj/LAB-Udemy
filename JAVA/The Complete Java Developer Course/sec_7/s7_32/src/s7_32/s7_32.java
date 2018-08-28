754a005e8a77c105daa0ab3b768dd5827856c7e9
specter01wj <specter01wj@gmail.com>
2018-08-28 01:29:55 -0400

    08/27/2018 v1

--- JAVA/The Complete Java Developer Course/sec_7/s7_32/src/s7_32/s7_32.java
+++ JAVA/The Complete Java Developer Course/sec_7/s7_32/src/s7_32/s7_32.java
@@ -7,11 +7,20 @@ public class s7_32 {
 		return result;
 	}
 	
+	float Sub(float n1, float n2, float n3){
+		float result = n1 - n2 - n3;
+		return result;
+	}
+	
 	public static void main(String[] args) {
 		
 		float r1 = new s7_32().Sub(23, 15);
 		
 		System.out.println("r1 = " + r1);
+		
+		float r2 = new s7_32().Sub(23, 15, 78);
+		
+		System.out.println("r2 = " + r2);
 	}
 
 }
