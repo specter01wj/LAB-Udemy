e92d3d2ea83f2bc8fd0b0b445ceaaa2f34b63233
specter01wj <specter01wj@gmail.com>
2018-09-06 18:26:29 -0400

    09/06/2018 v1

---
+++ JAVA/The Complete Java Developer Course/sec_5/s7_33/src/s7_33/s7_33.java
@@ -0,0 +1,16 @@
+package s7_33;
+
+public class s7_33 {
+	
+	double div(double n1, double n2) {
+		double r = n1 / n2;
+		return r;
+	}
+	
+	
+	public static void main(String[] args) {
+		double r = new s7_33().div(19.32, 7.26);
+		System.out.println("output r: " + r);
+	}
+	
+}
