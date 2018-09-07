20b662add1dc3a33c77f64706f01a397d3f03a98
specter01wj <specter01wj@gmail.com>
2018-09-07 17:52:13 -0400

    09/06/2018 v1

--- JAVA/The Complete Java Developer Course/sec_7/s7_34/src/s7_34/s7_34.java
+++ JAVA/The Complete Java Developer Course/sec_7/s7_34/src/s7_34/s7_34.java
@@ -12,6 +12,22 @@ public class s7_34 {
 		float r = new s7_34().sum(19.73f, 7.78f);
 		System.out.println("output r: " + r);
 		
+		for(int i=1;i<=4;i++){
+	        new s7_34().MoveForward();
+	        for(int j=1;j<=1;j++){
+	            new s7_34().MoveLeft();
+	        }
+	    }
+		
+	}
+	
+	static void MoveLeft(){
+	    //think this method have  able to move the Robot left
+	    //donot write code here 
+	}
+	static void MoveForward(){
+	    //think this method have  able to move the Robot left
+	    //donot write code here 
 	}
 
 }
