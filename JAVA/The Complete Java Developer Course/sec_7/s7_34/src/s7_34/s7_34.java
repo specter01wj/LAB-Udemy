c5f47558d275810269bd5b64b8bd5e783660d7e0
specter01wj <specter01wj@gmail.com>
2018-09-07 17:46:29 -0400

    09/06/2018 v1

---
+++ JAVA/The Complete Java Developer Course/sec_7/s7_34/src/s7_34/s7_34.java
@@ -0,0 +1,17 @@
+package s7_34;
+
+public class s7_34 {
+	
+	float sum(float n1, float n2) {
+		float r = n1 + n2;
+		return r;
+	}
+	
+	public static void main(String[] args) {
+		
+		float r = new s7_34().sum(19.73f, 7.78f);
+		System.out.println("output r: " + r);
+		
+	}
+
+}
