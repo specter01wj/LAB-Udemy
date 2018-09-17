9ec93605d146049a8497158f422c5fc1deb2b9fb
specter01wj <specter01wj@gmail.com>
2018-09-17 01:04:48 -0400

    09/16/2018 v1

---
+++ JAVA/The Complete Java Developer Course/sec_8/s8_43/src/s8_43/Car.java
@@ -0,0 +1,36 @@
+package s8_43;
+
+public class Car {
+
+	String Type;
+	protected int Model;
+	double Price;
+	double MilesDrive;
+	static String Owner;
+	Owners owners;
+	
+	public Car(String Type, int Model, double Price, double MilesDrive){
+		this.Type = Type;
+		this.Model = Model;
+		this.Price = Price;
+		this.MilesDrive = MilesDrive;
+		System.out.println("Constructor 1 created!");
+		owners = new Owners();
+		owners.FirstName = "Kim";
+		System.out.println("Owner name: " + owners.FirstName);
+	}
+	
+	public Car(){
+		this.Type = "TOYOTA";
+		this.Model = 2017;
+		this.Price = 50000;
+		this.MilesDrive = 15;
+		System.out.println("Constructor 2 created!");
+	}
+	
+	double GetPrice(){
+		double NewPrice = Price - (MilesDrive * 100);
+		return NewPrice;
+	}
+	
+}
---
+++ JAVA/The Complete Java Developer Course/sec_8/s8_43/src/s8_43/Owners.java
@@ -0,0 +1,11 @@
+package s8_43;
+
+import java.sql.Date;
+
+public class Owners {
+
+	String FirstName;
+	String LastName;
+	Date DOB;
+		
+}
