fe5371ca930ddcefa24b0bc84e19f997d976ccc8
specter01wj <specter01wj@gmail.com>
2018-09-16 22:04:29 -0400

    09/16/2018 v1

---
+++ JAVA/The Complete Java Developer Course/sec_8/s8_42/src/s8_42/CarTruck.java
@@ -0,0 +1,27 @@
+package s8_42;
+
+public class CarTruck extends Car {
+
+	private boolean HasTruck;
+	
+	void SetHasTruck(boolean HasTruck){
+		this.HasTruck = HasTruck;
+	}
+	
+	public CarTruck(){
+		super("Honda", 2010, 75000, 8);
+		System.out.println("Constructor of Car Truck!");
+	}
+	
+	void GetOwner(){
+		System.out.println("Owner: " + Owner);
+		System.out.println("Price: " + super.GetPrice());
+	}
+	
+	@Override
+	double GetPrice(){
+		double NewPrice = Price - (MilesDrive * 500);
+		return NewPrice;
+	}
+	
+}
