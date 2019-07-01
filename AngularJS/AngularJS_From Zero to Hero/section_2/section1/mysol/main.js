41abe6bc77ac1307621969eaaaf3ae9addee094c
specter01wj <specter01wj@gmail.com>
2019-07-01 18:09:12 -0400

    06/30/2019 v1

---
+++ AngularJS/AngularJS_From Zero to Hero/section_2/section1/mysol/main.js
@@ -0,0 +1,48 @@
+(function (angular) {
+
+	var app = angular.module('minmax', [])
+           .controller('MinMaxCtrl', MinMaxCtrl);
+
+    MinMaxCtrl.$inject = ['$http'];
+
+	// app.run(function (defaultErrorMessageResolver) {
+	// 		defaultErrorMessageResolver.getErrorMessages().then(function (errorMessages) {
+	// 			errorMessages['tooYoung'] = 'You must be at least {0} years old to use this site';
+	// 			errorMessages['tooOld'] = 'You must be max {0} years old to use this site';
+	// 			errorMessages['badUsername'] = 'Username can only contain numbers and letters and _';
+	// 		});
+	// 	}
+	// );
+
+
+	function MinMaxCtrl($http) {
+		var vm = this;
+		vm.formModel = {};
+		vm.submitting = false;
+		vm.submitted = false;
+		vm.has_error = false;
+
+
+
+		vm.onSubmit = function () {
+			vm.submitting = true;
+			console.log("Hey i'm submitted!");
+			console.log(vm.formModel);
+
+			$http.post('https://minmax-server.herokuapp.com/register/', vm.formModel).
+				success(function (data) {
+					console.log(":)");
+					vm.submitting = false;
+					vm.submitted = true;
+					vm.has_error = false;
+				}).error(function(data) {
+					console.log(":(");
+					vm.submitting = false;
+					vm.submitted = false;
+					vm.has_error = true;
+				});
+
+		};
+    }
+})(window.angular);
+
