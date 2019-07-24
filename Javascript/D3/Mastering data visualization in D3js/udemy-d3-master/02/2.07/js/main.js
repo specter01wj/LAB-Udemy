b5848b939feaabb0545f8426002df6401dac8cc2
specter01wj <specter01wj@gmail.com>
2019-07-24 17:16:13 -0400

    07/15/2019 v1

--- Javascript/D3/Mastering data visualization in D3js/udemy-d3-master/02/2.07/js/main.js
+++ Javascript/D3/Mastering data visualization in D3js/udemy-d3-master/02/2.07/js/main.js
@@ -7,6 +7,9 @@
 d3.tsv("data/ages.tsv").then(function(data){
     data.forEach(function(d){
         d.age = +d.age;
+        /*ParseInt() and + are different:
+        parseInt("10.3456") // returns 10
+        +"10.3456" // returns 10.3456*/
     });
 
     var svg = d3.select("#chart-area").append("svg")
@@ -18,22 +21,22 @@ d3.tsv("data/ages.tsv").then(function(data){
 
     circles.enter()
         .append("circle")
-            .attr("cx", function(d, i){
-                console.log(d);
-                return (i * 50) + 25;
-            })
-            .attr("cy", 25)
-            .attr("r", function(d){
-                return d.age * 2;
-            })
-            .attr("fill", function(d){
-                if (d.name == "Tony") {
-                    return "blue";
-                }
-                else {
-                    return "red";
-                }
-            });
+        .attr("cx", function(d, i){
+            console.log(d);
+            return (i * 50) + 25;
+        })
+        .attr("cy", 25)
+        .attr("r", function(d){
+            return d.age * 2;
+        })
+        .attr("fill", function(d){
+            if (d.name == "Andrew") {
+                return "blue";
+            }
+            else {
+                return "red";
+            }
+        });
 }).catch(function(error){
     console.log(error);
 })
