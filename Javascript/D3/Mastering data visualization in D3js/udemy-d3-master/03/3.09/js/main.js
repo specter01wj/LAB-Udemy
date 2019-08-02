227a8ca0356933bf2530c6a37a7407bd20f36a79
specter01wj <specter01wj@gmail.com>
2019-08-02 12:38:47 -0400

    07/25/2019 v1

--- Javascript/D3/Mastering data visualization in D3js/udemy-d3-master/03/3.09/js/main.js
+++ Javascript/D3/Mastering data visualization in D3js/udemy-d3-master/03/3.09/js/main.js
@@ -43,11 +43,11 @@ d3.json("data/buildings.json").then(function(data){
         
     rects.enter()
         .append("rect")
-        .attr("y", 0)
-        .attr("x", function(d){ return x(d.name); })
-        .attr("width", x.bandwidth)
-        .attr("height", function(d){ return y(d.height); })
-        .attr("fill", "grey");
+            .attr("y", 0)
+            .attr("x", function(d){ return x(d.name); })
+            .attr("width", x.bandwidth)
+            .attr("height", function(d){ return y(d.height); })
+            .attr("fill", "grey");
 
 })
 
