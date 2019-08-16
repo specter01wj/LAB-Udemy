d255778e7a01d83e4d034943dd12b034edb4d873
specter01wj <specter01wj@gmail.com>
2019-08-16 03:06:24 -0400

    08/08/2019 v1

--- AngularJS/Learn to Build AngularJS Custom Directives with Dan Wahlin/code/module3/mysol_1_link()fun/link.js
+++ AngularJS/Learn to Build AngularJS Custom Directives with Dan Wahlin/code/module3/mysol_1_link()fun/link.js
@@ -4,8 +4,18 @@
 
   app.directive('linkDemo', function () {
       return {
-
-      	
+      	restrict: 'A',
+          link: function(scope, elem, attrs) {
+              elem.on('click', function() {
+                  elem.html('You clicked me');
+              });
+              elem.on('mouseenter', function() {
+                  elem.css('background-color', 'yellow');
+              });
+              elem.on('mouseleave', function() {
+                  elem.css('background-color', 'white');
+              });
+          }
       };
   });
 
