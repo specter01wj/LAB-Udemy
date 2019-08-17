6825d5f7984f4df28372e0bd8ef2957c5bc8cfb3
specter01wj <specter01wj@gmail.com>
2019-08-17 00:36:00 -0400

    08/08/2019 v1

--- AngularJS/Learn to Build AngularJS Custom Directives with Dan Wahlin/code/module3/mysol_6_delayBindWithCompile/delayBindWithCompile.js
+++ AngularJS/Learn to Build AngularJS Custom Directives with Dan Wahlin/code/module3/mysol_6_delayBindWithCompile/delayBindWithCompile.js
@@ -2,11 +2,33 @@
 
     var delayBindWithCompile = ['$interpolate', function ($interpolate) {
 
+      	var compile = function(tElem, tAttrs) {
+            console.log('In compile');
+            var interpolateFunc = $interpolate(tAttrs.delayBind);
+            tAttrs.$set('delayBind', null); //Clear it out so no bindings occur
 
-      return {
-          restrict: 'A',
-          compile: compile
-      };
+            return {
+                pre: function(scope, elem, attrs) {
+                    console.log('In delayBind pre ' + elem[0].tagName);
+                },
+                post: function(scope, elem, attrs) {
+                    console.log('In delayBind post ' + elem[0].tagName);
+                    elem.on(attrs.trigger, function(event) {
+                        var attr = attrs.attribute,
+                            val = interpolateFunc(scope);
+                        if (attr && !elem.attr(attr)) {
+                            elem.attr(attr, val);
+                        }
+                    });
+                }
+            };
+
+        };
+
+      	return {
+          	restrict: 'A',
+          	compile: compile
+      	};
   }];
 
   angular.module('directivesModule')
