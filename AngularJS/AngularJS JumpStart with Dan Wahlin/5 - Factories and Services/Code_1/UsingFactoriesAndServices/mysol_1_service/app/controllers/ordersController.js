015dbb2ed9eb040ed2c69d197efc2d7891e7fca2
specter01wj <specter01wj@gmail.com>
2019-08-17 01:36:38 -0400

    08/08/2019 v1

---
+++ AngularJS/AngularJS JumpStart with Dan Wahlin/5 - Factories and Services/Code_1/UsingFactoriesAndServices/mysol_1_service/app/controllers/ordersController.js
@@ -0,0 +1,19 @@
+(function() {
+    
+    var OrdersController = function ($scope, $routeParams, customersService) {
+        var customerId = $routeParams.customerId;
+        $scope.customer = null;
+        
+        function init() {
+            $scope.customer = customersService.getCustomer(customerId);
+        }
+        
+        init();
+    };
+    
+    OrdersController.$inject = ['$scope', '$routeParams', 'customersService'];
+
+    angular.module('customersApp')
+      .controller('OrdersController', OrdersController);
+    
+}());
