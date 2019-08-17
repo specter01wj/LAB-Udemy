d45db08aea1cccfbca5b36ed92e80d9956fab452
specter01wj <specter01wj@gmail.com>
2019-08-17 01:47:32 -0400

    08/08/2019 v1

---
+++ AngularJS/AngularJS JumpStart with Dan Wahlin/5 - Factories and Services/Code_1/UsingFactoriesAndServices/mysol_1_values/app/controllers/ordersController.js
@@ -0,0 +1,19 @@
+(function() {
+    
+    var OrdersController = function ($scope, $routeParams, customersFactory) {
+        var customerId = $routeParams.customerId;
+        $scope.customer = null;
+        
+        function init() {
+            $scope.customer = customersFactory.getCustomer(customerId);
+        }
+        
+        init();
+    };
+    
+    OrdersController.$inject = ['$scope', '$routeParams', 'customersFactory'];
+
+    angular.module('customersApp')
+      .controller('OrdersController', OrdersController);
+    
+}());
---
+++ AngularJS/AngularJS JumpStart with Dan Wahlin/5 - Factories and Services/Code_1/UsingFactoriesAndServices/mysol_1_values/app/services/values.js
@@ -0,0 +1,10 @@
+/*angular.module('customersApp').value('appSettings', {
+    title: 'Customers Application',
+    version: '1.0'
+});
+*/
+
+angular.module('customersApp').constant('appSettings', {
+    title: 'Customers Application v1.1',
+    version: '1.0'
+});
