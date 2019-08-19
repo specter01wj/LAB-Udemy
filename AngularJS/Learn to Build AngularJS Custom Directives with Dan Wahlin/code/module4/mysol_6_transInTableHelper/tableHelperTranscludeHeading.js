c4017a3cabfd92d4affc1487b7916ef0a385fb67
specter01wj <specter01wj@gmail.com>
2019-08-19 03:44:33 -0400

    08/08/2019 v1

--- AngularJS/Learn to Build AngularJS Custom Directives with Dan Wahlin/code/module4/mysol_6_transInTableHelper/tableHelperTranscludeHeading.js
+++ AngularJS/Learn to Build AngularJS Custom Directives with Dan Wahlin/code/module4/mysol_6_transInTableHelper/tableHelperTranscludeHeading.js
@@ -2,9 +2,21 @@
 
   var controllerAs = function () {
 
-      var link = function(scope, elem, attrs) {
+      var link = function(scope, elem, attrs, tableHelperCtrl, transclude) {
 
+          transclude(scope, function(content) {
 
+              var th = angular.element('<th mapsto="' + scope.mapsto + '">' +
+                                       content.html() + '</th>');
+              th.on('click', function() {
+                  scope.$apply(function() {
+                    tableHelperCtrl.sort(scope.mapsto);
+                  });
+              });
+
+              elem.replaceWith(th);
+
+          });
       };
 
       return {
