3e81259f13df41f41649d6664949523d2754ddae
specter01wj <specter01wj@gmail.com>
2019-08-18 22:34:52 -0400

    08/08/2019 v1

--- AngularJS/Learn to Build AngularJS Custom Directives with Dan Wahlin/code/module4/mysol_1_replaceLink/withController.js
+++ AngularJS/Learn to Build AngularJS Custom Directives with Dan Wahlin/code/module4/mysol_1_replaceLink/withController.js
@@ -5,18 +5,33 @@
     var template = '<button ng-click="addItem()">Add Item</button><ul>' +
                  '<li ng-repeat="item in items">{{ ::item.name }}</li></ul>',
 
+        controller = ['$scope', function($scope) {
 
-      }];
+            init();
 
-      return {
-          restrict: 'EA', //Default in 1.3+
-          scope: {
-              datasource: '=',
-              add: '&',
-          },
+            function init() {
+               $scope.items = angular.copy($scope.datasource);
+            }
 
-          template: template
-      };
+            $scope.addItem = function() {
+                var name = 'New Directive Controller Item';
+                $scope.add()(name);
+                $scope.items.push({
+                    name: name
+                });
+            }
+
+        }];
+
+        return {
+            restrict: 'EA', //Default in 1.3+
+            scope: {
+                datasource: '=',
+                add: '&',
+            },
+            controller: controller,
+            template: template
+        };
   };
 
   angular.module('directivesModule')
