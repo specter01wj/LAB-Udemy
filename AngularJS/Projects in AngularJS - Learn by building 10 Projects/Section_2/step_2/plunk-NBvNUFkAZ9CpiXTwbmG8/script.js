9173ad0a205eaa2c168ed6ff6d604cc17d714029
specter01wj <specter01wj@gmail.com>
2019-08-23 22:37:24 -0600

    08/08/2019 v1

---
+++ AngularJS/Projects in AngularJS - Learn by building 10 Projects/Section_2/step_2/plunk-NBvNUFkAZ9CpiXTwbmG8/script.js
@@ -0,0 +1,16 @@
+var app = angular.module("computer",['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider){
+  $routeProvider.
+    when('/main',{
+      templateUrl: 'main.html',
+      controller:'MainCtrl'
+    }).
+    otherwise({redirectTo:'/main'})
+}])
+
+.controller('MainCtrl', ['$scope', function($scope){
+  $scope.person = 'John Doe';
+  console.log($scope);
+}]);
+
