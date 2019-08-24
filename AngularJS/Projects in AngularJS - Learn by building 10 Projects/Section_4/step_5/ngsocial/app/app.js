e8ce967e3cf3057d9df1891cbff52f6d70b0752c
specter01wj <specter01wj@gmail.com>
2019-08-23 22:44:16 -0600

    08/08/2019 v1

---
+++ AngularJS/Projects in AngularJS - Learn by building 10 Projects/Section_4/step_5/ngsocial/app/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('ngSocial', [
+  'ngRoute',
+  'ngSocial.view1',
+  'ngSocial.view2',
+  'ngSocial.facebook'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.otherwise({redirectTo: '/facebook'});
+}]);
