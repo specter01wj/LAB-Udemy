3c92a6f4867960fd82a63d7fb10c2897d2908112
specter01wj <specter01wj@gmail.com>
2019-08-23 22:41:05 -0600

    08/08/2019 v1

---
+++ AngularJS/Projects in AngularJS - Learn by building 10 Projects/Section_4/step_3/ngsocial/app/facebook/facebook.js
@@ -0,0 +1,34 @@
+'use strict';
+
+angular.module('ngSocial.facebook', ['ngRoute','ngFacebook'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/facebook', {
+    templateUrl: 'facebook/facebook.html',
+    controller: 'FacebookCtrl'
+  });
+}])
+
+.config( function( $facebookProvider ) {
+  $facebookProvider.setAppId('846149575421651');
+  $facebookProvider.setPermissions("email","public_profile", "user_posts", "publish_actions", "user_photos");
+})
+
+.run(function($rootScope){
+	(function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+})
+
+.controller('FacebookCtrl', ['$scope', '$facebook', function($scope, $facebook) {
+	$scope.isLoggedIn = false;
+	$scope.login = function(){
+		$facebook.login().then(function(){
+			console.log('LOGGED IN');
+		});
+	}
+}]);
