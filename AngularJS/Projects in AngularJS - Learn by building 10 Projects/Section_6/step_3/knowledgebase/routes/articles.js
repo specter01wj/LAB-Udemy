8b3198a6e5736f875a6c2007fc4af8bcfb9f1147
specter01wj <specter01wj@gmail.com>
2019-08-27 00:13:38 -0600

    08/08/2019 v1

---
+++ AngularJS/Projects in AngularJS - Learn by building 10 Projects/Section_6/step_3/knowledgebase/routes/articles.js
@@ -0,0 +1,96 @@
+var express = require('express');
+var router = express.Router();
+
+var Article = require('../models/article');
+
+router.get('/', function(req, res, next) {
+  Article.getArticles(function(err, articles){
+  	if(err){
+  		console.log(err);
+  	}
+  	res.json(articles);
+  });
+});
+
+router.get('/:id', function(req, res, next) {
+  Article.getArticleById(req.params.id, function(err, article){
+  	if(err){
+  		console.log(err);
+  	}
+  	res.json(article);
+  });
+});
+
+router.get('/category/:category', function(req, res, next) {
+  Article.getArticlesByCategory(req.params.category, function(err, articles){
+  	if(err){
+  		console.log(err);
+  	}
+  	res.json(articles);
+  });
+});
+
+router.post('/', function(req, res, next){
+	// Get Form Values
+	var title = req.body.title;
+	var category = req.body.category;
+	var body = req.body.body;
+
+	// Article Object
+	var newArticle = new Article({
+		title: title,
+		category: category,
+		body: body
+	});
+
+	// Create Article
+	Article.createArticle(newArticle, function(err, article){
+		if(err){
+			console.log(err);
+		}
+
+		res.location('/articles');
+		res.redirect('/articles');
+	});
+});
+
+
+// Update Article
+router.put('/', function(req, res, next){
+	var id	= req.body.id;
+	var data = {
+		title: req.body.title,
+		category: req.body.category,
+		body: req.body.body
+	};
+
+	// Create Article
+	Article.updateArticle(id, data, function(err, article){
+		if(err){
+ 			console.log(err);
+ 		}
+ 		
+		res.location('/articles');
+		res.redirect('/articles');
+	});
+});
+
+
+// Remove Article
+router.delete('/:id', function(req, res, next){
+	var id	= req.params.id;
+
+	// Create Article
+	Article.removeArticle(id, function(err, article){
+		if(err){
+ 			console.log(err);
+ 		}
+		
+		res.location('/articles');
+		res.redirect('/articles');
+	});
+});
+
+
+
+module.exports = router;
