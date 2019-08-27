71d4d4a33106d4a83e8ed5be6d93da3b6ffacce6
specter01wj <specter01wj@gmail.com>
2019-08-26 23:58:02 -0600

    08/08/2019 v1

---
+++ AngularJS/Projects in AngularJS - Learn by building 10 Projects/Section_5/step_3/jobfinder/app/models/job.server.model.js
@@ -0,0 +1,59 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+/**
+ * Job Schema
+ */
+var JobSchema = new Schema({
+	created: {
+		type: Date,
+		default: Date.now
+	},
+	company: {
+		type: String,
+		default: '',
+		trim: true,
+		required: 'Company cannot be blank'
+	},
+	title: {
+		type: String,
+		default: '',
+		trim: true,
+		required: 'Title cannot be blank'
+	},
+	description: {
+		type: String,
+		default: '',
+		trim: true
+	},
+	requirements: {
+		type: String,
+		default: '',
+		trim: true
+	},
+	hourly_wage: {
+		type: Number,
+		default: '',
+		trim: true
+	},
+	state: {
+		type: String,
+		default: ''
+	},
+	contact_email: {
+		type: String,
+		default: '',
+		trim: true
+	},
+	user: {
+		type: Schema.ObjectId,
+		ref: 'User'
+	}
+});
+
+mongoose.model('Job', JobSchema);
