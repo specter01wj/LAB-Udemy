ad48370bc256bb61c1acb3d1d8ca8d4453235334
specter01wj <specter01wj@gmail.com>
2019-08-27 00:23:19 -0600

    08/08/2019 v1

---
+++ AngularJS/Projects in AngularJS - Learn by building 10 Projects/Section_7/step_4/userauth/api/controllers/UserController.js
@@ -0,0 +1,60 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side logic for managing users
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	// Sign User Up
+	signup: function(req, res){
+		console.log('Backend Signup');
+
+		var Passwords = require('machinepack-passwords');
+
+		// Encrypt Password
+		Passwords.encryptPassword({
+			password: req.param('password'),
+			difficulty: 10,
+		}).exec({
+			error: function(err){
+				console.log(1);
+				return res.negotiate(err);
+			},
+			success: function(encryptedPassword){
+				console.log(2);
+				require('machinepack-gravatar').getImageUrl({
+					emailAddress: req.param('email')
+				}).exec({
+					error: function(err){
+						return res.negotiate(err);
+					},
+					success: function(gravatarUrl){
+						// Create User
+						User.create({
+							name: req.param('name'),
+							email: req.param('email'),
+							password: encryptedPassword,
+							lastLoggedIn: new Date(),
+							gravatarUrl: gravatarUrl
+						}, function userCreated(err, newUser){
+							if(err){
+								console.log('Error: '+err);
+								return res.negotiate(err);
+							}
+
+							//SESSION VAR
+
+							console.log('User Added');
+
+							return res.json({
+								id: newUser.id
+							});
+						})
+					}
+				})
+			}
+		})
+	}
+};
+
