ce4a4dfa9f9468b585a33855bb13ca884d9ce4ee
specter01wj <specter01wj@gmail.com>
2019-08-27 00:14:56 -0600

    08/08/2019 v1

---
+++ AngularJS/Projects in AngularJS - Learn by building 10 Projects/Section_6/step_4/knowledgebase/public/controllers/articles.controller.js
@@ -0,0 +1,20 @@
+angular.module("kB")
+
+.controller('ArticlesCtrl', ['$scope','$http', function($scope, $http){
+	$http.get('/articles').success(function(data){
+		$scope.articles = data;
+	});
+}])
+
+.controller('ArticlesCategoryCtrl', ['$scope','$http', '$routeParams', function($scope, $http, $routeParams){
+	$http.get('/articles/category/'+$routeParams.category).success(function(data){
+		$scope.cat_articles = data;
+		$scope.category = $routeParams.category;
+	});
+}])
+
+.controller('ArticlesDetailsCtrl', ['$scope','$http', '$routeParams', function($scope, $http, $routeParams){
+	$http.get('/articles/'+$routeParams.id).success(function(data){
+		$scope.article = data;
+	});
+}])
---
+++ AngularJS/Projects in AngularJS - Learn by building 10 Projects/Section_6/step_4/knowledgebase/routes/categories.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+var Category = require('../models/category');
+
+router.get('/', function(req, res, next) {
+  Category.getCategories(function(err, categories){
+  	if(err){
+  		console.log(err);
+  	}
+  	res.json(categories);
+  });
+});
+
+router.get('/:id', function(req, res, next) {
+  Category.getCategoryById(req.params.id, function(err, category){
+  	if(err){
+  		console.log(err);
+  	}
+  	res.json(category);
+  });
+});
+
+
+module.exports = router;
