0cb0efd73a283ba8be3f21aa5052e20c620e9237
specter01wj <specter01wj@gmail.com>
2019-08-27 00:29:05 -0600

    08/08/2019 v1

---
+++ AngularJS/Projects in AngularJS - Learn by building 10 Projects/Section_9/step_1/pnChat/app/scripts/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name pnChatApp
+ * @description
+ * # pnChatApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('pnChatApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
