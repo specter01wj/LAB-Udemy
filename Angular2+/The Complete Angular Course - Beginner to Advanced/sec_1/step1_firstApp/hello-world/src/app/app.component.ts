79fc9aae5fb2a41a328358efce702af34800e155
specter01wj <specter01wj@gmail.com>
2019-08-31 23:18:26 -0600

    08/30/2019 v1

--- Angular2+/The Complete Angular Course - Beginner to Advanced/sec_1/step1_firstApp/hello-world/src/app/app.component.ts
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_1/step1_firstApp/hello-world/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'Angular hello-world';
+  title = 'Angular James hello-world';
 }
