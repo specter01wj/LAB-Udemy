d6211f320734970603bcd2e9b4527b89e754fc43
specter01wj <specter01wj@gmail.com>
2019-10-11 12:13:01 -0400

    09/05/2019 v1

---
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_4/hello-world/src/app/courses/courses.component.ts
@@ -0,0 +1,30 @@
+
+import { Component, OnInit } from '@angular/core';
+import { CoursesService } from '../service/courses.service';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.less']
+})
+export class CoursesComponent implements OnInit {
+
+	name = "James Wang";
+
+	// courses = ["course1", "course2", "course3"];
+	courses = [];
+  imageUrl = "http://lorempixel.com/400/200/";
+
+	getTitle() {
+		return this.name;
+	}
+
+  constructor(service: CoursesService) { 
+  	// let service = new CoursesService();
+  	this.courses = service.getCourses();
+  }
+
+  ngOnInit() {
+  }
+
+}
