97f88aba44ec570a4535627c407dd96d3d608a3e
specter01wj <specter01wj@gmail.com>
2019-10-13 04:07:50 -0400

    09/05/2019 v1

--- Angular2+/The Complete Angular Course - Beginner to Advanced/sec_5/hello-world/src/app/favorite/favorite.component.ts
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_5/hello-world/src/app/favorite/favorite.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-favorite',
   templateUrl: './favorite.component.html',
-  styleUrls: ['./favorite.component.less']
+  styleUrls: ['./favorite.component.less'],
+  encapsulation: ViewEncapsulation.Emulated
   /*styles: [
   	`
   		.glyphicon {
