e2647f6a0e352fd9e9ca2d23f2efce2f68ff46ea
specter01wj <specter01wj@gmail.com>
2019-10-15 12:40:21 -0400

    10/15/2019 v1

--- Angular2+/The Complete Angular Course - Beginner to Advanced/sec_6/hello-world/src/app/directive/input-format.directive.ts
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_6/hello-world/src/app/directive/input-format.directive.ts
@@ -1,9 +1,17 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appInputFormat]'
 })
 export class InputFormatDirective {
+
+	@HostListener('focus') onFocus() {
+		console.log('on Focus!');
+	}
+
+	@HostListener('blur') onBlur() {
+		console.log('on Blur!');
+	}
 
   constructor() { }
 
