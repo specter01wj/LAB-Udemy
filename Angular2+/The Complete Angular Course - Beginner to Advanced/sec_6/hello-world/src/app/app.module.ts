2e1c9158d5e1344322331d3dd9db2931ea643dca
specter01wj <specter01wj@gmail.com>
2019-10-15 13:32:32 -0400

    10/15/2019 v1

--- Angular2+/The Complete Angular Course - Beginner to Advanced/sec_6/hello-world/src/app/app.module.ts
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_6/hello-world/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { FavoriteComponent } from './favorite/favorite.component';
 import { PanelComponent } from './panel/panel.component';
 import { LikeComponent } from './like/like.component';
 import { InputFormatDirective } from './directive/input-format.directive';
+import { ZippyComponent } from './zippy/zippy.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { InputFormatDirective } from './directive/input-format.directive';
     FavoriteComponent,
     PanelComponent,
     LikeComponent,
-    InputFormatDirective
+    InputFormatDirective,
+    ZippyComponent
   ],
   imports: [
     BrowserModule,
