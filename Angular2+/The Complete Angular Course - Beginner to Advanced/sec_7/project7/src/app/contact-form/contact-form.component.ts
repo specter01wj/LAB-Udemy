e2a5f6ec9dfc3c470f028370af44b526d4b9c35e
specter01wj <specter01wj@gmail.com>
2019-10-16 02:55:08 -0400

    10/15/2019 v1

---
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_7/project7/src/app/contact-form/contact-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-form',
+  templateUrl: './contact-form.component.html',
+  styleUrls: ['./contact-form.component.less']
+})
+export class ContactFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
