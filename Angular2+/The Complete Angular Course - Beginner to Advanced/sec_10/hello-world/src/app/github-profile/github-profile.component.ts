8c25b03208b1eb95314278874a9fe6550dad84df
specter01wj <specter01wj@gmail.com>
2019-10-21 19:43:50 -0400

    10/15/2019 v1

--- Angular2+/The Complete Angular Course - Beginner to Advanced/sec_10/hello-world/src/app/github-profile/github-profile.component.ts
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_10/hello-world/src/app/github-profile/github-profile.component.ts
@@ -11,12 +11,15 @@ export class GithubProfileComponent implements OnInit {
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
-  	this.route.paramMap
+  	/*this.route.paramMap
   		.subscribe(params => {
   			// console.log(params);
   			let id = +params.get('id');
   			console.log(id);
-  		});
+  		});*/
+  		// console.log("Github profile, oninit!");
+  		let id = this.route.snapshot.paramMap.get('id');
+  		console.log(id);
   }
 
 }
