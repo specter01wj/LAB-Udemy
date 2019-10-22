c7d225871e58438f55b57980109db4256d033d6a
specter01wj <specter01wj@gmail.com>
2019-10-22 13:00:57 -0400

    10/15/2019 v1

--- Angular2+/The Complete Angular Course - Beginner to Advanced/sec_10/assignment_9/assig9-routing/src/app/app.module.ts
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_10/assignment_9/assig9-routing/src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ArchiveComponent } from './archive/archive.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ArchiveComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'archive/:year/:month', component: ArchiveComponent },
+      { path: '**', component: NotFoundComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
