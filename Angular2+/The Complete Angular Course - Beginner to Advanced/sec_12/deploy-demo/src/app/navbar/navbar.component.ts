826d3a1fd54e73f47bd1806d76f10c30ece377e0
specter01wj <specter01wj@gmail.com>
2019-10-23 03:12:02 -0400

    10/15/2019 v1

--- Angular2+/The Complete Angular Course - Beginner to Advanced/sec_12/deploy-demo/src/app/navbar/navbar.component.ts
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_12/deploy-demo/src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from './../../environments/environment';
 
 @Component({
   selector: 'navbar',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+	backgroundColor = environment.navBarBackgroundColor;
 
   constructor() { }
 
