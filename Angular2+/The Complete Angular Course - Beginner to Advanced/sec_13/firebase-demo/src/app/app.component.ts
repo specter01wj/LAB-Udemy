23cac0852b3c9a006ba491362fd6dd85820833f9
specter01wj <specter01wj@gmail.com>
2019-10-25 13:14:21 -0400

    10/15/2019 v1

--- Angular2+/The Complete Angular Course - Beginner to Advanced/sec_13/firebase-demo/src/app/app.component.ts
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_13/firebase-demo/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'firebase-demo';
+  courses: any[];
+
+  constructor(db: AngularFireDatabase) {
+  	db.list('/courses').valueChanges()
+  		.subscribe(courses => {
+  			this.courses = courses;
+  			console.log(this.courses);
+  		});
+  }
+
 }
--- Angular2+/The Complete Angular Course - Beginner to Advanced/sec_13/firebase-demo/src/app/app.module.ts
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_13/firebase-demo/src/app/app.module.ts
@@ -1,5 +1,8 @@
+import { environment } from './../environments/environment';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
