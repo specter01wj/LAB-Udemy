fa9d46f653232e5ab7a86c3eeced22e0ef95a334
specter01wj <specter01wj@gmail.com>
2019-10-25 14:53:00 -0400

    10/15/2019 v1

--- Angular2+/The Complete Angular Course - Beginner to Advanced/sec_13/firebase-demo/src/app/app.component.ts
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_13/firebase-demo/src/app/app.component.ts
@@ -1,21 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'firebase-demo';
   courses: any[];
+  subscription: Subscription;
 
   constructor(db: AngularFireDatabase) {
-  	db.list('/courses').valueChanges()
+  	this.subscription = db.list('/courses').valueChanges()
   		.subscribe(courses => {
   			this.courses = courses;
   			console.log(this.courses);
   		});
+  }
+
+  ngOnDestroy() {
+  	this.subscription.unsubscribe();
   }
 
 }
