05ebe2a7a6665f9304653988bf80c0c245b3b784
specter01wj <specter01wj@gmail.com>
2019-10-28 13:06:35 -0400

    10/15/2019 v1

--- Angular2+/The Complete Angular Course - Beginner to Advanced/sec_20/oshop-getting-started-complete/oshop/src/app/bs-navbar/bs-navbar.component.ts
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_20/oshop-getting-started-complete/oshop/src/app/bs-navbar/bs-navbar.component.ts
@@ -1,5 +1,7 @@
+import { Observable } from 'rxjs/Observable';
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth'; 
+import * as firebase from 'firebase'; 
 
 @Component({
   selector: 'bs-navbar',
@@ -8,8 +10,13 @@ import { AngularFireAuth } from 'angularfire2/auth';
 })
 export class BsNavbarComponent implements OnInit {
 
+	// user: firebase.User;
+	user$: Observable<firebase.User>;
+
   constructor(private afAuth: AngularFireAuth) {
-  	afAuth.authState.subscribe(x => console.log(x));
+  	// afAuth.authState.subscribe(x => console.log(x));
+  	// afAuth.authState.subscribe(user => this.user = user);
+  	this.user$ = afAuth.authState;
   }
 
   ngOnInit() {
