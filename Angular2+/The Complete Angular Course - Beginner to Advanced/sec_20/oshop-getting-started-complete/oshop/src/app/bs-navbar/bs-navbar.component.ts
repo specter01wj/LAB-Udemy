98c1e13e481783d254e0c51cc0abf0714f492e12
specter01wj <specter01wj@gmail.com>
2019-10-28 13:24:17 -0400

    10/15/2019 v1

--- Angular2+/The Complete Angular Course - Beginner to Advanced/sec_20/oshop-getting-started-complete/oshop/src/app/bs-navbar/bs-navbar.component.ts
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_20/oshop-getting-started-complete/oshop/src/app/bs-navbar/bs-navbar.component.ts
@@ -1,7 +1,8 @@
-import { Observable } from 'rxjs/Observable';
+// import { Observable } from 'rxjs/Observable';
 import { Component, OnInit } from '@angular/core';
-import { AngularFireAuth } from 'angularfire2/auth'; 
-import * as firebase from 'firebase'; 
+import { AuthService } from './../service/auth.service'; 
+// import { AngularFireAuth } from 'angularfire2/auth'; 
+// import * as firebase from 'firebase'; 
 
 @Component({
   selector: 'bs-navbar',
@@ -11,19 +12,21 @@ import * as firebase from 'firebase';
 export class BsNavbarComponent implements OnInit {
 
 	// user: firebase.User;
-	user$: Observable<firebase.User>;
+	// user$: Observable<firebase.User>;
 
-  constructor(private afAuth: AngularFireAuth) {
+  // constructor(private afAuth: AngularFireAuth) {
+  constructor(public auth: AuthService) {
   	// afAuth.authState.subscribe(x => console.log(x));
   	// afAuth.authState.subscribe(user => this.user = user);
-  	this.user$ = afAuth.authState;
+  	// this.user$ = afAuth.authState;
   }
 
   ngOnInit() {
   }
 
   logout() {
-  	this.afAuth.auth.signOut();
+  	// this.afAuth.auth.signOut();
+  	this.auth.logout();
   }
 
 }
---
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_20/oshop-getting-started-complete/oshop/src/app/service/auth.service.ts
@@ -0,0 +1,22 @@
+import { Observable } from 'rxjs/Observable';
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth'; 
+import * as firebase from 'firebase'; 
+
+@Injectable()
+export class AuthService {
+	user$: Observable<firebase.User>;
+
+  constructor(private afAuth: AngularFireAuth) { 
+  	this.user$ = afAuth.authState;
+  }
+
+  login() { 
+    this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+  }
+
+  logout() {
+  	this.afAuth.auth.signOut();
+  }
+
+}
