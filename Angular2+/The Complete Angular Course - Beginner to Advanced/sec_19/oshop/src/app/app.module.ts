f7faef45b74cdf8225de30ee6b45a438b6753d95
specter01wj <specter01wj@gmail.com>
2019-10-27 23:28:35 -0400

    10/15/2019 v1

--- Angular2+/The Complete Angular Course - Beginner to Advanced/sec_19/oshop/src/app/app.module.ts
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_19/oshop/src/app/app.module.ts
@@ -8,8 +8,11 @@ import { AngularFireAuthModule } from 'angularfire2/auth';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+
 import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
+
 import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
