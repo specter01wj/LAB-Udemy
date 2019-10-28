c7ef7c0d55ecfb2cc9f56bb75afa5b52c91611d2
specter01wj <specter01wj@gmail.com>
2019-10-27 21:45:40 -0400

    10/15/2019 v1

--- Angular2+/The Complete Angular Course - Beginner to Advanced/sec_19/oshop/src/app/app.module.ts
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_19/oshop/src/app/app.module.ts
@@ -1,5 +1,9 @@
+import { environment } from './../environments/environment';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
