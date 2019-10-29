0c52537ad2b49e7d800331c5ae263178d179f9f5
specter01wj <specter01wj@gmail.com>
2019-10-29 03:05:55 -0400

    10/15/2019 v1

--- Angular2+/The Complete Angular Course - Beginner to Advanced/sec_20/oshop-getting-started-complete/oshop/src/app/service/auth-guard.service.ts
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_20/oshop-getting-started-complete/oshop/src/app/service/auth-guard.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Router, CanActivate } from '@angular/router';
+import { AuthService } from './auth.service';
+import 'rxjs/add/operator/map';
 
 @Injectable()
-export class AuthGuardService {
+export class AuthGuardService implements CanActivate {
 
-  constructor() { }
+  constructor(
+  	private auth: AuthService,
+  	private router: Router
+  ) { }
+
+  canActivate() {
+  	return this.auth.user$.map(user => {
+  		if(user) return true;
+  		this.router.navigate(['/login']);
+  		return false;
+  	});
+  }
 
 }
