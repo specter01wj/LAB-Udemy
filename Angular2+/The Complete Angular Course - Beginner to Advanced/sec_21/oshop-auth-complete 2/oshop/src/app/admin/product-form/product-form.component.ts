23493d6150d1c6f2a166f3310117fedd6445787b
specter01wj <specter01wj@gmail.com>
2019-10-31 03:40:42 -0400

    10/15/2019 v1

---
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_21/oshop-auth-complete 2/oshop/src/app/admin/product-form/product-form.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { CategoryService } from './../../category.service';
+
+@Component({
+  selector: 'app-product-form',
+  templateUrl: './product-form.component.html',
+  styleUrls: ['./product-form.component.css']
+})
+export class ProductFormComponent implements OnInit {
+
+	categories$;
+
+  constructor(categoryService: CategoryService) {
+  	this.categories$ = categoryService.getCategories();
+  }
+
+  ngOnInit() {
+  }
+
+}
--- Angular2+/The Complete Angular Course - Beginner to Advanced/sec_21/oshop-auth-complete/oshop/src/app/category.service.ts
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_21/oshop-auth-complete/oshop/src/app/category.service.ts
@@ -1,17 +1,30 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
+// import { AngularFireDatabase } from '@angular/fire/database';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class CategoryService {
 
-  constructor(private db: AngularFireDatabase) { }
+	categories$: Observable<any[]>;
+
+  constructor(private db: AngularFireDatabase) { 
+  	this.categories$ = this.db.object('/categories').snapshotChanges();
+  }
 
   getCategories() {
+ 		// return this.observableCategories$;
+ 	return this.categories$;
+  }
+
+  /*public getCategories() {
   	return this.db.list('/categories', {
   		query: {
   			orderByChild: 'name'
   		}
   	});
+  	return this.db.list('/categories', ref => ref.orderByChild('name')).snapshotChanges();
   }
-
+*/
 }
