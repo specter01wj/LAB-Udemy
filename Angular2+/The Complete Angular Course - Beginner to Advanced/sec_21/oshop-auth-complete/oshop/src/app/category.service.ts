daf90cdd00319fc03ce4653b9babc3ba939d0a50
specter01wj <specter01wj@gmail.com>
2019-10-31 03:41:17 -0400

    10/15/2019 v1

--- Angular2+/The Complete Angular Course - Beginner to Advanced/sec_21/oshop-auth-complete/oshop/src/app/category.service.ts
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_21/oshop-auth-complete/oshop/src/app/category.service.ts
@@ -1,30 +1,32 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
 // import { AngularFireDatabase } from '@angular/fire/database';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map';
+// import { Observable } from 'rxjs/Observable';
+// import 'rxjs/add/operator/map';
+import { FirebaseListObservable } from 'angularfire2/database';
+import { Subscription } from 'rxjs/Subscription';
 
 @Injectable()
 export class CategoryService {
 
-	categories$: Observable<any[]>;
+	// categories$: Observable<any[]>;
 
   constructor(private db: AngularFireDatabase) { 
-  	this.categories$ = this.db.object('/categories').snapshotChanges();
+  	// this.categories$ = this.db.object('/categories').snapshotChanges();
   }
 
-  getCategories() {
+  /*getCategories() {
  		// return this.observableCategories$;
  	return this.categories$;
-  }
+  }*/
 
-  /*public getCategories() {
+  getCategories() {
   	return this.db.list('/categories', {
   		query: {
   			orderByChild: 'name'
   		}
   	});
-  	return this.db.list('/categories', ref => ref.orderByChild('name')).snapshotChanges();
+  	// return this.db.list('/categories', ref => ref.orderByChild('name')).snapshotChanges();
   }
-*/
+
 }
