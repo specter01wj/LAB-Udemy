0265bf008ea31bd319b04d92b335012f411ab2d3
specter01wj <specter01wj@gmail.com>
2019-11-02 03:49:50 -0400

    10/15/2019 v1

--- Angular2+/The Complete Angular Course - Beginner to Advanced/sec_21/oshop-auth-complete/oshop_step8_datatableComponent/src/app/admin/admin-products/admin-products.component.ts
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_21/oshop-auth-complete/oshop_step8_datatableComponent/src/app/admin/admin-products/admin-products.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProductService } from './../../product.service';
 import { Product } from './../../models/product';
 import { Subscription } from 'rxjs/Subscription';
+import { DataTableResource } from 'angular-4-data-table';
 
 @Component({
   selector: 'app-admin-products',
@@ -15,11 +16,28 @@ export class AdminProductsComponent implements OnInit, OnDestroy {
 	products: Product[];
 	subscription: Subscription;
 	filteredProducts: any[];
+  tableResource: DataTableResource<Product>;
+  items: Product[] = [];
+  itemCount: number;
 
   constructor(private productService: ProductService) { 
   	this.subscription = this.productService.getAll().subscribe(
-  		products => this.filteredProducts = this.products = products
-  	);
+  		products => { this.filteredProducts = this.products = products;
+
+      this.initializeTable(products);
+
+      
+  	});
+  }
+
+  private initializeTable(products: Product[]) {
+      this.tableResource = new DataTableResource(products);
+
+      this.tableResource.query({ offset: 0 })
+        .then(items => this.items = items);
+
+      this.tableResource.count()
+        .then(count => this.itemCount = count);
   }
 
   ngOnInit() {
@@ -34,6 +52,15 @@ export class AdminProductsComponent implements OnInit, OnDestroy {
   	this.filteredProducts = (query) ? 
   		this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) :
   		this.products;
+
+      this.initializeTable(this.filteredProducts);
+  }
+
+  reloadItems(params) {
+    if(!this.tableResource) return;
+
+    this.tableResource.query(params)
+        .then(items => this.items = items);
   }
 
 }
