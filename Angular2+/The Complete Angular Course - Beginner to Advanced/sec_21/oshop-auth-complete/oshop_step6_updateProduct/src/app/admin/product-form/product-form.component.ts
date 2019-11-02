14a09e5a68a79b679cb477cf262256ecdf36f0a2
specter01wj <specter01wj@gmail.com>
2019-11-01 21:27:52 -0400

    10/15/2019 v1

---
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_21/oshop-auth-complete/oshop_step6_updateProduct/src/app/admin/product-form/product-form.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { CategoryService } from './../../category.service';
+import { ProductService } from './../../product.service';
+import 'rxjs/add/operator/take';
+
+@Component({
+  selector: 'app-product-form',
+  templateUrl: './product-form.component.html',
+  styleUrls: ['./product-form.component.css']
+})
+export class ProductFormComponent implements OnInit {
+
+	categories$;
+  product = {};
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private categoryService: CategoryService,
+  	private productService: ProductService) {
+  	this.categories$ = categoryService.getCategories();
+
+    let id = this.route.snapshot.paramMap.get('id');
+    if(id) {
+      this.productService.get(id).take(1).subscribe(
+        p => this.product = p
+      );
+    }
+  }
+
+  ngOnInit() {
+  }
+
+  save(product) {
+  	// console.log(product);
+  	this.productService.create(product);
+    this.router.navigate(['/admin/products']);
+  }
+
+
+}
