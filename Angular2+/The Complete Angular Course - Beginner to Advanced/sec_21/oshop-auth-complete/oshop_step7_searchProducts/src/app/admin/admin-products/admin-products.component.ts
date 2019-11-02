73a8bf09beff7aae160b05017080d0293cd991a5
specter01wj <specter01wj@gmail.com>
2019-11-02 02:27:50 -0400

    10/15/2019 v1

--- Angular2+/The Complete Angular Course - Beginner to Advanced/sec_21/oshop-auth-complete/oshop_step7_searchProducts/src/app/admin/admin-products/admin-products.component.ts
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_21/oshop-auth-complete/oshop_step7_searchProducts/src/app/admin/admin-products/admin-products.component.ts
@@ -1,20 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProductService } from './../../product.service';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
+export class AdminProductsComponent implements OnInit, OnDestroy {
 
-	products$;
+	products: any[];
+	subscription: Subscription;
+	filteredProducts: any[];
 
   constructor(private productService: ProductService) { 
-  	this.products$ = this.productService.getAll();
+  	this.subscription = this.productService.getAll().subscribe(
+  		products => this.filteredProducts = this.products = products
+  	);
   }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy() {
+  	this.subscription.unsubscribe();
+  }
+
+  filter(query: string) {
+  	// console.log(query);
+  	this.filteredProducts = (query) ? 
+  		this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) :
+  		this.products;
   }
 
 }
