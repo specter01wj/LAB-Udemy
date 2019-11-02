4dd9d5e90b3503f9148ab3bbc340efca2934d5b6
specter01wj <specter01wj@gmail.com>
2019-11-02 02:11:31 -0400

    10/15/2019 v1

---
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_21/oshop-auth-complete/oshop_step7_searchProducts/src/app/admin/admin-products/admin-products.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from './../../product.service';
+
+@Component({
+  selector: 'app-admin-products',
+  templateUrl: './admin-products.component.html',
+  styleUrls: ['./admin-products.component.css']
+})
+export class AdminProductsComponent implements OnInit {
+
+	products$;
+
+  constructor(private productService: ProductService) { 
+  	this.products$ = this.productService.getAll();
+  }
+
+  ngOnInit() {
+  }
+
+}
