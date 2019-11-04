c6ce9cdb3bad7c72146a41c33add7f5136db58ca
specter01wj <specter01wj@gmail.com>
2019-11-03 22:13:59 -0500

    11/03/2019 v1

---
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_22/oshop-product-management-complete/oshop_step2_filterCategory/src/app/products/products.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from './../product.service';
+import { CategoryService } from './../category.service';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+
+	products$;
+	categories$;
+
+  constructor(productService: ProductService,
+  	categoryService: CategoryService) { 
+  	this.products$ = productService.getAll();
+  	this.categories$ = categoryService.getCategories();
+  }
+
+  ngOnInit() {
+  }
+
+}
