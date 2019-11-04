dd60fcf6128c6802751b67cca1c39f5ade104666
specter01wj <specter01wj@gmail.com>
2019-11-03 22:36:20 -0500

    11/03/2019 v1

--- Angular2+/The Complete Angular Course - Beginner to Advanced/sec_22/oshop-product-management-complete/oshop_step2_filterCategory/src/app/products/products.component.ts
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_22/oshop-product-management-complete/oshop_step2_filterCategory/src/app/products/products.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { ProductService } from './../product.service';
 import { CategoryService } from './../category.service';
+import { Product } from './../models/product';
 
 @Component({
   selector: 'app-products',
@@ -9,13 +11,29 @@ import { CategoryService } from './../category.service';
 })
 export class ProductsComponent implements OnInit {
 
-	products$;
+  // products$;
+	products: Product[] = [];
+  filteredProducts: Product[] = [];
 	categories$;
+  categoryBtn: string;
 
-  constructor(productService: ProductService,
+  constructor(
+    route: ActivatedRoute,
+    productService: ProductService,
   	categoryService: CategoryService) { 
-  	this.products$ = productService.getAll();
+
+    // this.products$ = productService.getAll();
+  	productService.getAll().subscribe(products => this.products = products);
+
   	this.categories$ = categoryService.getCategories();
+
+    route.queryParamMap.subscribe(params => {
+      this.categoryBtn = params.get('category');
+
+      this.filteredProducts = (this.categoryBtn) ? 
+        this.products.filter(p => p.category === this.categoryBtn) :
+        this.products;
+    });
   }
 
   ngOnInit() {
