3a659ce6c33a205e7b2646274d219b3706e670b1
specter01wj <specter01wj@gmail.com>
2019-11-06 17:46:31 -0500

    11/04/2019 v1

---
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_23/oshop-product-catalog-complete/oshop_step4_displayQuantity/src/app/shopping-cart.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Product } from './models/product';
+import 'rxjs/add/operator/take';
+
+@Injectable()
+export class ShoppingCartService {
+
+  constructor(private db: AngularFireDatabase) { }
+
+  private create() {
+  	return this.db.list('/shopping-carts').push({
+  		dateCreated: new Date().getTime()
+  	});
+  }
+
+  private getCart(cartId: string) {
+  	return this.db.object('/shopping-carts/' + cartId);
+  }
+
+  private getItem(cartId: string, productId: string) {
+    return this.db.object('/shopping-carts/' + cartId + '/item/' + productId);
+  }
+
+  private async getOrCreateCartId() {
+  	let cartId = localStorage.getItem('cartId');
+  	if(cartId) {
+  		return cartId;
+  	}
+
+	let result = await this.create();
+	localStorage.setItem('cartId', result.key);
+	return result.key;
+  }
+
+  async addToCart(product: Product) {
+  	let cartId = await this.getOrCreateCartId();
+  	let item$ = this.getItem(cartId, product.$key);
+  	item$.take(1).subscribe(item => {
+  		item$.update({ product: product, quantity: (item.quantity || 0) + 1 });
+  	});
+  }
+
+}
