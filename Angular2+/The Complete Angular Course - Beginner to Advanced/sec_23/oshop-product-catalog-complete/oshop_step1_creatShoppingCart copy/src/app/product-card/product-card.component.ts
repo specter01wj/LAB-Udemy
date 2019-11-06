ca79e3b46e375337d978af1efd8bbc4e6c499ae0
specter01wj <specter01wj@gmail.com>
2019-11-06 10:29:28 -0500

    11/04/2019 v1

---
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_23/oshop-product-catalog-complete/oshop_step1_creatShoppingCart copy/src/app/product-card/product-card.component.ts
@@ -0,0 +1,27 @@
+import { Product } from './../models/product';
+import { Component, OnInit, Input } from '@angular/core';
+import { ShoppingCartService } from './../shopping-cart.service';
+
+@Component({
+  selector: 'product-card',
+  templateUrl: './product-card.component.html',
+  styleUrls: ['./product-card.component.css']
+})
+export class ProductCardComponent {
+  @Input('product') product: Product;
+  @Input('show-actions') showActions = true;
+
+  constructor(private shoppingCartService: ShoppingCartService) { }
+
+  addToCart(product: Product) {
+  	let cardId = localStorage.getItem('cardId');
+  	if(!cardId) {
+  		this.shoppingCartService.create().then(result => {
+  			localStorage.setItem('cardId', result.key);
+  		});
+  	} else {
+  		
+  	}
+  }
+
+}
