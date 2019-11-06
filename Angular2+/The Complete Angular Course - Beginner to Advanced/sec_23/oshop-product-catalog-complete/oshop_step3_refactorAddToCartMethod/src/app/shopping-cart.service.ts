ca1cdf9cc5f1adb261476bb0877c9406997eb554
specter01wj <specter01wj@gmail.com>
2019-11-06 17:32:23 -0500

    11/04/2019 v1

--- Angular2+/The Complete Angular Course - Beginner to Advanced/sec_23/oshop-product-catalog-complete/oshop_step3_refactorAddToCartMethod/src/app/shopping-cart.service.ts
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_23/oshop-product-catalog-complete/oshop_step3_refactorAddToCartMethod/src/app/shopping-cart.service.ts
@@ -14,44 +14,30 @@ export class ShoppingCartService {
   	});
   }
 
-  private getCart(cardId: string) {
-  	return this.db.object('/shopping-carts/' + cardId);
+  private getCart(cartId: string) {
+  	return this.db.object('/shopping-carts/' + cartId);
   }
 
-/*  private async getOrCreateCart() {
-  	let cardId = localStorage.getItem('cardId');
-  	if(!cardId) {
-  		let result = await this.create();
-  		localStorage.setItem('cardId', result.key);
-  		return this.getCart(result.key);
-  	}
-  	return this.getCart(cardId);
-  }*/
+  private getItem(cartId: string, productId: string) {
+    return this.db.object('/shopping-carts/' + cartId + '/item/' + productId);
+  }
 
   private async getOrCreateCartId() {
-  	let cardId = localStorage.getItem('cardId');
-  	if(cardId) {
-  		return cardId;
+  	let cartId = localStorage.getItem('cartId');
+  	if(cartId) {
+  		return cartId;
   	}
 
 	let result = await this.create();
-	localStorage.setItem('cardId', result.key);
+	localStorage.setItem('cartId', result.key);
 	return result.key;
   }
 
   async addToCart(product: Product) {
-  	let cardId = await this.getOrCreateCartId();
-  	let item$ = this.db.object('/shopping-carts/' + cardId + '/item/' + product.$key);
+  	let cartId = await this.getOrCreateCartId();
+  	let item$ = this.getItem(cartId, product.$key);
   	item$.take(1).subscribe(item => {
-  		if(item.$exists()) {
-  			item$.update({
-  				quantity: item.quantity + 1
-  			});
-  		} else {
-  			item$.set({
-  				product: product, quantity: 1
-  			});
-  		}
+  		item$.update({ product: product, quantity: (item.quantity || 0) + 1 });
   	});
   }
 
