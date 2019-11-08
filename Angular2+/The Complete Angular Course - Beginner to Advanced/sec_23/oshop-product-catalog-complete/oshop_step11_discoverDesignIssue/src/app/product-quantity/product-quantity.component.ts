ad7d6636f599b61fb5c208070dafe6dfece372bb
specter01wj <specter01wj@gmail.com>
2019-11-08 16:55:25 -0500

    11/04/2019 v1

---
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_23/oshop-product-catalog-complete/oshop_step11_discoverDesignIssue/src/app/product-quantity/product-quantity.component.ts
@@ -0,0 +1,31 @@
+import { Product } from './../models/product';
+import { Component, OnInit, Input } from '@angular/core';
+import { ShoppingCartService } from './../shopping-cart.service';
+
+@Component({
+  selector: 'app-product-quantity',
+  templateUrl: './product-quantity.component.html',
+  styleUrls: ['./product-quantity.component.css']
+})
+export class ProductQuantityComponent implements OnInit {
+
+  ngOnInit() {
+  }
+
+  @Input('product') product: Product;
+  @Input('shopping-cart') shoppingCart;
+
+  constructor(private shoppingCartService: ShoppingCartService) { }
+
+  addToCart() {
+    this.shoppingCartService.addToCart(this.product);
+  }
+
+  removeFromCart() {
+    this.shoppingCartService.removeFromCart(this.product);
+  }
+
+
+
+
+}
