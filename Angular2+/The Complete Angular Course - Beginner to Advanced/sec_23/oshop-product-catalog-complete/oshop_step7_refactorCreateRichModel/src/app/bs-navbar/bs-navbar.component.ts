d1533ff1ebd4945f6dc9ee0a50d76e3fb150f27b
specter01wj <specter01wj@gmail.com>
2019-11-08 03:37:20 -0500

    11/04/2019 v1

---
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_23/oshop-product-catalog-complete/oshop_step7_refactorCreateRichModel/src/app/bs-navbar/bs-navbar.component.ts
@@ -0,0 +1,35 @@
+import { AppUser } from './../models/app-user';
+import { AuthService } from './../auth.service';
+import { ShoppingCartService } from './../shopping-cart.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bs-navbar',
+  templateUrl: './bs-navbar.component.html',
+  styleUrls: ['./bs-navbar.component.css']
+})
+export class BsNavbarComponent implements OnInit {
+  appUser: AppUser;
+  shoppingCartItemCount: number;
+
+  constructor(private auth: AuthService,
+    private shoppingCartService: ShoppingCartService) { 
+    
+  }
+
+  async ngOnInit() {
+    this.auth.appUser$.subscribe(appUser => this.appUser = appUser);
+    let cart$ = await this.shoppingCartService.getCart();
+    cart$.subscribe(cart => {
+      this.shoppingCartItemCount = 0;
+      for(let productId in cart.items) {
+        this.shoppingCartItemCount += cart.items[productId].quantity;
+      }
+    });
+  }
+
+  logout() {
+    this.auth.logout();
+  }
+
+}
---
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_23/oshop-product-catalog-complete/oshop_step7_refactorCreateRichModel/src/app/shopping-cart.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase, FirebaseObjectObservable } from 'angularfire2/database';
+import { Product } from './models/product';
+import { ShoppingCart } from './models/shopping-cart';
+import 'rxjs/add/operator/take';
+
+@Injectable()
+export class ShoppingCartService {
+
+  constructor(private db: AngularFireDatabase) { }
+
+  private create() {
+  	return this.db.list('/shopping-carts').push({
+  		dateCreated: new Date().getTime()
+  	});
+  }
+
+  async getCart(): Promise<FirebaseObjectObservable<ShoppingCart>> {
+    let cartId = await this.getOrCreateCartId();
+  	return this.db.object('/shopping-carts/' + cartId);
+  }
+
+  private getItem(cartId: string, productId: string) {
+    return this.db.object('/shopping-carts/' + cartId + '/items/' + productId);
+  }
+
+  private async getOrCreateCartId(): Promise<string> {
+  	let cartId = localStorage.getItem('cartId');
+  	if(cartId) {
+  		return cartId;
+  	}
+
+	let result = await this.create();
+	localStorage.setItem('cartId', result.key);
+	return result.key;
+  }
+
+  async addToCart(product: Product) {
+    this.updateItemQuantity(product, 1);
+  }
+
+  async removeFromCart(product: Product) {
+  	this.updateItemQuantity(product, -1);
+  }
+
+  private async updateItemQuantity(product: Product, change: number) {
+    let cartId = await this.getOrCreateCartId();
+    let item$ = this.getItem(cartId, product.$key);
+    item$.take(1).subscribe(item => {
+      item$.update({ product: product, quantity: (item.quantity || 0) + change });
+    });
+  }
+
+}
+
+
+
+
+
+
+
+
+
