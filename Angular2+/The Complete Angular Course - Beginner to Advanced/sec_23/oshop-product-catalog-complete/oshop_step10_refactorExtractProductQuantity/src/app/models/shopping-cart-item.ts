ad45989942e7094d63ee0bc27660cc738e691297
specter01wj <specter01wj@gmail.com>
2019-11-08 15:05:22 -0500

    11/04/2019 v1

---
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_23/oshop-product-catalog-complete/oshop_step10_refactorExtractProductQuantity/src/app/models/shopping-cart-item.ts
@@ -0,0 +1,14 @@
+import { Product } from './product';
+
+export class ShoppingCartItem {
+  // product: Product;
+  // quantity: number;
+
+  constructor(public product: Product, public quantity: number) {
+  	
+  }
+
+  get totalPrice() {
+  	return this.product.price * this.quantity;
+  }
+}
---
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_23/oshop-product-catalog-complete/oshop_step10_refactorExtractProductQuantity/src/app/models/shopping-cart.ts
@@ -0,0 +1,34 @@
+import { ShoppingCartItem } from './shopping-cart-item';
+
+export class ShoppingCart {
+	items: ShoppingCartItem[] = [];
+
+	constructor(public itemsMap: {[productId: string]: ShoppingCartItem}) {
+		for (let productId in itemsMap) {
+			// this.items.push(itemsMap[productId]);
+			let item = itemsMap[productId];
+			this.items.push(new ShoppingCartItem(item.product, item.quantity));
+		}
+	}
+
+	get totalSum() {
+		let sum = 0;
+		for (let productId in this.items) {
+			sum += this.items[productId].totalPrice;
+		}
+		return sum;
+	}
+
+	get totalItemsCount() {
+  		let count = 0;
+		for(let productId in this.itemsMap) {
+		    count += this.itemsMap[productId].quantity;
+		}
+		return count;
+  	}
+}
+
+
+
+
+
