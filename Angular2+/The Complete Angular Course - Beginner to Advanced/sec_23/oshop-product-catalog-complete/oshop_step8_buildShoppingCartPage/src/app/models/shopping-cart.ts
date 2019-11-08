a56774af3c12539d0a6433af283d4436f923ed6b
specter01wj <specter01wj@gmail.com>
2019-11-08 12:46:08 -0500

    11/04/2019 v1

--- Angular2+/The Complete Angular Course - Beginner to Advanced/sec_23/oshop-product-catalog-complete/oshop_step8_buildShoppingCartPage/src/app/models/shopping-cart.ts
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_23/oshop-product-catalog-complete/oshop_step8_buildShoppingCartPage/src/app/models/shopping-cart.ts
@@ -5,6 +5,10 @@ export class ShoppingCart {
 
 	constructor(public items: ShoppingCartItem[]) {}
 
+	get productIds() {
+		return Object.keys(this.items);
+	}
+
 	get totalItemsCount() {
   		let count = 0;
 		for(let productId in this.items) {
