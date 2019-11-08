76399f8046ca4977165c5304467dd92a1746355a
specter01wj <specter01wj@gmail.com>
2019-11-07 19:36:01 -0500

    11/04/2019 v1

---
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_23/oshop-product-catalog-complete/oshop_step5_improveCardFooter/src/app/product-card/product-card.component.ts
@@ -0,0 +1,30 @@
+import { Product } from './../models/product';
+import { Component, OnInit, Input } from '@angular/core';
+import { ShoppingCartService } from './../shopping-cart.service';
+
+@Component({
+  selector: 'product-card',
+  templateUrl: './product-card.component.html',
+  styleUrls: ['./product-card.component.css']
+})
+export class ProductCardComponent {
+  @Input('product') product: Product;
+  @Input('show-actions') showActions = true;
+  @Input('shopping-cart') shoppingCart;
+
+  constructor(private shoppingCartService: ShoppingCartService) { }
+
+  addToCart(product: Product) {
+
+    this.shoppingCartService.addToCart(product);
+  	
+  }
+
+  getQuantity() {
+    if(!this.shoppingCart) return 0;
+
+    let item = this.shoppingCart.items[this.product.$key];
+    return item ? item.quantity : 0;
+  }
+
+}
