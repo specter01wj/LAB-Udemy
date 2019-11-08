b922e7d205ef111fba36ad6190129d679ba72d7a
specter01wj <specter01wj@gmail.com>
2019-11-07 20:21:03 -0500

    11/04/2019 v1

--- Angular2+/The Complete Angular Course - Beginner to Advanced/sec_23/oshop-product-catalog-complete/oshop_step6_displayNuminNav/src/app/bs-navbar/bs-navbar.component.ts
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_23/oshop-product-catalog-complete/oshop_step6_displayNuminNav/src/app/bs-navbar/bs-navbar.component.ts
@@ -1,5 +1,6 @@
 import { AppUser } from './../models/app-user';
 import { AuthService } from './../auth.service';
+import { ShoppingCartService } from './../shopping-cart.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,11 +8,20 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent {
+export class BsNavbarComponent implements OnInit {
   appUser: AppUser;
 
-  constructor(private auth: AuthService) { 
+  constructor(private auth: AuthService,
+    private shoppingCartService: ShoppingCartService) { 
+    
+  }
+
+  async ngOnInit() {
     auth.appUser$.subscribe(appUser => this.appUser = appUser);
+    let cart$ = await this.shoppingCartService.getCart();
+    cart$.subscribe(cart => {
+      cart.items
+    });
   }
 
   logout() {
