276d85972ebf97bc4da41c6e54efc4a3f8275b34
specter01wj <specter01wj@gmail.com>
2019-11-08 13:24:41 -0500

    11/04/2019 v1

--- Angular2+/The Complete Angular Course - Beginner to Advanced/sec_23/oshop-product-catalog-complete/oshop_step9_fixDesignIssue/src/app/models/shopping-cart.ts
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_23/oshop-product-catalog-complete/oshop_step9_fixDesignIssue/src/app/models/shopping-cart.ts
@@ -3,7 +3,7 @@ import { ShoppingCartItem } from './shopping-cart-item';
 export class ShoppingCart {
 	items: ShoppingCartItem[] = [];
 
-	constructor(public itemsMap: {[productId: string]: ShoppingCartItem[]}) {
+	constructor(public itemsMap: {[productId: string]: ShoppingCartItem}) {
 		for (let productId in itemsMap) {
 			this.items.push(itemsMap[productId]);
 		}
