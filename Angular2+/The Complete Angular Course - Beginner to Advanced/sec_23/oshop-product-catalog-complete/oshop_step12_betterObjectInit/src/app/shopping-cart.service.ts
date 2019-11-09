931444350cc9ce7d6b0cb8fd6ebb99501fd9c8cc
specter01wj <specter01wj@gmail.com>
2019-11-09 03:03:09 -0500

    11/04/2019 v1

--- Angular2+/The Complete Angular Course - Beginner to Advanced/sec_23/oshop-product-catalog-complete/oshop_step12_betterObjectInit/src/app/shopping-cart.service.ts
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_23/oshop-product-catalog-complete/oshop_step12_betterObjectInit/src/app/shopping-cart.service.ts
@@ -58,6 +58,11 @@ export class ShoppingCartService {
     });
   }
 
+  async clearCart() {
+    let cartId = await this.getOrCreateCartId();
+    this.db.object('/shopping-carts/' + cartId + '/items').remove();
+  }
+
 }
 
 
