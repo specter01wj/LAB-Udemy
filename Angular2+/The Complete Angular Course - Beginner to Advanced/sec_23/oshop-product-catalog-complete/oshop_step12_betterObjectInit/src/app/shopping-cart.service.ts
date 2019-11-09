a57a2a005a3753df869fcc87abce2872f293eb0b
specter01wj <specter01wj@gmail.com>
2019-11-09 03:15:41 -0500

    11/04/2019 v1

--- Angular2+/The Complete Angular Course - Beginner to Advanced/sec_23/oshop-product-catalog-complete/oshop_step12_betterObjectInit/src/app/shopping-cart.service.ts
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_23/oshop-product-catalog-complete/oshop_step12_betterObjectInit/src/app/shopping-cart.service.ts
@@ -50,11 +50,18 @@ export class ShoppingCartService {
     let cartId = await this.getOrCreateCartId();
     let item$ = this.getItem(cartId, product.$key);
     item$.take(1).subscribe(item => {
-      item$.update({ 
-        title: product.title,
-        imageUrl: product.imageUrl,
-        price: product.price,
-        quantity: (item.quantity || 0) + change });
+      let quantity = (item.quantity || 0) + change;
+
+      if(quantity === 0) {
+        item$.remove();
+      } else {
+        item$.update({ 
+            title: product.title,
+            imageUrl: product.imageUrl,
+            price: product.price,
+            quantity: quantity
+        });
+      }
     });
   }
 
