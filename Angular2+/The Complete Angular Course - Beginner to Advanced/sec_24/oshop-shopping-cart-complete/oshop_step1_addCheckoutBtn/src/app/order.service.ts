f1f8cb5556559b2bc88019a218c6b22d1fc3f822
specter01wj <specter01wj@gmail.com>
2019-11-11 18:27:01 -0500

    11/11/2019 v1

--- Angular2+/The Complete Angular Course - Beginner to Advanced/sec_24/oshop-shopping-cart-complete/oshop_step1_addCheckoutBtn/src/app/order.service.ts
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_24/oshop-shopping-cart-complete/oshop_step1_addCheckoutBtn/src/app/order.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
 
 @Injectable()
 export class OrderService {
 
-  constructor() { }
+  constructor(private db: AngularFireDatabase) { }
+  
+  create(order) { 
+    return this.db.list('/orders').push(order);
+  }
 
 }
