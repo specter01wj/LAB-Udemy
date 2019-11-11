30c76df0c5a1f8553827b21fc378443414a2099d
specter01wj <specter01wj@gmail.com>
2019-11-11 13:17:48 -0500

    11/11/2019 v1

--- Angular2+/The Complete Angular Course - Beginner to Advanced/sec_24/oshop-shopping-cart-complete/oshop_step1_addCheckoutBtn/src/app/check-out/check-out.component.ts
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_24/oshop-shopping-cart-complete/oshop_step1_addCheckoutBtn/src/app/check-out/check-out.component.ts
@@ -1,21 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ShoppingCart } from './../models/shopping-cart';
 import { ShoppingCartService } from './../shopping-cart.service';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-check-out',
   templateUrl: './check-out.component.html',
   styleUrls: ['./check-out.component.css']
 })
-export class CheckOutComponent implements OnInit {
+export class CheckOutComponent implements OnInit, OnDestroy {
 	shipping = {}; 
+	cart: ShoppingCart;
+	subscription: Subscription;
 	
   constructor(private shoppingCartService: ShoppingCartService) { }
 
   ngOnInit() {
+  	let cart$ = await this.shoppingCartService.getCart();
+  	this.subscription = cart$.subscribe(cart => this.cart = cart);
   }
 
+  ngOnDestroy() {
+  	this.subscription.unsubscribe();
+  }
 
-  
   placeOrder() {
     // console.log(this.shipping);
     let order = {
