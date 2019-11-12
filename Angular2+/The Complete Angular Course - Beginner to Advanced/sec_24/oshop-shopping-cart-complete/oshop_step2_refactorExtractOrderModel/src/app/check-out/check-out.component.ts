c346f8000846c351fadc6b6d4936708a67d179be
specter01wj <specter01wj@gmail.com>
2019-11-12 12:57:15 -0500

    11/04/2019 v1

---
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_24/oshop-shopping-cart-complete/oshop_step2_refactorExtractOrderModel/src/app/check-out/check-out.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ShoppingCart } from './../models/shopping-cart';
+import { ShoppingCartService } from './../shopping-cart.service';
+import { OrderService } from './../order.service';
+import { AuthService } from './../auth.service';
+import { Subscription } from 'rxjs/Subscription';
+import 'rxjs/add/operator/map';
+
+@Component({
+  selector: 'app-check-out',
+  templateUrl: './check-out.component.html',
+  styleUrls: ['./check-out.component.css']
+})
+export class CheckOutComponent implements OnInit, OnDestroy {
+	shipping = {}; 
+	cart: ShoppingCart;
+	userId: string;
+	cartSubscription: Subscription;
+	userSubscription: Subscription;
+	
+  constructor(
+  	private authService: AuthService,
+  	private orderService: OrderService,
+  	private shoppingCartService: ShoppingCartService) { }
+
+  async ngOnInit() {
+  	let cart$ = await this.shoppingCartService.getCart();
+  	this.cartSubscription = cart$.subscribe(cart => this.cart = cart);
+  	this.authService.user$.subscribe(user => this.userId = user.uid);
+  }
+
+  ngOnDestroy() {
+  	this.cartSubscription.unsubscribe();
+  	this.userSubscription.unsubscribe();
+  }
+
+  placeOrder() {
+    // console.log(this.shipping);
+    let order = {
+    	userId: this.userId,
+    	datePlaced: new Date().getTime(),
+    	shipping: this.shipping,
+    	items: this.cart.items.map(i => {
+    		return {
+    			product: {
+    				title: i.title,
+    				imageUrl: i.imageUrl,
+    				price: i.price
+    			},
+    			quantity: i.quantity,
+    			totalPrice: i.totalPrice
+    		}
+    	})
+    };
+
+    this.orderService.storeOrder(order);
+  }    
+
+}
