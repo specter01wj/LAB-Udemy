eaccec61764ef8cc77922f318fb01ccf05c5583c
specter01wj <specter01wj@gmail.com>
2019-11-12 18:00:46 -0500

    11/04/2019 v1

--- Angular2+/The Complete Angular Course - Beginner to Advanced/sec_24/oshop-shopping-cart-complete/oshop_step5_addOrderSummaryWidget/src/app/app.module.ts
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_24/oshop-shopping-cart-complete/oshop_step5_addOrderSummaryWidget/src/app/app.module.ts
@@ -34,6 +34,7 @@ import { ProductFormComponent } from './admin/product-form/product-form.componen
 import { ProductFilterComponent } from './products/product-filter/product-filter.component';
 import { ProductCardComponent } from './product-card/product-card.component';
 import { ProductQuantityComponent } from './product-quantity/product-quantity.component';
+import { ShoppingCartSummaryComponent } from './shopping-cart-summary/shopping-cart-summary.component';
 
 @NgModule({
   declarations: [
@@ -51,7 +52,8 @@ import { ProductQuantityComponent } from './product-quantity/product-quantity.co
     ProductFormComponent,
     ProductFilterComponent,
     ProductCardComponent,
-    ProductQuantityComponent
+    ProductQuantityComponent,
+    ShoppingCartSummaryComponent
   ],
   imports: [
     BrowserModule,
