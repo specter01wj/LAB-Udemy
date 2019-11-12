fd4812c9367b82d130cb5c7bffcc103c8e64c3d0
specter01wj <specter01wj@gmail.com>
2019-11-12 18:45:28 -0500

    11/04/2019 v1

--- Angular2+/The Complete Angular Course - Beginner to Advanced/sec_24/oshop-shopping-cart-complete/oshop_step5_addOrderSummaryWidget/src/app/shopping-cart-summary/shopping-cart-summary.component.ts
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_24/oshop-shopping-cart-complete/oshop_step5_addOrderSummaryWidget/src/app/shopping-cart-summary/shopping-cart-summary.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ShoppingCart } from './../models/shopping-cart';
 
 @Component({
   selector: 'app-shopping-cart-summary',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-cart-summary.component.css']
 })
 export class ShoppingCartSummaryComponent implements OnInit {
+	@Input('cart') cart: ShoppingCart;
 
   constructor() { }
 
