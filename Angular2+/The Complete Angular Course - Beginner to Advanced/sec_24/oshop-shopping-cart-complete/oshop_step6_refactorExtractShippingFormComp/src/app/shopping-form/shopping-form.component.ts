8d4d09b1b6a572c35323e3220f735081ef8cd60d
specter01wj <specter01wj@gmail.com>
2019-11-12 19:25:09 -0500

    11/04/2019 v1

--- Angular2+/The Complete Angular Course - Beginner to Advanced/sec_24/oshop-shopping-cart-complete/oshop_step6_refactorExtractShippingFormComp/src/app/shopping-form/shopping-form.component.ts
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_24/oshop-shopping-cart-complete/oshop_step6_refactorExtractShippingFormComp/src/app/shopping-form/shopping-form.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { OrderService } from './../order.service';
+import { AuthService } from './../auth.service';
+import { Order } from './../models/order';
+import { ShoppingCart } from './../models/shopping-cart';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-shopping-form',
   templateUrl: './shopping-form.component.html',
   styleUrls: ['./shopping-form.component.css']
 })
-export class ShoppingFormComponent implements OnInit {
+export class ShoppingFormComponent implements OnInit, OnDestroy {
+  @Input('cart') cart: ShoppingCart;
 
-  constructor() { }
+	shipping = {}; 
+	userId: string;
+	userSubscription: Subscription;
 
-  ngOnInit() {
+  constructor(private router: Router,
+    private authService: AuthService,
+    private orderService: OrderService) { }
+
+  async ngOnInit() {
+  	this.userSubscription = this.authService.user$.subscribe(user => this.userId = user.uid);
   }
+
+  ngOnDestroy() {
+  	this.userSubscription.unsubscribe();
+  }
+
+  async placeOrder() {
+    
+    let order = new Order(this.userId, this.shipping, this.cart);
+    let result = await this.orderService.storeOrder(order);
+    this.router.navigate(['/order-success', result.key]);
+  }    
 
 }
