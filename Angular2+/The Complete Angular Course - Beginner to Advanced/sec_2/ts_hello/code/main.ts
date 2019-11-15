347311d2b464d4ad5d49291afe11b296ec1e3fdd
specter01wj <specter01wj@gmail.com>
2019-11-15 11:12:56 -0800

    11/15/2019 v1

--- Angular2+/The Complete Angular Course - Beginner to Advanced/sec_2/ts_hello/code/main.ts
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_2/ts_hello/code/main.ts
@@ -4,4 +4,5 @@ import { LikeComponent } from './like.component';
 let component = new LikeComponent(10, true);
 
 component.onClick();
+
 console.log(`likesCount: ${component.likesCount}, isSelected: ${component.isSelected}`);
