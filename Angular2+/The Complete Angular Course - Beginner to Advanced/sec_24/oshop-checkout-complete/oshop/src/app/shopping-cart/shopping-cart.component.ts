612c15985dcfa958161d35126f4c8c46535c3728
specter01wj <specter01wj@gmail.com>
2019-11-17 10:36:43 -0800

    11/04/2019 v1

--- Angular2+/The Complete Angular Course - Beginner to Advanced/sec_24/oshop-checkout-complete/oshop/src/app/shopping-cart/shopping-cart.component.ts
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_24/oshop-checkout-complete/oshop/src/app/shopping-cart/shopping-cart.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './shopping-cart.component.html',
   styleUrls: ['./shopping-cart.component.css']
 })
+
 export class ShoppingCartComponent implements OnInit {
   cart$;
 
