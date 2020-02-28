1efc329b524c9ef38db4da659be453fcf17e7687
specter01wj <specter01wj@gmail.com>
2020-02-28 18:53:54 -0500

    02/28/2020 v1

--- Angular2+/The Complete Angular Course - Beginner to Advanced/sec_9/project9/src/app/posts/posts.component.ts
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_9/project9/src/app/posts/posts.component.ts
@@ -24,10 +24,30 @@ export class PostsComponent implements OnInit {
 
   createPost(input: HTMLInputElement) {
   	let post = { title: input.value };
+  	input.value = '';
   	this.http.post(this.url, JSON.stringify(post))
+  		.subscribe(response => {
+  			post['id'] = response.id;
+  			this.posts.splice(0, 0, post);
+  			// console.log(response);
+  		});
+  }
+
+  updatePost(post) {
+  	this.http.patch(this.url + '/' + post.id, JSON.stringify({ isRead: true }))
   		.subscribe(response => {
   			console.log(response);
   		});
   }
+
+  deletePost(post) {
+  	this.http.delete(this.url + '/' + post.id)
+  		.subscribe(response => {
+  			let index = this.posts.indexOf(post);
+  			this.posts.splice(index, 1);
+  		});
+  }
+
+  
 
 }
