5b4eb840bd55b5182bcaa63fa611af71d3f3e3d2
specter01wj <specter01wj@gmail.com>
2020-02-28 13:53:41 -0500

    02/28/2020 v1

--- Angular2+/The Complete Angular Course - Beginner to Advanced/sec_9/project9/src/app/posts/posts.component.ts
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_9/project9/src/app/posts/posts.component.ts
@@ -1,13 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.less']
 })
 export class PostsComponent implements OnInit {
+	posts: any;
 
-  constructor() { }
+  constructor(http: HttpClient) { 
+  	http.get('http://jsonplaceholder.typicode.com/posts')
+  		.subscribe(response => {
+  			// console.log(response);
+  			this.posts = response;
+  		});
+  }
 
   ngOnInit() {
   }
