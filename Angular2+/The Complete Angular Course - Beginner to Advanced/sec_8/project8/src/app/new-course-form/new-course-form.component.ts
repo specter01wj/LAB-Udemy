afabef8242c0d19912da2ff959dd7a1bc264c2f9
specter01wj <specter01wj@gmail.com>
2020-02-27 21:09:09 -0500

    02/27/2020 v1

--- Angular2+/The Complete Angular Course - Beginner to Advanced/sec_8/project8/src/app/new-course-form/new-course-form.component.ts
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_8/project8/src/app/new-course-form/new-course-form.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormGroup, FormArray, FormControl, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-new-course-form',
   templateUrl: './new-course-form.component.html',
   styleUrls: ['./new-course-form.component.less']
 })
 export class NewCourseFormComponent implements OnInit {
+
+	form = new FormGroup({
+		topics: new FormArray([])
+	});
 
   constructor() { }
 
