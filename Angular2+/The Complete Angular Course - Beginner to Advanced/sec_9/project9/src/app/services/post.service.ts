7a506a8b63793716b65724656ce2ec3ca01991d9
specter01wj <specter01wj@gmail.com>
2020-02-29 20:13:31 -0500

    02/29/2020 v1

--- Angular2+/The Complete Angular Course - Beginner to Advanced/sec_9/project9/src/app/services/post.service.ts
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_9/project9/src/app/services/post.service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@angular/core';
 
 import { HttpClient } from '@angular/common/http';
+import { AppError } from '../common/app-error';
+import { NotFoundError } from '../common/not-found-error';
+
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/catch';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
-	private url = 'http://jsonplaceholder.typicode.com/posts';
+	  private url = 'http://jsonplaceholder.typicode.com/posts';
 
   	constructor(private http: HttpClient) {}
 
@@ -14,17 +19,27 @@ export class PostService {
   		return this.http.get(this.url);
   	}
 
-	createPost(post) {
+	  createPost(post) {
   		return this.http.post(this.url, JSON.stringify(post));
   	}
 
-	updatePost(post) {
+	  updatePost(post) {
   		return this.http.patch(this.url + '/' + post.id, JSON.stringify({ isRead: true }));
   	}
 
-	deletePost(post) {
+	  /*deletePost(post) {
   		return this.http.delete(this.url + '/' + post.id);
-  	}
+  	}*/
+
+    deletePost(post) {
+      return this.http.delete(this.url + '/' + post.id)
+        .catch((error: Response) => {
+          if(error.status === 404) {
+            return Observable.throw(new NotFoundError());
+          }
+          return Observable.throw(new AppError(error));
+        });
+    }
 
 
 }
