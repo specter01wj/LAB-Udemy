4598d4e2d48de49ca4d1d5d21716250527e1bf82
specter01wj <specter01wj@gmail.com>
2020-03-07 00:43:24 -0500

    03/04/2020 v1

--- Angular2+/The Complete Angular Course - Beginner to Advanced/sec_9/assignment_8/assignment-a8/src/app/app.module.ts
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_9/assignment_8/assignment-a8/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GithubFollowersComponent } from './github-followers/github-followers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GithubFollowersComponent
   ],
   imports: [
     BrowserModule,
