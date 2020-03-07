59c58caa96b7c81a4791c945fc19b544e45728c6
specter01wj <specter01wj@gmail.com>
2020-03-07 00:50:41 -0500

    03/04/2020 v1

--- Angular2+/The Complete Angular Course - Beginner to Advanced/sec_9/assignment_8/assignment-a8/src/app/github-followers/github-followers.component.ts
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_9/assignment_8/assignment-a8/src/app/github-followers/github-followers.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { GithubFollowersService } from './../services/github-followers.service';
+
 @Component({
   selector: 'app-github-followers',
   templateUrl: './github-followers.component.html',
   styleUrls: ['./github-followers.component.less']
 })
 export class GithubFollowersComponent implements OnInit {
+  followers: any[];
 
-  constructor() { }
+  constructor(private service: GithubFollowersService) { }
 
   ngOnInit() {
+  	this.service.getAll()
+      .subscribe(followers => this.followers = followers);
   }
 
 }
--- Angular2+/The Complete Angular Course - Beginner to Advanced/sec_9/assignment_8/assignment-a8/src/app/services/data.service.ts
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_9/assignment_8/assignment-a8/src/app/services/data.service.ts
@@ -1,5 +1,15 @@
 import { Injectable } from '@angular/core';
 
+import { BadInput } from './../common/bad-input';
+import { NotFoundError } from './../common/not-found-error';
+import { AppError } from './../common/app-error';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/observable/throw';
+
 @Injectable({
   providedIn: 'root'
 })
