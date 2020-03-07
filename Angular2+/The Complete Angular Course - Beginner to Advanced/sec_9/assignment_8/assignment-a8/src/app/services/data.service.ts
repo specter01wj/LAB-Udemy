bd14862a4f89640f0dc9a136b0ef0ad8f9626d6e
specter01wj <specter01wj@gmail.com>
2020-03-07 01:06:22 -0500

    03/04/2020 v1

--- Angular2+/The Complete Angular Course - Beginner to Advanced/sec_9/assignment_8/assignment-a8/src/app/services/data.service.ts
+++ Angular2+/The Complete Angular Course - Beginner to Advanced/sec_9/assignment_8/assignment-a8/src/app/services/data.service.ts
@@ -3,17 +3,60 @@ import { Injectable } from '@angular/core';
 import { BadInput } from './../common/bad-input';
 import { NotFoundError } from './../common/not-found-error';
 import { AppError } from './../common/app-error';
-import { Http } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/toPromise';
-import 'rxjs/add/observable/throw';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map, take, catchError, delay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  constructor(private url: string, private http: HttpClient) { }
+
+  getAll() {
+    return this.http.get(this.url)
+    	.pipe(
+	    	map(response => response),
+	      	catchError(this.handleError)
+    	);
+  }
+
+  create(resource) {
+    return this.http.post(this.url, JSON.stringify(resource))
+      	.pipe(
+	    	map(response => response),
+	      	catchError(this.handleError)
+    	);
+  }
+
+  update(resource) {
+    return this.http.patch(this.url + '/' + resource.id, JSON.stringify({ isRead: true }))
+      	.pipe(
+	    	map(response => response),
+	      	catchError(this.handleError)
+    	);
+  }
+
+  delete(id) {
+    return this.http.delete(this.url + '/' + id)
+      /*.map(response => response.json())
+      .toPromise()
+      .catch(this.handleError);*/
+      	.pipe(
+	    	map(response => response),
+	      	catchError(this.handleError)
+    	);
+  }
+
+  private handleError(error: Response) {
+    if (error.status === 400)
+      return Observable.throw(new BadInput(error.json()));
+  
+    if (error.status === 404)
+      return Observable.throw(new NotFoundError());
+    
+    return Observable.throw(new AppError(error));
+  }
+
 }
