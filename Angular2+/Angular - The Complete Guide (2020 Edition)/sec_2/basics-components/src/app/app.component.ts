bedf09f4a99aef9402b352afb689dbb5a734c78b
specter01wj <specter01wj@gmail.com>
2020-07-27 23:46:27 -0400

    06/04/2020 v1

--- Angular2+/Angular - The Complete Guide (2020 Edition)/sec_2/basics-components/src/app/app.component.ts
+++ Angular2+/Angular - The Complete Guide (2020 Edition)/sec_2/basics-components/src/app/app.component.ts
@@ -3,12 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css']
-  styles: [`
+  styleUrls: ['./app.component.css']
+  /*styles: [`
     h3 {
       color: dodgerblue;
     }
-  `]
+  `]*/
 })
 export class AppComponent {
 }
