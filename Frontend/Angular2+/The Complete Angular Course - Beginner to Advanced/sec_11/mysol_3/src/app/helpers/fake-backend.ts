78c10334bd91ca6391087bf1e96e04b494944074
Jin Wang <specter01wj@gmail.com>
2022-01-16 20:08:58 -0500

    v11

--- Frontend/Angular2+/The Complete Angular Course - Beginner to Advanced/sec_11/mysol_3/src/app/helpers/fake-backend.ts
+++ Frontend/Angular2+/The Complete Angular Course - Beginner to Advanced/sec_11/mysol_3/src/app/helpers/fake-backend.ts
@@ -5,7 +5,8 @@ export function fakeBackendFactory(
     backend: MockBackend,
     options: BaseRequestOptions) {
 
-  let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6Ik1vc2ggSGFtZWRhbmkiLCJhZG1pbiI6dHJ1ZX0.iy8az1ZDe-_hS8GLDKsQKgPHvWpHl0zkQBqy1QIPOkA';
+  let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkphbWVzIFdhbmciLCJpYXQiOjE1MTYyMzkwMjIsImFkbWluIjp0cnVlfQ.MbGCmOb7DVWhoOmBXFZWLWfMz23uwqPA-mLG1nJp32c';
+  // let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkphbWVzIFdhbmciLCJpYXQiOjE1MTYyMzkwMjIsImFkbWluIjpmYWxzZX0.UkTiXLr13rXzJcLVw_eP97hWoezmBSqmmLUIUnVqswA';
 
   backend.connections.subscribe((connection: MockConnection) => {
     // We are using the setTimeout() function to simulate an
