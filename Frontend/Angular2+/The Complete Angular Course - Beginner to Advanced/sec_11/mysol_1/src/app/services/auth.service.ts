f6974703e031e44e2ac68319174b5a78e7ba43c5
Jin Wang <specter01wj@gmail.com>
2022-01-16 19:05:04 -0500

    v2

--- Frontend/Angular2+/The Complete Angular Course - Beginner to Advanced/sec_11/mysol_1/src/app/services/auth.service.ts
+++ Frontend/Angular2+/The Complete Angular Course - Beginner to Advanced/sec_11/mysol_1/src/app/services/auth.service.ts
@@ -1,20 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class AuthService {
   constructor(private http: Http) {
   }
 
-  login(credentials) { 
-   return this.http.post('/api/authenticate', 
-      JSON.stringify(credentials));
+  login(credentials) {
+   return this.http.post('/api/authenticate',
+      JSON.stringify(credentials))
+      .map(response => {
+
+      });
   }
 
-  logout() { 
+  logout() {
   }
 
-  isLoggedIn() { 
+  isLoggedIn() {
     return false;
   }
 }
