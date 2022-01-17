629b4d0d939886d56bef38cb74df3898be02dc02
Jin Wang <specter01wj@gmail.com>
2022-01-16 20:52:18 -0500

    v16

--- Frontend/Angular2+/The Complete Angular Course - Beginner to Advanced/sec_11/mysol_5/src/app/services/auth-guard.service.ts
+++ Frontend/Angular2+/The Complete Angular Course - Beginner to Advanced/sec_11/mysol_5/src/app/services/auth-guard.service.ts
@@ -15,7 +15,7 @@ export class AuthGuardService implements CanActivate {
       return true;
     }
 
-    this.router.navigate(['/login']);
+    this.router.navigate(['/login'], {queryParams: {returnUrl: state.url}});
     return false;
   }
 
