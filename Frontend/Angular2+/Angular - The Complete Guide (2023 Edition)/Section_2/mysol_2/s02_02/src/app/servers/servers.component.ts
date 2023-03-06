4705dbfadda089fd125ad1bc4164b739bdcc9498
Jin Wang <specter01wj@gmail.com>
2023-03-05 23:58:11 -0500

    v20

--- Frontend/Angular2+/Angular - The Complete Guide (2023 Edition)/Section_2/mysol_2/s02_02/src/app/servers/servers.component.ts
+++ Frontend/Angular2+/Angular - The Complete Guide (2023 Edition)/Section_2/mysol_2/s02_02/src/app/servers/servers.component.ts
@@ -29,16 +29,5 @@ export class ServersComponent implements OnInit {
 
   onUpdateServerName(event: Event) {
     this.serverName = (<HTMLInputElement>event.target).value;
-
-
-
-
-
-
-
-
-
-
-
   }
 }
