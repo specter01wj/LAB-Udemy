5604acea2223581b7812dee12def7a64d9a7d5a4
Jin Wang <specter01wj@gmail.com>
2023-03-06 01:40:25 -0500

    v6

--- Frontend/Angular2+/Angular - The Complete Guide (2023 Edition)/Section_2/mysol_3/s02_03/src/app/servers/servers.component.ts
+++ Frontend/Angular2+/Angular - The Complete Guide (2023 Edition)/Section_2/mysol_3/s02_03/src/app/servers/servers.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.scss']
 })
-export class ServersComponent {
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'Testserver';
+  serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
 
+  constructor() { }
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
