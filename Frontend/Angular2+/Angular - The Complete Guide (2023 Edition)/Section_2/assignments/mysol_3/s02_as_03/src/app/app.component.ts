c99f3afaa29cbc7e0a72a6b302ffc50fe44b8bb6
Jin Wang <specter01wj@gmail.com>
2023-03-06 02:53:42 -0500

    v14

--- Frontend/Angular2+/Angular - The Complete Guide (2023 Edition)/Section_2/assignments/mysol_3/s02_as_03/src/app/app.component.ts
+++ Frontend/Angular2+/Angular - The Complete Guide (2023 Edition)/Section_2/assignments/mysol_3/s02_as_03/src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 's02_as_03';
+  showSecret = false;
+  log: number[] = [1,2];
+
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    this.log.push(this.log.length + 1);
+  }
 }
