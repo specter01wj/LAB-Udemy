14ed8a86db895f4dded66a5ba64c7f61f13974bb
Jin Wang <specter01wj@gmail.com>
2023-03-09 18:04:23 -0500

    v7

--- Frontend/Angular2+/Angular - The Complete Guide (2023 Edition)/Section_5/mysol_1/s05_01/src/app/app.module.ts
+++ Frontend/Angular2+/Angular - The Complete Guide (2023 Edition)/Section_5/mysol_1/s05_01/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CockpitComponent
   ],
   imports: [
     BrowserModule,
