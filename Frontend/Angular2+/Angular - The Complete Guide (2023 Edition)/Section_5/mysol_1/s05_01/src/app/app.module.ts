c3df3ae4fb648023d2c316f8c38d6d098bf3d26f
Jin Wang <specter01wj@gmail.com>
2023-03-25 18:20:01 -0400

    v1

--- Frontend/Angular2+/Angular - The Complete Guide (2023 Edition)/Section_5/mysol_1/s05_01/src/app/app.module.ts
+++ Frontend/Angular2+/Angular - The Complete Guide (2023 Edition)/Section_5/mysol_1/s05_01/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { ServerElementComponent } from './server-element/server-element.componen
     BrowserModule,
     FormsModule,
     AppRoutingModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
