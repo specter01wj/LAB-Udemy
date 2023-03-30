557235b1c2b08f3e0bddcd8742f59d13aa3cd2b7
Jin Wang <specter01wj@gmail.com>
2023-03-29 23:28:39 -0400

    v2

--- Frontend/Angular2+/Angular University/Angular Testing Masterclass/Section_1/mysol_1/ags_test_s1/src/app/courses/services/calculator.service.spec.ts
+++ Frontend/Angular2+/Angular University/Angular Testing Masterclass/Section_1/mysol_1/ags_test_s1/src/app/courses/services/calculator.service.spec.ts
@@ -18,6 +18,7 @@ fdescribe('CalculatorService', () => {
             providers: [
                 CalculatorService,
 
+
                 {provide: LoggerService, useValue: loggerSpy}
             ]
         });
