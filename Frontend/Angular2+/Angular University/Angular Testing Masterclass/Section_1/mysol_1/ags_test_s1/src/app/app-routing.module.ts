b5944618a0ca553402e62c387081a192aa587bf9
Jin Wang <specter01wj@gmail.com>
2023-03-31 18:42:14 -0400

    v16

--- Frontend/Angular2+/Angular University/Angular Testing Masterclass/Section_1/mysol_1/ags_test_s1/src/app/app-routing.module.ts
+++ Frontend/Angular2+/Angular University/Angular Testing Masterclass/Section_1/mysol_1/ags_test_s1/src/app/app-routing.module.ts
@@ -29,22 +29,6 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
