188c66e0b86d1b8aae05e54a19fd463386696dd2
Jin Wang <specter01wj@gmail.com>
2023-03-30 23:17:56 -0400

    v1

--- Frontend/Angular2+/Angular University/Angular Testing Masterclass/Section_1/mysol_1/ags_test_s1/src/app/app-routing.module.ts
+++ Frontend/Angular2+/Angular University/Angular Testing Masterclass/Section_1/mysol_1/ags_test_s1/src/app/app-routing.module.ts
@@ -1,35 +1,34 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import {HomeComponent} from "./courses/home/home.component";
-import {AboutComponent} from "./about/about.component";
-import {CourseComponent} from "./courses/course/course.component";
-import {CourseResolver} from "./courses/services/course.resolver";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./courses/home/home.component";
+import { AboutComponent } from "./about/about.component";
+import { CourseComponent } from "./courses/course/course.component";
+import { CourseResolver } from "./courses/services/course.resolver";
 
 const routes: Routes = [
-    {
-        path: "",
-        component: HomeComponent
-
+  {
+    path: "",
+    component: HomeComponent,
+  },
+  {
+    path: "about",
+    component: AboutComponent,
+  },
+  {
+    path: "courses/:id",
+    component: CourseComponent,
+    resolve: {
+      course: CourseResolver,
     },
-    {
-        path: "about",
-        component: AboutComponent
-    },
-    {
-        path: 'courses/:id',
-        component: CourseComponent,
-        resolve: {
-            course: CourseResolver
-        }
-    },
-    {
-        path: "**",
-        redirectTo: '/'
-    }
+  },
+  {
+    path: "**",
+    redirectTo: "/",
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
