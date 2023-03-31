0b31aa8b0cde47749ead872b43cf6b3ff12e7634
Jin Wang <specter01wj@gmail.com>
2023-03-30 23:23:01 -0400

    v11

--- Frontend/Angular2+/Angular University/Angular Testing Masterclass/Section_1/mysol_1/ags_test_s1/src/app/courses/services/course.resolver.ts
+++ Frontend/Angular2+/Angular University/Angular Testing Masterclass/Section_1/mysol_1/ags_test_s1/src/app/courses/services/course.resolver.ts
@@ -1,24 +1,21 @@
-
-
-
-import {Injectable} from "@angular/core";
-import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from "@angular/router";
-import {Course} from "../model/course";
-import {Observable} from "rxjs";
-import {CoursesService} from "./courses.service";
-
-
+import { Injectable } from "@angular/core";
+import {
+  ActivatedRouteSnapshot,
+  Resolve,
+  RouterStateSnapshot,
+} from "@angular/router";
+import { Course } from "../model/course";
+import { Observable } from "rxjs";
+import { CoursesService } from "./courses.service";
 
 @Injectable()
 export class CourseResolver implements Resolve<Course> {
+  constructor(private coursesService: CoursesService) {}
 
-    constructor(private coursesService:CoursesService) {
-
-    }
-
-    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Course> {
-        return this.coursesService.findCourseById(route.params['id']);
-    }
-
+  resolve(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<Course> {
+    return this.coursesService.findCourseById(route.params["id"]);
+  }
 }
-
