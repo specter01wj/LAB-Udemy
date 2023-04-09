b53a2722312c2d664d570ccefa9f1f91443671de
Jin Wang <specter01wj@gmail.com>
2023-04-08 22:33:55 -0400

    v28

--- Frontend/Angular2+/Angular University/Angular Testing Masterclass/Section_1/mysol_1/ags_test_s1/src/app/app.module.ts
+++ Frontend/Angular2+/Angular University/Angular Testing Masterclass/Section_1/mysol_1/ags_test_s1/src/app/app.module.ts
@@ -36,16 +36,6 @@ import { CoursesModule } from "./courses/courses.module";
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-
-
-
-
-
-
-
-
-
-
     BrowserAnimationsModule,
     HttpClientModule,
     MatToolbarModule,
