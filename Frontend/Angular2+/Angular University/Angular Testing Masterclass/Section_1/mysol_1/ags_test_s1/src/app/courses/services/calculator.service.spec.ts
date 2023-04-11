c1e8e82cd9e6958a1efd0c0d9754957ee470a5a7
Jin Wang <specter01wj@gmail.com>
2023-04-10 23:53:37 -0400

    v20

--- Frontend/Angular2+/Angular University/Angular Testing Masterclass/Section_1/mysol_1/ags_test_s1/src/app/courses/services/calculator.service.spec.ts
+++ Frontend/Angular2+/Angular University/Angular Testing Masterclass/Section_1/mysol_1/ags_test_s1/src/app/courses/services/calculator.service.spec.ts
@@ -30,7 +30,6 @@ fdescribe("CalculatorService", () => {
 
   it("should subtract two numbers", () => {
     console.log("subtract test");
-
     const result = calculator.subtract(2, 2);
 
     expect(result).toBe(0, "unexpected subtraction result");
