044a2a76e06907946bd701265657a87e22cb50d9
Jin Wang <specter01wj@gmail.com>
2023-04-16 18:08:25 -0400

    v22

--- Frontend/Angular2+/Angular University/Angular Testing Masterclass/Section_1/mysol_1/ags_test_s1/src/app/courses/services/lessons.datasource.ts
+++ Frontend/Angular2+/Angular University/Angular Testing Masterclass/Section_1/mysol_1/ags_test_s1/src/app/courses/services/lessons.datasource.ts
@@ -38,8 +38,6 @@ export class LessonsDataSource implements DataSource<Lesson> {
 
   disconnect(collectionViewer: CollectionViewer): void {
     this.lessonsSubject.complete();
-
-
     this.loadingSubject.complete();
   }
 }
