0ecabe1a1f35a2467fed20c94e0cf983dc85e6e9
Jin Wang <specter01wj@gmail.com>
2023-04-17 23:55:47 -0400

    v22

--- Frontend/Angular2+/Angular University/Angular Testing Masterclass/Section_1/mysol_1/ags_test_s1/src/app/courses/services/lessons.datasource.ts
+++ Frontend/Angular2+/Angular University/Angular Testing Masterclass/Section_1/mysol_1/ags_test_s1/src/app/courses/services/lessons.datasource.ts
@@ -33,6 +33,7 @@ export class LessonsDataSource implements DataSource<Lesson> {
 
   connect(collectionViewer: CollectionViewer): Observable<Lesson[]> {
     console.log("Connecting data source");
+
     return this.lessonsSubject.asObservable();
   }
 
