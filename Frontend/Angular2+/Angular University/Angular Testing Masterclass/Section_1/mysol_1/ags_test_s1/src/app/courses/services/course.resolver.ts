7561fe02ee1cfbc2819886cb0cdf5718028c4a4e
Jin Wang <specter01wj@gmail.com>
2023-04-18 23:55:27 -0400

    v2

--- Frontend/Angular2+/Angular University/Angular Testing Masterclass/Section_1/mysol_1/ags_test_s1/src/app/courses/services/course.resolver.ts
+++ Frontend/Angular2+/Angular University/Angular Testing Masterclass/Section_1/mysol_1/ags_test_s1/src/app/courses/services/course.resolver.ts
@@ -15,6 +15,7 @@ export class CourseResolver implements Resolve<Course> {
   resolve(
     route: ActivatedRouteSnapshot,
 
+
     state: RouterStateSnapshot
   ): Observable<Course> {
     return this.coursesService.findCourseById(route.params["id"]);
