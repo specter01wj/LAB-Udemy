7cfe9da868cdf9cf3196fad21eaafade3b19cde8
Jin Wang <specter01wj@gmail.com>
2023-04-21 20:03:37 -0400

    v13

--- Frontend/Angular2+/Angular University/Angular Testing Masterclass/Section_1/mysol_1/ags_test_s1/src/app/courses/services/courses.service.ts
+++ Frontend/Angular2+/Angular University/Angular Testing Masterclass/Section_1/mysol_1/ags_test_s1/src/app/courses/services/courses.service.ts
@@ -26,19 +26,6 @@ export class CoursesService {
     filter = "",
     sortOrder = "asc",
     pageNumber = 0,
-
-
-
-
-
-
-
-
-
-
-
-
-
     pageSize = 3
   ): Observable<Lesson[]> {
     return this.http
