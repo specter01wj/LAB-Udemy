93b8780a14b467d0ff3f9db0435ae12350a0639d
Jin Wang <specter01wj@gmail.com>
2023-04-26 23:56:59 -0400

    v2

--- Frontend/Angular2+/Angular University/Angular Testing Masterclass/Section_1/mysol_1/ags_test_s1/src/app/courses/services/courses.service.ts
+++ Frontend/Angular2+/Angular University/Angular Testing Masterclass/Section_1/mysol_1/ags_test_s1/src/app/courses/services/courses.service.ts
@@ -36,6 +36,7 @@ export class CoursesService {
           .set("sortOrder", sortOrder)
           .set("pageNumber", pageNumber.toString())
 
+
           .set("pageSize", pageSize.toString()),
       })
       .pipe(map((res) => res["payload"]));
