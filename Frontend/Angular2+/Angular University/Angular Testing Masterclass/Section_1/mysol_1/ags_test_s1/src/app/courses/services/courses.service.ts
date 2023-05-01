bb3bef23e6f1ac1b1abb05f4d6bfdd4f27028f1b
Jin Wang <specter01wj@gmail.com>
2023-04-30 23:55:21 -0400

    v20

--- Frontend/Angular2+/Angular University/Angular Testing Masterclass/Section_1/mysol_1/ags_test_s1/src/app/courses/services/courses.service.ts
+++ Frontend/Angular2+/Angular University/Angular Testing Masterclass/Section_1/mysol_1/ags_test_s1/src/app/courses/services/courses.service.ts
@@ -19,6 +19,7 @@ export class CoursesService {
 
   saveCourse(courseId: number, changes: Partial<Course>): Observable<Course> {
     return this.http.put<Course>(`/api/courses/${courseId}`, changes);
+
   }
 
   findLessons(
