155f8cec264847a09510df5249af49acc6cc3676
Jin Wang <specter01wj@gmail.com>
2023-05-02 12:59:47 -0400

    v3

--- Frontend/Angular2+/Angular University/Angular for Beginners Course/Section_2/mysol_1/ags_beg_s2/src/app/app.module.ts
+++ Frontend/Angular2+/Angular University/Angular for Beginners Course/Section_2/mysol_1/ags_beg_s2/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CourseCardComponent } from './course-card/course-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseCardComponent
   ],
   imports: [
     BrowserModule,
