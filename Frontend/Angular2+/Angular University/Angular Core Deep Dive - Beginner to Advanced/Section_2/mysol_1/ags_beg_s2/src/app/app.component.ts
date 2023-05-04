7ec79c98a05d6301d3834f9c3252af9e1392deec
Jin Wang <specter01wj@gmail.com>
2023-05-04 18:23:59 -0400

    v2

---
+++ Frontend/Angular2+/Angular University/Angular Core Deep Dive - Beginner to Advanced/Section_2/mysol_1/ags_beg_s2/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, ElementRef } from '@angular/core';
+import { COURSES } from '../db-data';
+import { Course } from './model/course';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  courses = COURSES;
+
+  constructor() {}
+
+
+  onLogoClick() {
+  }
+
+  onCourseSelected(course:Course) {
+    console.log('received courses', course);
+  }
+
+
+}
