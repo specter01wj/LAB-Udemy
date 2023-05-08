499f80f5c6721d2da52b76dcba085fd2fbb4bbd6
Jin Wang <specter01wj@gmail.com>
2023-05-08 12:29:53 -0400

    v1

--- Frontend/Angular2+/Angular University/Angular Core Deep Dive - Beginner to Advanced/Section_3/mysol_1/ags_core_dp/src/app/app.component.ts
+++ Frontend/Angular2+/Angular University/Angular Core Deep Dive - Beginner to Advanced/Section_3/mysol_1/ags_core_dp/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { COURSES } from '../db-data';
 import { Course } from './model/course';
 import { CourseCardComponent } from './course-card/course-card.component';
@@ -11,14 +11,27 @@ import { CourseCardComponent } from './course-card/course-card.component';
 export class AppComponent implements AfterViewInit {
   courses = COURSES;
 
+  newCourse = {
+      id: 1,
+      description: "Angular Core Deep Dive",
+      iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-core-in-depth-small.png',
+      longDescription: "A detailed walk-through of the most important part of Angular - the Core and Common modules",
+      lessonsCount: 10
+  };
+
   @ViewChild('cardRef') card: CourseCardComponent;
 
-  @ViewChild('container') containerDiv: ElementRef;
+  // @ViewChild('container') containerDiv: ElementRef;
+
+  // @ViewChildren(CourseCardComponent) cards: QueryList<CourseCardComponent>;
+  @ViewChildren(CourseCardComponent, {read: ElementRef}) cards: QueryList<ElementRef>;
 
   constructor() {}
 
   ngAfterViewInit(): void {
-
+    this.cards.changes.subscribe(
+      cards => console.log(cards)
+    );
   }
 
 
@@ -27,8 +40,11 @@ export class AppComponent implements AfterViewInit {
 
   onCourseSelected(course:Course) {
     console.log(this.card);
-    console.log(this.containerDiv);
+    // console.log(this.containerDiv);
   }
 
+  onCoursesEdited() {
+    this.courses.push(this.newCourse);
+  }
 
 }
