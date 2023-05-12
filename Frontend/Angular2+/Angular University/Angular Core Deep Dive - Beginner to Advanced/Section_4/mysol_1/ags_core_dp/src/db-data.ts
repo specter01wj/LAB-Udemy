22fab5ab2fb88cd050145c1b7a1e82a0fe01c077
Jin Wang <specter01wj@gmail.com>
2023-05-12 18:15:07 -0400

    v6

--- Frontend/Angular2+/Angular University/Angular Core Deep Dive - Beginner to Advanced/Section_4/mysol_1/ags_core_dp/src/db-data.ts
+++ Frontend/Angular2+/Angular University/Angular Core Deep Dive - Beginner to Advanced/Section_4/mysol_1/ags_core_dp/src/db-data.ts
@@ -12,7 +12,7 @@ export const COURSES: any = [
     {
         id: 2,
         description: "RxJs In Practice Course",
-        // iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/rxjs-in-practice-course.png',
+        iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/rxjs-in-practice-course.png',
         longDescription: "Understand the RxJs Observable pattern, learn the RxJs Operators via practical examples",
         category: 'BEGINNER',
         lessonsCount: 10
