2ed5c7f51ca947da81e78ce1a6db11904b021440
Jin Wang <specter01wj@gmail.com>
2023-05-12 13:17:17 -0400

    v2

--- Frontend/Angular2+/Angular University/Angular Core Deep Dive - Beginner to Advanced/Section_4/mysol_1/ags_core_dp/src/app/app.module.ts
+++ Frontend/Angular2+/Angular University/Angular Core Deep Dive - Beginner to Advanced/Section_4/mysol_1/ags_core_dp/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CourseCardComponent } from './course-card/course-card.component';
+import { CourseImageComponent } from './course-image/course-image.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseCardComponent
+    CourseCardComponent,
+    CourseImageComponent
   ],
   imports: [
     BrowserModule,
