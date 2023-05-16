cb1ab5efad045bd8c7d674fce4164556cba40af8
Jin Wang <specter01wj@gmail.com>
2023-05-16 13:29:06 -0400

    v1

---
+++ Frontend/Angular2+/Angular University/Angular Core Deep Dive - Beginner to Advanced/Section_6/mysol_1/ags_core_dp/src/app/course-card/course-card.component.ts
@@ -0,0 +1,56 @@
+import { AfterContentInit, AfterViewInit, Component, ContentChild, ContentChildren, EventEmitter, Input, OnInit, Output, TemplateRef, ViewChild } from '@angular/core';
+import { COURSES } from '../../db-data';
+import { Course } from '../model/course';
+
+@Component({
+  selector: 'app-course-card',
+  templateUrl: './course-card.component.html',
+  styleUrls: ['./course-card.component.scss']
+})
+export class CourseCardComponent implements OnInit, AfterViewInit, AfterContentInit {
+  @Input() course: Course | undefined;
+  @Input() cardIndex: number | undefined;
+  @Input() noImageTpl: TemplateRef<any>;
+
+  @Output('courseSelected') courseEmitter = new EventEmitter<Course>();
+
+  @ContentChild('courseImage') courseImage;
+  @ContentChildren('courseImage') courseImages;
+
+  constructor() {}
+
+  ngOnInit(): void {
+
+  }
+
+  ngAfterViewInit(): void {
+    console.log(this.courseImage);
+  }
+
+  ngAfterContentInit(): void {
+    console.log(this.courseImages);
+  }
+
+  onCourseViewed() {
+    this.courseEmitter.emit(this.course);
+  }
+
+  isImageVisible() {
+    return this.course && this.course.iconUrl;
+  }
+
+  cardClasses() {
+    if (this.course.category === 'BEGINNER') {
+      return 'beginner';
+    } else {
+      return '';
+    }
+  }
+
+  cardStyles() {
+    return {
+      'background-image': 'url(' + this.course.iconUrl + ')'
+    };
+  }
+
+}
---
+++ Frontend/Angular2+/Angular University/Angular Core Deep Dive - Beginner to Advanced/Section_6/mysol_1/ags_core_dp/src/app/course-image/course-image.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { Course } from '../model/course';
+
+@Component({
+  selector: 'app-course-image',
+  templateUrl: './course-image.component.html',
+  styleUrls: ['./course-image.component.scss']
+})
+export class CourseImageComponent {
+  @Input() course: Course;
+
+}
