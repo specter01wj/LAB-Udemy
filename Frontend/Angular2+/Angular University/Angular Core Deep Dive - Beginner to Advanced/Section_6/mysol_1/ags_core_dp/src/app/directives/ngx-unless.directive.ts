0dc0da4c6de72f0907a090be1550e6d136992cf3
Jin Wang <specter01wj@gmail.com>
2023-05-23 02:26:36 -0400

    v4

--- Frontend/Angular2+/Angular University/Angular Core Deep Dive - Beginner to Advanced/Section_6/mysol_1/ags_core_dp/src/app/directives/ngx-unless.directive.ts
+++ Frontend/Angular2+/Angular University/Angular Core Deep Dive - Beginner to Advanced/Section_6/mysol_1/ags_core_dp/src/app/directives/ngx-unless.directive.ts
@@ -1,11 +1,24 @@
-import { Directive, TemplateRef, ViewContainerRef } from '@angular/core';
+import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
 
 @Directive({
   selector: '[appNgxUnless]'
 })
 export class NgxUnlessDirective {
 
+  visible: boolean = false;
+
   constructor(private templateRef: TemplateRef<any>,
               private viewContainer: ViewContainerRef) { }
+
+
+  @Input() set appNgxUnless(condition: boolean) {
+    if (!condition && !this.visible) {
+      this.viewContainer.createEmbeddedView(this.templateRef);
+      this.visible = true;
+    } else {
+      this.viewContainer.clear();
+      this.visible = false;
+    }
+  }
 
 }
