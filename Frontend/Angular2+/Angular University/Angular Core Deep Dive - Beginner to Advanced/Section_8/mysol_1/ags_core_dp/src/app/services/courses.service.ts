d8f4ae241be8561337afb6a2f60565ae053900d6
Jin Wang <specter01wj@gmail.com>
2023-05-25 13:29:42 -0400

    v1

--- Frontend/Angular2+/Angular University/Angular Core Deep Dive - Beginner to Advanced/Section_8/mysol_1/ags_core_dp/src/app/services/courses.service.ts
+++ Frontend/Angular2+/Angular University/Angular Core Deep Dive - Beginner to Advanced/Section_8/mysol_1/ags_core_dp/src/app/services/courses.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Course } from '../model/course';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  loadCourses(): Observable<Course[]> {
+    const params = new HttpParams()
+                    .set('page', 1)
+                    .set('pageSize', 10);
+
+    return this.http.get<Course[]>('/api/courses', { params });
+  }
+
+
 }
