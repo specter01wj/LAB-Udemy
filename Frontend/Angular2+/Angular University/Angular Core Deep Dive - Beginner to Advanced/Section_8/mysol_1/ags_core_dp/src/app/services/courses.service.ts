17a24ee9ef873086b64451668901e1462f94ff71
Jin Wang <specter01wj@gmail.com>
2023-05-25 18:31:23 -0400

    v8

--- Frontend/Angular2+/Angular University/Angular Core Deep Dive - Beginner to Advanced/Section_8/mysol_1/ags_core_dp/src/app/services/courses.service.ts
+++ Frontend/Angular2+/Angular University/Angular Core Deep Dive - Beginner to Advanced/Section_8/mysol_1/ags_core_dp/src/app/services/courses.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Course } from '../model/course';
@@ -19,7 +19,10 @@ export class CoursesService {
   }
 
   saveCourse(course: Course) {
-    return this.http.put(`/api/courses/${course.id}`, course);
+    const headers = new HttpHeaders()
+          .set("X-Auth", "userId");
+
+    return this.http.put(`/api/courses/${course.id}`, course, { headers });
   }
 
 
