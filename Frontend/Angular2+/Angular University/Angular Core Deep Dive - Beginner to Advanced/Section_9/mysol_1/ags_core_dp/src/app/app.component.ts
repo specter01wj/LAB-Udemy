6d1147ae6b2ce708d518ae03810b6c370cc1f62c
Jin Wang <specter01wj@gmail.com>
2023-05-26 16:15:58 -0400

    v1

--- Frontend/Angular2+/Angular University/Angular Core Deep Dive - Beginner to Advanced/Section_9/mysol_1/ags_core_dp/src/app/app.component.ts
+++ Frontend/Angular2+/Angular University/Angular Core Deep Dive - Beginner to Advanced/Section_9/mysol_1/ags_core_dp/src/app/app.component.ts
@@ -1,15 +1,25 @@
-import {AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, InjectionToken, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
 import {COURSES} from '../db-data';
 import {Course} from './model/course';
 import {CourseCardComponent} from './course-card/course-card.component';
 import {HighlightedDirective} from './directives/highlighted.directive';
 import {Observable} from 'rxjs';
 import { CoursesService } from './services/courses.service';
+import { HttpClient } from '@angular/common/http';
+
+function courseServiceProvider(http: HttpClient): CoursesService {
+  return new CoursesService(http);
+}
+
+export const COURSE_SERVICE = new InjectionToken<CoursesService>('COURSE_SERVICE');
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [
+    { provide: COURSE_SERVICE, useFactory: courseServiceProvider }
+  ]
 })
 export class AppComponent implements OnInit {
   courses$: Observable<Course[]>;
