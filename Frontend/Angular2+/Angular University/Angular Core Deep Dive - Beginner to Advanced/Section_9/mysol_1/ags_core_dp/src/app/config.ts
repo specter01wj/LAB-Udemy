476348ff9af17bd759688aeb5fa53c18e0cdef3c
Jin Wang <specter01wj@gmail.com>
2023-05-31 01:38:48 -0400

    v2

--- Frontend/Angular2+/Angular University/Angular Core Deep Dive - Beginner to Advanced/Section_9/mysol_1/ags_core_dp/src/app/config.ts
+++ Frontend/Angular2+/Angular University/Angular Core Deep Dive - Beginner to Advanced/Section_9/mysol_1/ags_core_dp/src/app/config.ts
@@ -1,21 +1,16 @@
-import {InjectionToken} from '@angular/core';
-
+import { InjectionToken } from "@angular/core";
 
 export interface AppConfig {
-    apiUrl:string;
-    courseCacheSize:number;
+  apiUrl: string;
+  courseCacheSize: number;
 }
 
+export const APP_CONFIG: AppConfig = {
+  apiUrl: "http://localhost:9000",
+  courseCacheSize: 10,
+};
 
-export const APP_CONFIG:AppConfig = {
-    apiUrl: 'http://localhost:9000',
-    courseCacheSize: 10
-}
-
-
-export const CONFIG_TOKEN =
-    new InjectionToken<AppConfig>('CONFIG_TOKEN',
-        {
-            providedIn: 'root',
-            factory: () => APP_CONFIG
-        });
+export const CONFIG_TOKEN = new InjectionToken<AppConfig>("CONFIG_TOKEN", {
+  providedIn: "root",
+  factory: () => APP_CONFIG,
+});
