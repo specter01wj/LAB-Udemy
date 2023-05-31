f520ef953cfd9a59d335a1f05ecf8e70ceb7d534
Jin Wang <specter01wj@gmail.com>
2023-05-31 18:53:08 -0400

    v3

---
+++ Frontend/Angular2+/Angular University/Angular Core Deep Dive - Beginner to Advanced/Section_10/mysol_1/ags_core_dp/src/app/app.component.ts
@@ -0,0 +1,56 @@
+import {AfterViewInit, Component, ElementRef, Inject, InjectionToken, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
+import {COURSES} from '../db-data';
+import {Course} from './model/course';
+import {CourseCardComponent} from './course-card/course-card.component';
+import {HighlightedDirective} from './directives/highlighted.directive';
+import {Observable} from 'rxjs';
+import { CoursesService } from './services/courses.service';
+import { HttpClient } from '@angular/common/http';
+import { APP_CONFIG, AppConfig, CONFIG_TOKEN } from './config';
+
+/* function courseServiceProvider(http: HttpClient): CoursesService {
+  return new CoursesService(http);
+}
+
+export const COURSE_SERVICE = new InjectionToken<CoursesService>('COURSE_SERVICE');
+ */
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [
+    /* {
+      provide: CONFIG_TOKEN, useFactory: () => APP_CONFIG
+    }, */
+    CoursesService
+    /* {
+      provide: CoursesService,
+      useClass: CoursesService,
+      // useFactory: courseServiceProvider,
+      // deps: [ HttpClient ]
+    } */
+  ]
+})
+export class AppComponent implements OnInit {
+  courses$: Observable<Course[]>;
+
+  // courses = COURSES;
+
+  // constructor(@Inject(COURSE_SERVICE)private coursesService: CoursesService) {
+  constructor(
+    private coursesService: CoursesService) {
+    // @Inject(CONFIG_TOKEN) private config: AppConfig) {
+    // console.log(config);
+  }
+
+  ngOnInit() {
+    this.courses$ = this.coursesService.loadCourses();
+  }
+
+  save(course: Course) {
+    this.coursesService.saveCourse(course).subscribe({
+      next: val => console.log(val),
+    });
+  }
+
+}
---
+++ Frontend/Angular2+/Angular University/Angular Core Deep Dive - Beginner to Advanced/Section_10/mysol_1/ags_core_dp/src/app/course-card/course-card.component.ts
@@ -0,0 +1,54 @@
+import {
+    AfterContentInit,
+    AfterViewInit,
+    Component,
+    ContentChildren,
+    ElementRef,
+    EventEmitter,
+    Inject,
+    Input,
+    OnInit,
+    Output,
+    QueryList,
+    ViewEncapsulation
+} from '@angular/core';
+import {Course} from '../model/course';
+import {CourseImageComponent} from '../course-image/course-image.component';
+import { CoursesService } from '../services/courses.service';
+// import { COURSE_SERVICE } from '../app.component';
+
+@Component({
+    selector: 'course-card',
+    templateUrl: './course-card.component.html',
+    styleUrls: ['./course-card.component.css']
+})
+export class CourseCardComponent implements OnInit {
+
+    @Input()
+    course: Course;
+
+    @Input()
+    cardIndex: number;
+
+    @Output('courseChanged')
+    courseEmitter = new EventEmitter<Course>();
+
+
+    // constructor(@Inject(COURSE_SERVICE)private coursesService: CoursesService) {
+    constructor(private coursesService: CoursesService) {
+
+    }
+
+    ngOnInit() {
+
+    }
+
+
+    onSaveClicked(description:string) {
+        this.courseEmitter.emit({...this.course, description});
+    }
+
+
+
+
+}
