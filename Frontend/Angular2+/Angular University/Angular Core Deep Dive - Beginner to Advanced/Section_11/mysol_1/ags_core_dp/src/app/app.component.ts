2b8f167336a4a66bba383a2ec1fd0885e4f6300f
Jin Wang <specter01wj@gmail.com>
2023-06-05 23:21:21 -0400

    v1

---
+++ Frontend/Angular2+/Angular University/Angular Core Deep Dive - Beginner to Advanced/Section_11/mysol_1/ags_core_dp/src/app/app.component.ts
@@ -0,0 +1,51 @@
+import {AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, DoCheck, ElementRef, Inject, InjectionToken, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
+import {COURSES} from '../db-data';
+import {Course} from './model/course';
+import {CourseCardComponent} from './course-card/course-card.component';
+import {HighlightedDirective} from './directives/highlighted.directive';
+import {Observable} from 'rxjs';
+import { CoursesService } from './services/courses.service';
+import { HttpClient } from '@angular/common/http';
+import { APP_CONFIG, AppConfig, CONFIG_TOKEN } from './config';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AppComponent implements OnInit, DoCheck {
+  // courses = COURSES;
+  // courses$: Observable<Course[]>;
+  courses: Course[];
+
+  constructor(
+    private coursesService: CoursesService,
+    private cd: ChangeDetectorRef) {
+  }
+
+  ngDoCheck(): void {
+    this.cd.markForCheck();
+  }
+
+  ngOnInit() {
+    // this.courses$ = this.coursesService.loadCourses();
+    this.coursesService.loadCourses().subscribe({
+      next: courses => {
+        this.courses = courses
+        // this.cd.markForCheck();
+      }
+    });
+  }
+
+  onEditCourse() {
+    // this.courses[0].description = 'New Value';
+  }
+
+  save(course: Course) {
+    this.coursesService.saveCourse(course).subscribe({
+      next: val => console.log(val),
+    });
+  }
+
+}
