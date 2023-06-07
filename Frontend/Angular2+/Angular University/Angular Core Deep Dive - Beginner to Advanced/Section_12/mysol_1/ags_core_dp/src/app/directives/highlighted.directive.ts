103ddda528752f61459fc143a63ef9f0d7706d1a
Jin Wang <specter01wj@gmail.com>
2023-06-07 02:43:47 -0400

    v8

---
+++ Frontend/Angular2+/Angular University/Angular Core Deep Dive - Beginner to Advanced/Section_12/mysol_1/ags_core_dp/src/app/directives/highlighted.directive.ts
@@ -0,0 +1,49 @@
+import {Directive, EventEmitter, Host, HostBinding, HostListener, Input, Output} from '@angular/core';
+import { CoursesService } from '../services/courses.service';
+
+@Directive({
+    selector: '[highlighted]',
+    exportAs: 'hl'
+})
+export class HighlightedDirective {
+
+    @Input('highlighted')
+    isHighlighted = false;
+
+    @Output()
+    toggleHighlight = new EventEmitter();
+
+    constructor(private coursesService: CoursesService) {
+
+        console.log('Directive created..');
+
+    }
+
+    @HostBinding('class.highlighted')
+    get cssClasses() {
+        return this.isHighlighted;
+    }
+
+    @HostListener('mouseover', ['$event'])
+    mouseOver($event) {
+
+        console.log($event);
+
+        this.isHighlighted = true;
+        this.toggleHighlight.emit(this.isHighlighted);
+    }
+
+    @HostListener('mouseleave')
+    mouseLeave() {
+        this.isHighlighted = false;
+        this.toggleHighlight.emit(this.isHighlighted);
+    }
+
+    toggle() {
+        this.isHighlighted = !this.isHighlighted;
+        this.toggleHighlight.emit(this.isHighlighted);
+    }
+
+
+
+}
