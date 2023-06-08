fb3b9f2a657350c29c133a930830a88827f71271
Jin Wang <specter01wj@gmail.com>
2023-06-07 23:43:31 -0400

    v1

--- Frontend/Angular2+/Angular University/Angular Core Deep Dive - Beginner to Advanced/Section_11/mysol_1/ags_core_dp/src/app/course-card/course-card.component.ts
+++ Frontend/Angular2+/Angular University/Angular Core Deep Dive - Beginner to Advanced/Section_11/mysol_1/ags_core_dp/src/app/course-card/course-card.component.ts
@@ -84,6 +84,7 @@ export class CourseCardComponent implements OnInit, OnDestroy,
 
     onSaveClicked(description:string) {
         this.courseEmitter.emit({...this.course, description});
+
     }
 
 
