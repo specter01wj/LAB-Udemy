495158370c3b57ceb899fc6bae01246107e2b77f
Jin Wang <specter01wj@gmail.com>
2023-06-08 01:56:30 -0400

    v2

--- Frontend/Angular2+/Angular University/Angular Core Deep Dive - Beginner to Advanced/Section_12/mysol_1/ags_core_dp/src/app/app.module.ts
+++ Frontend/Angular2+/Angular University/Angular Core Deep Dive - Beginner to Advanced/Section_12/mysol_1/ags_core_dp/src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CourseCardComponent } from './course-card/course-card.component';
-import { CourseImageComponent } from './course-image/course-image.component';
-import { HighlightedDirective } from './directives/highlighted.directive';
-import { NgxUnlessDirective } from './directives/ngx-unless.directive';
-import {HttpClientModule} from '@angular/common/http';
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { CourseCardComponent } from "./course-card/course-card.component";
+import { CourseImageComponent } from "./course-image/course-image.component";
+import { HighlightedDirective } from "./directives/highlighted.directive";
+import { NgxUnlessDirective } from "./directives/ngx-unless.directive";
+import { HttpClientModule } from "@angular/common/http";
+import { CoursesModule } from "./courses/courses.module";
 
 @NgModule({
   declarations: [
@@ -15,14 +16,15 @@ import {HttpClientModule} from '@angular/common/http';
     CourseCardComponent,
     CourseImageComponent,
     HighlightedDirective,
-    NgxUnlessDirective
+    NgxUnlessDirective,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    CoursesModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
