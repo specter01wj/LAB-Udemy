096128a3363b019b5b52170785cc012a4cfb6877
Jin Wang <specter01wj@gmail.com>
2023-06-09 00:01:36 -0400

    v2

--- Frontend/Angular2+/Angular University/Angular Core Deep Dive - Beginner to Advanced/Section_12/mysol_1/ags_core_dp/src/app/pipes/custom-pipes.module.ts
+++ Frontend/Angular2+/Angular University/Angular Core Deep Dive - Beginner to Advanced/Section_12/mysol_1/ags_core_dp/src/app/pipes/custom-pipes.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FilterByCategoryPipe } from './filter-by-category.pipe';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FilterByCategoryPipe
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    FilterByCategoryPipe
   ]
 })
 export class CustomPipesModule { }
