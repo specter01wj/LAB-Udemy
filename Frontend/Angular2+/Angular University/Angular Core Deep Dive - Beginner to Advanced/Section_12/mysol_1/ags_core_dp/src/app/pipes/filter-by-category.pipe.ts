2f8f4280635c324ab7124cd3279233cce7592eba
Jin Wang <specter01wj@gmail.com>
2023-06-09 18:21:43 -0400

    v13

--- Frontend/Angular2+/Angular University/Angular Core Deep Dive - Beginner to Advanced/Section_12/mysol_1/ags_core_dp/src/app/pipes/filter-by-category.pipe.ts
+++ Frontend/Angular2+/Angular University/Angular Core Deep Dive - Beginner to Advanced/Section_12/mysol_1/ags_core_dp/src/app/pipes/filter-by-category.pipe.ts
@@ -8,17 +8,5 @@ import { Course } from "../model/course";
 export class FilterByCategoryPipe implements PipeTransform {
   transform(courses: Course[], category: string) {
     return courses.filter((course) => course.category === category);
-
-
-
-
-
-
-
-
-
-
-
-
   }
 }
