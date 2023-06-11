5083a73065feb757b39e86ad6cca93628a1f024e
Jin Wang <specter01wj@gmail.com>
2023-06-11 12:10:53 -0400

    v1

---
+++ Frontend/Angular2+/Angular University/Angular Core Deep Dive - Beginner to Advanced/Section_14/mysol_1/ags_core_dp/src/app/courses/courses.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CourseCardComponent } from "./course-card/course-card.component";
+import { CourseImageComponent } from "./course-image/course-image.component";
+
+import { CoursesRoutingModule } from './courses-routing.module';
+import { CustomPipesModule } from '../pipes/custom-pipes.module';
+
+
+@NgModule({
+  declarations: [
+    CourseCardComponent,
+    CourseImageComponent,
+  ],
+  imports: [
+    CommonModule,
+    CoursesRoutingModule,
+    CustomPipesModule,
+  ],
+  exports: [
+    CourseCardComponent,
+    CourseImageComponent,
+  ]
+})
+export class CoursesModule { }
