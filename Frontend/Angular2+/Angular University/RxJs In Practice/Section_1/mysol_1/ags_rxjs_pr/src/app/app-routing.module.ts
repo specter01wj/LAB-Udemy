0d7368eb7f7e5577aa272707070300dd12a16fb6
Jin Wang <specter01wj@gmail.com>
2023-06-13 23:17:40 -0400

    v7

--- Frontend/Angular2+/Angular University/RxJs In Practice/Section_1/mysol_1/ags_rxjs_pr/src/app/app-routing.module.ts
+++ Frontend/Angular2+/Angular University/RxJs In Practice/Section_1/mysol_1/ags_rxjs_pr/src/app/app-routing.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {AboutComponent} from "./about/about.component";
+import {CourseComponent} from "./course/course.component";
 
 const routes: Routes = [
   {
