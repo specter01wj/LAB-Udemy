85781cf175ccac9be64e6500bf1e3d7436aa3594
Jin Wang <specter01wj@gmail.com>
2023-06-15 10:27:41 -0400

    v1

---
+++ Frontend/Angular2+/Angular University/RxJs In Practice/Section_2/mysol_1/ags_rxjs_pr/src/app/about/about.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Observable, fromEvent, interval, noop, timer } from 'rxjs';
+
+@Component({
+  selector: 'about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    // const interval$ = interval(1000);
+    /* const interval$ = timer(3000, 1000);
+
+    const sub = interval$.subscribe(val => {
+      console.log('stream 1 ' + val);
+    });
+
+    setTimeout(() => sub.unsubscribe(), 5000);
+
+
+    const click$ = fromEvent(document, 'click');
+
+    click$.subscribe(
+      evt => {
+        console.log(evt);
+      },
+      err => {
+        console.log(err);
+      },
+      () => {
+        console.log('completed');
+      }); */
+
+      const http$ = Observable.create(observer => {
+        fetch('/api/courses')
+          .then(response => {
+            return response.json();
+          })
+          .then(body => {
+            observer.next(body);
+            observer.complete();
+          })
+          .catch(err => {
+            observer.error(err);
+          })
+      });
+
+      http$.subscribe(
+        courses => {
+          console.log(courses);
+        },
+        noop,
+        () => console.log('completed')
+      )
+
+  }
+
+}
