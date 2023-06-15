64783b931bb795fed2ddf5c60d8f833f2053a90b
Jin Wang <specter01wj@gmail.com>
2023-06-15 00:49:57 -0400

    v2

--- Frontend/Angular2+/Angular University/RxJs In Practice/Section_1/mysol_1/ags_rxjs_pr/src/app/about/about.component.ts
+++ Frontend/Angular2+/Angular University/RxJs In Practice/Section_1/mysol_1/ags_rxjs_pr/src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { interval } from 'rxjs';
+import { fromEvent, interval, timer } from 'rxjs';
 
 @Component({
   selector: 'about',
@@ -11,11 +11,20 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    const interval$ = interval(1000);
+    // const interval$ = interval(1000);
+    const interval$ = timer(3000, 1000);
 
     interval$.subscribe(val => {
       console.log('stream 1 ' + val);
     });
+
+
+    const click$ = fromEvent(document, 'click');
+
+    click$.subscribe( evt => {
+      console.log(evt);
+    });
+
   }
 
 }
