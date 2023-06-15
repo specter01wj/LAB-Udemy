a4b952c8ffef2ae87dfd0f03349cd4dc5f2e5fd5
Jin Wang <specter01wj@gmail.com>
2023-06-14 23:39:58 -0400

    v14

--- Frontend/Angular2+/Angular University/RxJs In Practice/Section_1/mysol_1/ags_rxjs_pr/src/app/home/home.component.ts
+++ Frontend/Angular2+/Angular University/RxJs In Practice/Section_1/mysol_1/ags_rxjs_pr/src/app/home/home.component.ts
@@ -1,25 +1,22 @@
-import {Component, OnInit} from '@angular/core';
-import {Course} from "../model/course";
-import {interval, Observable, of, timer} from 'rxjs';
-import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
-
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
+import { interval, Observable, of, timer } from 'rxjs';
+import {
+  catchError,
+  delayWhen,
+  map,
+  retryWhen,
+  shareReplay,
+  tap,
+} from 'rxjs/operators';
 
 @Component({
-    selector: 'home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css']
+  selector: 'home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  constructor() {}
 
-
-    constructor() {
-
-    }
-
-    ngOnInit() {
-
-
-
-    }
-
+  ngOnInit() {}
 }
