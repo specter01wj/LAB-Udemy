918773ad85a0fefd58c75caba52d5b93877e993b
Jin Wang <specter01wj@gmail.com>
2023-06-21 12:49:28 -0400

    v1

---
+++ Frontend/Angular2+/Angular University/Typescript Bootcamp - Beginner To Advanced/Section_2/mysol_1/fundamentals/src/02-primitive-types.ts
@@ -0,0 +1,65 @@
+
+// primitive types: number
+const lessonsCount = 10;
+
+const total = lessonsCount + 10;
+
+console.log("total =", total);
+
+// primitive types: string
+let title = "Typescript Bootcamp";
+
+let subtitle = "Learn the language fundamentals, build practical projects";
+
+let fullTitle = `Full title:${title}: ${subtitle}`;
+
+console.log(`Full title: ${fullTitle}`);
+
+// primitive types: boolean
+const published = true;
+
+if (published) {
+    console.log("The course is published.");
+}
+
+printCourse(title, subtitle, lessonsCount);
+
+function printCourse(title:string, subtitle:string, lessonsCount:number) {
+
+    let fullTitle = title + subtitle;
+
+}
+/*
+author: {
+    firstName: "Vasco",
+        lastName: "Cavalheiro"
+}*/
+
+
+// primitive types: object
+
+let course = {
+  title: "Typescript Bootcamp",
+  subtitle: "Learn the language fundamentals, build practical projects",
+  lessonsCount: 10
+};
+
+console.log("type of course is " + typeof course);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Frontend/Angular2+/Angular University/Typescript Bootcamp - Beginner To Advanced/Section_2/mysol_1/fundamentals/src/12-modules-imports.ts
@@ -0,0 +1,15 @@
+
+import {PAGE_SIZE, COURSE} from "./12-modules-exports";
+
+const pageSize = PAGE_SIZE;
+
+import {HELLO_WORLD} from './32-plain-javascript'
+
+import {Course, loadAllCourses, saveCourse}
+    from "14-module-reexports";
+
+import printCourse from "./15-default-exports";
+
+import * as constants from "./15-default-exports";
+
+printCourse({});
