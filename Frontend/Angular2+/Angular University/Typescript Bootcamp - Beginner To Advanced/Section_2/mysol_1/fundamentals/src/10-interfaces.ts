55249a4417fac6ed6926d334bfcf8285a6bdfed4
Jin Wang <specter01wj@gmail.com>
2023-06-29 01:34:05 -0400

    v7

--- Frontend/Angular2+/Angular University/Typescript Bootcamp - Beginner To Advanced/Section_2/mysol_1/fundamentals/src/10-interfaces.ts
+++ Frontend/Angular2+/Angular University/Typescript Bootcamp - Beginner To Advanced/Section_2/mysol_1/fundamentals/src/10-interfaces.ts
@@ -1,22 +1,20 @@
-
 interface Course {
-    readonly title:string;
-    subtitle:string;
+  readonly title: string;
+  subtitle: string;
 }
 
 const course: Course = {
-    title: "Typescript Bootcamp",
-    subtitle: "Learn the language fundamentals, build practical projects",
-    lessonsCount: 10
+  title: "Typescript Bootcamp",
+  subtitle: "Learn the language fundamentals, build practical projects",
+  lessonsCount: 10,
 };
 
 interface Course {
-    lessonsCount?:number;
+  lessonsCount?: number;
 }
 
 const otherCourse: Course = {
-    title: "Typescript Bootcamp v2",
-    subtitle: "Learn the language fundamentals, build practical projects",
-    lessonsCount: 10
+  title: "Typescript Bootcamp v2",
+  subtitle: "Learn the language fundamentals, build practical projects",
+  lessonsCount: 10,
 };
-
