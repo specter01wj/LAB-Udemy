f540798864e5be647b8635afd959794e72a9504f
Jin Wang <specter01wj@gmail.com>
2023-06-30 15:34:29 -0400

    v2

--- Frontend/Angular2+/Angular University/Typescript Bootcamp - Beginner To Advanced/Section_3/mysol_1/fundamentals/src/12-modules-imports.ts
+++ Frontend/Angular2+/Angular University/Typescript Bootcamp - Beginner To Advanced/Section_3/mysol_1/fundamentals/src/12-modules-imports.ts
@@ -1,12 +1,10 @@
-
-import {PAGE_SIZE, COURSE} from "./12-modules-exports";
+import { PAGE_SIZE, COURSE } from "./12-modules-exports";
 
 const pageSize = PAGE_SIZE;
 
-import {HELLO_WORLD} from './32-plain-javascript'
+import { HELLO_WORLD } from "./32-plain-javascript";
 
-import {Course, loadAllCourses, saveCourse}
-    from "14-module-reexports";
+import { Course, loadAllCourses, saveCourse } from "14-module-reexports";
 
 import printCourse from "./15-default-exports";
 
