f55b8705d777308de944f8dbc4f0a541b1e30940
Jin Wang <specter01wj@gmail.com>
2023-06-30 15:35:00 -0400

    v3

---
+++ Frontend/Angular2+/Angular University/Typescript Bootcamp - Beginner To Advanced/Section_3/mysol_1/fundamentals/src/12-modules-exports.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.COURSE = exports.PAGE_SIZE = void 0;
+exports.PAGE_SIZE = 100;
+var pageSize = exports.PAGE_SIZE;
+exports.COURSE = {
+    title: "Typescript Bootcamp",
+    subtitle: "Learn the language fundamentals, build practical projects",
+    lessonsCount: 10,
+};
