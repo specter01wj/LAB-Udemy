d79544e7018c3e110ec643d1f0f78d8f3e7860c0
Jin Wang <specter01wj@gmail.com>
2023-07-02 19:38:16 -0400

    v2

--- Frontend/Angular2+/Angular University/Typescript Bootcamp - Beginner To Advanced/Section_4/mysol_1/fundamentals/src/16-arrow-functions.ts
+++ Frontend/Angular2+/Angular University/Typescript Bootcamp - Beginner To Advanced/Section_4/mysol_1/fundamentals/src/16-arrow-functions.ts
@@ -1,16 +1,11 @@
-
 function saveCourse(course, callback: Function) {
+  this.course = course;
 
-    this.course = course;
-
-    setTimeout(() => {
-
-        callback(this.course?.title ?? "unknown course");
-
-    }, 1000);
-
+  setTimeout(() => {
+    callback(this.course?.title ?? "unknown course");
+  }, 1000);
 }
 
-const cb = (title:string) => console.log("Save successful.", title);
+const cb = (title: string) => console.log("Save successful.", title);
 
-saveCourse({title:"Typescript Bootcamp"}, cb);
+saveCourse({ title: "Typescript Bootcamp" }, cb);
