847ed56f7505ac05fe46aea9e7fc32cf0dff619e
Jin Wang <specter01wj@gmail.com>
2023-07-03 18:27:48 -0400

    v5

---
+++ Frontend/Angular2+/Angular University/Typescript Bootcamp - Beginner To Advanced/Section_4/mysol_1/fundamentals/src/18-object-spread.js
@@ -0,0 +1,22 @@
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+var course = {
+    title: "Typescript Bootcamp",
+    subtitle: "Learn the language fundamentals, build practical projects",
+    stats: {
+        lessonsCount: 10,
+    },
+};
+var newCourse = __assign({}, course);
+console.log(newCourse);
+course.stats.lessonsCount = 100;
+console.log(newCourse);
