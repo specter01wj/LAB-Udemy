867f42a7244ae6ae7a1ebf95f03ae4c6baad0e00
Jin Wang <specter01wj@gmail.com>
2023-07-03 18:27:32 -0400

    v4

--- Frontend/Angular2+/Angular University/Typescript Bootcamp - Beginner To Advanced/Section_4/mysol_1/fundamentals/src/18-object-spread.ts
+++ Frontend/Angular2+/Angular University/Typescript Bootcamp - Beginner To Advanced/Section_4/mysol_1/fundamentals/src/18-object-spread.ts
@@ -1,25 +1,23 @@
-
 interface Course {
-    title:string;
-    subtitle:string;
-    stats: {
-        lessonsCount:number;
-    }
+  title: string;
+  subtitle: string;
+  stats: {
+    lessonsCount: number;
+  };
 }
 
 let course: Course = {
-    title: "Typescript Bootcamp",
-    subtitle: "Learn the language fundamentals, build practical projects",
-    stats: {
-        lessonsCount: 10
-    }
+  title: "Typescript Bootcamp",
+  subtitle: "Learn the language fundamentals, build practical projects",
+  stats: {
+    lessonsCount: 10,
+  },
 };
 
-const newCourse = {...course};
+const newCourse = { ...course };
 
 console.log(newCourse);
 
 course.stats.lessonsCount = 100;
 
 console.log(newCourse);
-
