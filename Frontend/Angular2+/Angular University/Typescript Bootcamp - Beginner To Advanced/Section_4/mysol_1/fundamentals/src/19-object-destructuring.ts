4642f8567dd7dbd806c5615abdb398e08f01cb46
Jin Wang <specter01wj@gmail.com>
2023-07-04 02:16:06 -0400

    v2

--- Frontend/Angular2+/Angular University/Typescript Bootcamp - Beginner To Advanced/Section_4/mysol_1/fundamentals/src/19-object-destructuring.ts
+++ Frontend/Angular2+/Angular University/Typescript Bootcamp - Beginner To Advanced/Section_4/mysol_1/fundamentals/src/19-object-destructuring.ts
@@ -1,22 +1,21 @@
-
 interface Course {
-    title:string;
-    subtitle:string;
-    lessonsCount:number;
+  title: string;
+  subtitle: string;
+  lessonsCount: number;
 }
 
 let course: Course = {
-    title: "Typescript Bootcamp",
-    subtitle: "Learn the language fundamentals, build practical projects",
-    lessonsCount: 10
+  title: "Typescript Bootcamp",
+  subtitle: "Learn the language fundamentals, build practical projects",
+  lessonsCount: 10,
 };
 
 printCourse(course);
 
-function printCourse(course:Course) {
+function printCourse(course: Course) {
+  const { title, ...other } = course;
 
-    const {title, ...other} = course;
-
-    console.log(`Title: ${title}, Subtitle: ${other.subtitle} lessons count: ${other.lessonsCount}`);
-
+  console.log(
+    `Title: ${title}, Subtitle: ${other.subtitle} lessons count: ${other.lessonsCount}`
+  );
 }
