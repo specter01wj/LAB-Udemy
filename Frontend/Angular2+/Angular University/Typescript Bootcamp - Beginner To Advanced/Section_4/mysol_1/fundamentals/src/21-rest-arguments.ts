8cd1a311c9c07011d4c50995b42de9596c472610
Jin Wang <specter01wj@gmail.com>
2023-07-05 00:52:00 -0400

    v4

--- Frontend/Angular2+/Angular University/Typescript Bootcamp - Beginner To Advanced/Section_4/mysol_1/fundamentals/src/21-rest-arguments.ts
+++ Frontend/Angular2+/Angular University/Typescript Bootcamp - Beginner To Advanced/Section_4/mysol_1/fundamentals/src/21-rest-arguments.ts
@@ -1,36 +1,26 @@
-
 interface Course {
-    title:string;
-    lessonsCount:number;
+  title: string;
+  lessonsCount: number;
 }
 
-const course1:Course = {
-    title: "Typescript Bootcamp",
-    lessonsCount: 100
+const course1: Course = {
+  title: "Typescript Bootcamp",
+  lessonsCount: 100,
 };
 
 const course2: Course = {
   title: "Angular For Beginners",
-  lessonsCount: 20
+  lessonsCount: 20,
 };
 
-function printCourses(message:string, ...courses: Course[]) {
+function printCourses(message: string, ...courses: Course[]) {
+  console.log(message);
 
-    console.log(message);
-
-    for (let course of courses) {
-        console.log(course.title);
-    }
-
+  for (let course of courses) {
+    console.log(course.title);
+  }
 }
 
 // printCourses("Welcome to the Angular University", [course1, course2]);
 
 printCourses("Welcome to the Angular University", course1, course2);
-
-
-
-
-
-
-
