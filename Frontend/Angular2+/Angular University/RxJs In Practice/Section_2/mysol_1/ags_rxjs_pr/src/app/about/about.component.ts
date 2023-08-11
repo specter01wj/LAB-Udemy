5148fd9b3ffea72a1aa4801c144ee8714c7f7e6d
Jin Wang <specter01wj@gmail.com>
2023-08-10 23:01:24 -0400

    v2

--- Frontend/Angular2+/Angular University/RxJs In Practice/Section_2/mysol_1/ags_rxjs_pr/src/app/about/about.component.ts
+++ Frontend/Angular2+/Angular University/RxJs In Practice/Section_2/mysol_1/ags_rxjs_pr/src/app/about/about.component.ts
@@ -41,7 +41,8 @@ export class AboutComponent implements OnInit {
           })
           .then(body => {
             observer.next(body);
-            
+
+
             observer.complete();
           })
           .catch(err => {
