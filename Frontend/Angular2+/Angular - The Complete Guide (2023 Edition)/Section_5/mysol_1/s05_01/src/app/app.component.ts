6823dadf3a643b5281ef81e465050d0af68e8c94
Jin Wang <specter01wj@gmail.com>
2023-12-21 01:27:19 -0500

    v1

--- Frontend/Angular2+/Angular - The Complete Guide (2023 Edition)/Section_5/mysol_1/s05_01/src/app/app.component.ts
+++ Frontend/Angular2+/Angular - The Complete Guide (2023 Edition)/Section_5/mysol_1/s05_01/src/app/app.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 's05_01';
+  serverElements: any[] = [
+    {
+      type: 'server',
+      name: 'Test_server1',
+      content: 'Just a test1!'
+    },
+    {
+      type: 'server',
+      name: 'Test_server2',
+      content: 'Just a test2!'
+    }
+  ];
+
+
+
 }
---
+++ Frontend/Angular2+/Angular - The Complete Guide (2023 Edition)/Section_5/mysol_1/s05_01/src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.scss']
+})
+export class CockpitComponent {
+  newServerName = '';
+  newServerContent = '';
+
+  onAddServer() {
+    /* this.serverElements.push({
+      type: 'server',
+      name: this.newServerName,
+      content: this.newServerContent
+    }); */
+  }
+
+  onAddBlueprint() {
+    /* this.serverElements.push({
+      type: 'blueprint',
+      name: this.newServerName,
+      content: this.newServerContent
+    }); */
+  }
+
+}
