4ba80bd83e29e895f3b00deb2c789bc4357ca86c
Jin Wang <specter01wj@gmail.com>
2024-02-05 01:18:42 -0500

    v2

---
+++ Frontend/Javascript/JavaScript - Basics to Advanced step by step/Sec_2/mysol_1/js/app.js
@@ -0,0 +1,35 @@
+$( document ).ready(function() {
+
+  console.clear();
+
+  var firstNum = 10;
+  console.log(firstNum);
+
+  var secondNum = 15;
+  console.log(secondNum);
+
+  // Camel case (Recommended for JS)
+  var firstNum;
+  // Pascal case
+  var FirstNum;
+  // Snake case
+  var first_num;
+
+  // Rules for naming JS variables
+  // - Name can contain letters, digits, _ and $
+  var mySubject1, math$
+  // - Name cannot start with a digit
+  var maths;
+  // - Name can start with an _ or $
+  var _subjectmarks, $rankInClass;
+  // - Names are case sentitive
+  var x, X;
+  // - Names cannnot be reserved keywords
+  var varName;
+
+
+
+  $('#output').html('Length is: ' + secondNum);
+
+
+});
