fa9260495f2eed5d30255266dbc625e79dce2c2c
Jin Wang <specter01wj@gmail.com>
2024-02-10 14:17:18 -0500

    v24

--- Frontend/Javascript/Master JavaScript from Basics to Advanced/Sec_2/mysol_6/js/app.js
+++ Frontend/Javascript/Master JavaScript from Basics to Advanced/Sec_2/mysol_6/js/app.js
@@ -2,44 +2,15 @@ $( document ).ready(function() {
 
   console.clear()
 
-  var exampleString = "This is Javascript tutorial";
+  var mVar //mvar holds no value
+  console.log(mVar)
 
-  // toUpperCase()
-  console.log(exampleString.toUpperCase());
-
-  // toLowerCase()
-  console.log(exampleString.toLowerCase());
-
-  // concat()
-  var firstName = "Javascript";
-  var lastName = "Playground";
-  console.log(exampleString.concat(firstName));
-  console.log(exampleString.concat(' ', firstName, ' ', lastName));
-
-  // +
-  console.log(firstName + ' ' + lastName + ' ' + exampleString);
-
-  // trim()
-  var extraSpaceString = "      mystring     ";
-  console.log(extraSpaceString.trim());
-
-  // charAt()
-  var charAtExampleString = "This is my test string";
-  console.log(charAtExampleString.charAt(5));
-
-  // split()
-  var sampleString = "This is my sample string";
-  console.log(sampleString.split(' '));
-
-  var sampleString2 = "This, is, my, sample, string";
-  console.log(sampleString2.split(','));
-
-  var sampleString3 = "This is my sample string";
-  console.log(sampleString3.split());
+  mVar = null //mVar holds the value null
+  console.log(mVar)
 
 
 
-  $('#output').html('Output is: ' + exampleString);
+  $('#output').html('Output is: ' + mVar);
 
 
 });
