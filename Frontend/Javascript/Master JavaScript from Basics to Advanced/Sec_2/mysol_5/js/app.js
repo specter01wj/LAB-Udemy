30ee5045f2d428445fc216ed0248a91167fc2071
Jin Wang <specter01wj@gmail.com>
2024-02-10 14:01:43 -0500

    v16

--- Frontend/Javascript/Master JavaScript from Basics to Advanced/Sec_2/mysol_5/js/app.js
+++ Frontend/Javascript/Master JavaScript from Basics to Advanced/Sec_2/mysol_5/js/app.js
@@ -2,37 +2,14 @@ $( document ).ready(function() {
 
   console.clear()
 
-  var myFirstString = "This is a string for Javascript string functions, Javascript"
+  var exampleString = "This is Javascript tutorial";
 
-  // Returns the length of the string
-  console.log(myFirstString.length)
 
-  // Find index of a string inside another string
-  console.log(myFirstString.indexOf('Javascript'))
-  console.log(myFirstString.indexOf('This'))
 
-  // Find LAST index of a string inside another string
-  console.log(myFirstString.lastIndexOf('Javascript'))
-  console.log(myFirstString.lastIndexOf('This'))
 
-  // Get a part of our string slice(start, end)
-  console.log(myFirstString.slice(0, 4))
-  console.log(myFirstString.slice(21, 31))
-  console.log(myFirstString.slice(-10))
-  console.log(myFirstString.slice(5))
 
-  // Get sub string function - substr(startPos, length)
-  console.log(myFirstString.substr(0, 4))
-  console.log(myFirstString.substr(21, 10))
-  console.log(myFirstString.substr(21))
 
-  // substring()
-  console.log('substring()---------');
-  console.log(myFirstString.substring(0, 4));
-  console.log(myFirstString.substring(21, 31));
-  console.log(myFirstString.substring(21));
-
-  $('#output').html('Output is: ' + myFirstString);
+  $('#output').html('Output is: ' + exampleString);
 
 
 });
