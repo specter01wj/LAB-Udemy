347c9662ce85b7749f6abe9ba9c1c628690b3751
Jin Wang <specter01wj@gmail.com>
2024-02-10 14:02:47 -0500

    v17

--- Frontend/Javascript/Master JavaScript from Basics to Advanced/Sec_2/mysol_5/js/app.js
+++ Frontend/Javascript/Master JavaScript from Basics to Advanced/Sec_2/mysol_5/js/app.js
@@ -4,9 +4,8 @@ $( document ).ready(function() {
 
   var exampleString = "This is Javascript tutorial";
 
-
-
-
+  // toUpperCase()
+  console.log(exampleString.toUpperCase());
 
 
   $('#output').html('Output is: ' + exampleString);
