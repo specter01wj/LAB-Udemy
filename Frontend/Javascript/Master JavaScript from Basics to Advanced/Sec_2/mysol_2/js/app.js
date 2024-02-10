1a162253b39626001c0931b79811c2b1ed67ae7d
Jin Wang <specter01wj@gmail.com>
2024-02-10 12:09:10 -0500

    v6

--- Frontend/Javascript/Master JavaScript from Basics to Advanced/Sec_2/mysol_2/js/app.js
+++ Frontend/Javascript/Master JavaScript from Basics to Advanced/Sec_2/mysol_2/js/app.js
@@ -1,41 +1,37 @@
 $( document ).ready(function() {
 
   console.clear()
+  var num1 = 10;
+  var num2 = 20.3;
 
-  var num1 = 10 // Integer
-  var num2 = 10.9 // Decimal Number
+  // toString() number as input and returns a string
+  console.log(num1.toString())
+  console.log(num2.toString())
 
-  console.log(typeof(num1))
-  console.log(typeof(num2))
+  var strNum1 = "10";
+  var strNum2 = "11.5";
+  var strNum3 = "ABC";
 
-  //Addition
-  var num3 = 15;
-  var sum = num3 + num1 + num2;
-  console.log(sum);
+  // parseInt() string numeral as input and returns a number
+  console.log(parseInt(strNum1));
+  console.log(parseInt(strNum2));
+  console.log(parseInt(strNum3));
 
-  //Substraction
-  var diff = num3 - num1;
-  console.log(diff);
+  // parseFloat() string numeral as an input and returns a foating
+  console.log(parseFloat(strNum2))
+  console.log(parseFloat(strNum3))
 
-  //Multiplication
-  var mul = num3 * num1;
-  console.log(mul);
+  // toFixed() takes floating number and rounds it off to given position
 
-  // Division
-  var div = num3 / num1;
-  console.log(div);
-
-  var divByZero = num3 / 0;
-  console.log(divByZero);
-  console.log(typeof(divByZero));
-
-  var mulByString = num3 * 'A';
-  console.log(mulByString);
-  console.log(typeof(mulByString));
+  var strFloat = 87.987653;
+  console.log(strFloat.toFixed())
+  console.log(strFloat.toFixed(2))
+  console.log(strFloat.toFixed(5))
+  console.log(strFloat.toFixed(10))
 
 
 
-  $('#output').html('Length is: ' + mulByString);
+  $('#output').html('Output is: ' + strFloat);
 
 
 });
