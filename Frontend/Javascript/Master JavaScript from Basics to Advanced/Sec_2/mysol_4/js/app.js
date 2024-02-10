d5907109ddd2a3a86dc6361f1e6ebeadd139572a
Jin Wang <specter01wj@gmail.com>
2024-02-10 12:39:48 -0500

    v11

---
+++ Frontend/Javascript/Master JavaScript from Basics to Advanced/Sec_2/mysol_4/js/app.js
@@ -0,0 +1,28 @@
+$( document ).ready(function() {
+
+  console.clear()
+
+  var myFirstString = "I love JavaScript"
+
+  console.log(typeof(myFirstString))
+
+  var mySecondString = 'We will learn JavaScript together'
+  console.log(typeof(mySecondString))
+
+  //What if my string contains a double quote
+  var doubleQuotesString = "This is a \"JavaScript\" string"
+  console.log(doubleQuotesString)
+
+  // What if my string contains a single quote
+  var singleQuotesString = 'This is a \'JavaScript\' string'
+  console.log(singleQuotesString)
+
+  var doubleQuotesString2 = "This is a 'JavaScript' string"
+  var singleQuotesString2 = 'This is a "JavaScript" string'
+
+
+
+  $('#output').html('Output is: ' + doubleQuotesString2);
+
+
+});
