085da4956bd53d17fef789cd9488a1f8d7645431
Jin Wang <specter01wj@gmail.com>
2024-02-10 14:19:36 -0500

    v25

--- Frontend/Javascript/Master JavaScript from Basics to Advanced/Sec_2/mysol_6/js/app.js
+++ Frontend/Javascript/Master JavaScript from Basics to Advanced/Sec_2/mysol_6/js/app.js
@@ -4,9 +4,11 @@ $( document ).ready(function() {
 
   var mVar //mvar holds no value
   console.log(mVar)
+  console.log(typeof(mVar));
 
   mVar = null //mVar holds the value null
   console.log(mVar)
+  console.log(typeof(mVar));
 
 
 
