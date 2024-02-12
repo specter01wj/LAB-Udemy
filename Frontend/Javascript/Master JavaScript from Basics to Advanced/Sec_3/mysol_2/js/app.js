3f14dd4c9940bdd691e77bb9e292adb87545dfca
Jin Wang <specter01wj@gmail.com>
2024-02-12 00:52:49 -0500

    v7

--- Frontend/Javascript/Master JavaScript from Basics to Advanced/Sec_3/mysol_2/js/app.js
+++ Frontend/Javascript/Master JavaScript from Basics to Advanced/Sec_3/mysol_2/js/app.js
@@ -28,22 +28,22 @@ $( document ).ready(function() {
       console.log('Timings: 09:00-05:00')
       break;
     case 'Wed':
-    console.log('Timings: 09:30-05:30')
+      console.log('Timings: 09:30-05:30')
       break;
     case 'Thu':
-    console.log('Timings: 10:15-06:15')
+      console.log('Timings: 10:15-06:15')
       break;
     case 'Fri':
-    console.log('Timings: 09:05-05:05')
+     console.log('Timings: 09:05-05:05')
       break;
     case 'Sat':
-    console.log('Timings: 09:00-05:00')
+      console.log('Timings: 09:00-05:00')
       break;
     case 'Sun':
-    console.log('Timings: 09:00-01:00')
+      console.log('Timings: 09:00-01:00')
       break;
     default:
-    console.log('Timings: 09:00-01:00')
+      console.log('Timings: 09:00-01:00')
   }
 
 
