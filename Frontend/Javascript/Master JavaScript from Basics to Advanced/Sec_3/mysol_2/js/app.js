4f045f0b80110f3c012a87e9679bbb58ea63bda3
Jin Wang <specter01wj@gmail.com>
2024-02-12 00:24:14 -0500

    v6

--- Frontend/Javascript/Master JavaScript from Basics to Advanced/Sec_3/mysol_2/js/app.js
+++ Frontend/Javascript/Master JavaScript from Basics to Advanced/Sec_3/mysol_2/js/app.js
@@ -2,27 +2,52 @@ $( document ).ready(function() {
 
   console.clear()
 
-  // if(7>5) { 
-  // 	console.log(true)
-  // }
+  var currentDay = 'Thu'
 
-  // if (5 > 7) {
-  // 	console.log(true)
-  // } else {
-  // 	console.log(false)
-  // }
+  if(currentDay === 'Mon') {
+    console.log('Timings: 10:00-06:00')
+  } else if(currentDay === 'Tue') {
+    console.log('Timings: 09:00-05:00')
+  } else if(currentDay === 'Wed') {
+    console.log('Timings: 09:30-05:30')
+  } else if(currentDay === 'Thu') {
+    console.log('Timings: 10:15-06:15')
+  } else if(currentDay === 'Fri') {
+    console.log('Timings: 09:05-05:05')
+  } else if(currentDay === 'Sat') {
+    console.log('Timings: 09:00-05:00')
+  } else if(currentDay === 'Sun') {
+    console.log('Timings: 09:00-01:00')
+  }
 
-  if (5 > 7) {
-    console.log('5 > 7')
-  } else if(7 > 8){ 
-    console.log('7 > 8')
-  } else {
-    console.log('Nothing')
+  switch(currentDay) {
+    case 'Mon':
+      console.log('Timings: 10:00-06:00')
+      break;
+    case 'Tue':
+      console.log('Timings: 09:00-05:00')
+      break;
+    case 'Wed':
+    console.log('Timings: 09:30-05:30')
+      break;
+    case 'Thu':
+    console.log('Timings: 10:15-06:15')
+      break;
+    case 'Fri':
+    console.log('Timings: 09:05-05:05')
+      break;
+    case 'Sat':
+    console.log('Timings: 09:00-05:00')
+      break;
+    case 'Sun':
+    console.log('Timings: 09:00-01:00')
+      break;
+    default:
+    console.log('Timings: 09:00-01:00')
   }
 
 
-
-  $('#output').html('Length is: ' + '');
+  $('#output').html('currentDay is: ' + currentDay);
 
 
 });
