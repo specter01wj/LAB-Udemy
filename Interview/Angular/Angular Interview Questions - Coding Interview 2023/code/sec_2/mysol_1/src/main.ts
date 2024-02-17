5efd91851650275dbe3914f9371ff3c658edbde9
Jin Wang <specter01wj@gmail.com>
2024-02-17 12:40:00 -0500

    v12

--- Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_2/mysol_1/src/main.ts
+++ Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_2/mysol_1/src/main.ts
@@ -1,2 +1,18 @@
 
+export interface UserInterface {
+  id: string; 
+  name: string; 
+  age: number;
+  getMessage(): string;
+}
 
+let user: UserInterface;
+
+user = {
+  id: '1001',
+  name: 'Jin',
+  age: 34,
+  getMessage() {
+    return `${this.name} is age: ${this.age}`;
+  }
+}
