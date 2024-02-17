a1aeba5bbac78397525eba662b013a6117fd9a32
Jin Wang <specter01wj@gmail.com>
2024-02-17 11:30:09 -0500

    v4

--- Frontend/Javascript/Master JavaScript from Basics to Advanced/Sec_4/mysol_1/js/app.js
+++ Frontend/Javascript/Master JavaScript from Basics to Advanced/Sec_4/mysol_1/js/app.js
@@ -1,28 +1,46 @@
 $( document ).ready(function() {
 
-  console.clear()
+  console.clear();
 
-  // if(7>5) { 
-  // 	console.log(true)
-  // }
+  var num1 = 10;
+  var num2 = 15;
+  var num3 = 20;
 
-  // if (5 > 7) {
-  // 	console.log(true)
-  // } else {
-  // 	console.log(false)
-  // }
+  //Addition
+  console.log(num1 + num2)
+  console.log(num1 + num2 + num3)
 
-  if (5 > 7) {
-    console.log('5 > 7')
-  } else if(7 > 8){ 
-    console.log('7 > 8')
-  } else {
-    console.log('Nothing')
-  }
+  //Subtraction
+  console.log(num2 - num1)
+  console.log(num3 - num2 - num1)
+
+  //Multiplication
+  console.log(num1 * num2)
+
+  //Division
+  console.log(num2/num1)
+
+  //Modulus
+  console.log(num2 % num1)
+
+  //Increment - Post and Pre
+  console.log(num1++)
+  console.log(num1++)
+  console.log(num1)
+
+  console.log(++num1)
+  console.log(num1)
+
+  //Decrement - Post and Pre
+  console.log(num1--)
+  console.log(num1)
+
+  console.log(--num1)
+  console.log(num1)
 
 
 
-  $('#output').html('Length is: ' + '');
+  $('#output').html('Length is: ' + num1);
 
 
 });
