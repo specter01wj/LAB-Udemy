5a678f739d18a3190a415d4a032bf225a3475663
Jin Wang <specter01wj@gmail.com>
2024-02-17 12:02:19 -0500

    v8

--- Frontend/Javascript/Master JavaScript from Basics to Advanced/Sec_4/mysol_2/js/app.js
+++ Frontend/Javascript/Master JavaScript from Basics to Advanced/Sec_4/mysol_2/js/app.js
@@ -6,25 +6,25 @@ $( document ).ready(function() {
   var num2 = 30;
 
   // =
-  // num1 = 20;
+  num1 = 20;
 
   // +=
-  // num1 += 10; // num1 = num1 + 10
-  // num1 += num2 // num1 = num1 + num2
-  // console.log(num1)
+  num1 += 10; // num1 = num1 + 10
+  num1 += num2 // num1 = num1 + num2
+  console.log(num1)
 
   // -=
-  // num1 -= 10; //num1 = num1 - 10
-  // num1 -= num2 //num1 = num1 - num2
-  // console.log(num1)
+  num1 -= 10; //num1 = num1 - 10
+  num1 -= num2 //num1 = num1 - num2
+  console.log(num1)
 
   // *=
-  // num1 *= 20 //num1 = num1 * 20
-  // console.log(num1)
+  num1 *= 20 //num1 = num1 * 20
+  console.log(num1)
 
   // /=
-  // num1 /= 10 //num1 = num1 / 10
-  // console.log(num1)
+  num1 /= 10 //num1 = num1 / 10
+  console.log(num1)
 
   // %=
   num1 %= 10 //num1 = num1 % 10
