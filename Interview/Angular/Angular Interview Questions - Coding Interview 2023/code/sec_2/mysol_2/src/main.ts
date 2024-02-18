d3300551c9e1fa5dfcfbd2cb1bb06d4147592229
Jin Wang <specter01wj@gmail.com>
2024-02-17 19:14:02 -0500

    v3

--- Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_2/mysol_2/src/main.ts
+++ Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_2/mysol_2/src/main.ts
@@ -1,4 +1,11 @@
-const getItem = (item: number | undefined) => {
-  return item.toString();
-};
-    }
+
+let fruits1: string[] = ['Apple', 'Orange', 'Banana'];
+let fruits2: Array<string> = ['Apple', 'Orange', 'Banana'];
+
+let foo: string | number = 'foo';
+let values: (string | number)[] = ['Apple', 2, 'Orange', 3];
+
+const appDiv = document.getElementById('app');
+if (appDiv) {
+  appDiv.innerHTML = values.toString();
+}
