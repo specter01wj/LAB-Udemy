de32f26b7d03e97d638ab3ab6b3017c607173512
Jin Wang <specter01wj@gmail.com>
2024-02-17 23:42:53 -0500

    v18

--- Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_2/mysol_7/src/main.ts
+++ Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_2/mysol_7/src/main.ts
@@ -1,1 +1,16 @@
 
+export interface UserInterface {
+  id: string; 
+  name: string; 
+}
+
+const getName = (user?: UserInterface): string => {
+  return user?.name ?? 'not set';
+}
+
+
+
+const appDiv = document.getElementById('app');
+if (appDiv) {
+  appDiv.innerHTML = getName({id: '1001', name: null});
+}
