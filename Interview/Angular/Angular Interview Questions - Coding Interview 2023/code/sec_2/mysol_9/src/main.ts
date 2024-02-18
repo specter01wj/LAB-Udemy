253b376c14b9e1e3eb4e367e6178b505f64392c9
Jin Wang <specter01wj@gmail.com>
2024-02-18 00:05:15 -0500

    v23

--- Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_2/mysol_9/src/main.ts
+++ Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_2/mysol_9/src/main.ts
@@ -1,1 +1,15 @@
 
+export enum StatusEnum {
+  ACTIVE = 'active',
+  DRAFT = 'draft',
+  ARCHIVED = 'archived'
+}
+
+const isActive = (status: StatusEnum): boolean => {
+  return status === StatusEnum.ACTIVE;
+}
+
+const appDiv = document.getElementById('app');
+if (appDiv) {
+  appDiv.innerHTML = isActive(StatusEnum.ARCHIVED).toString();
+}
