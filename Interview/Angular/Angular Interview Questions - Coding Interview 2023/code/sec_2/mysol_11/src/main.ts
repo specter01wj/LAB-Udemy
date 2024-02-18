9c639c19bf8fa53cf6c19761a3324f44935cfc9f
Jin Wang <specter01wj@gmail.com>
2024-02-18 01:11:51 -0500

    v30

--- Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_2/mysol_11/src/main.ts
+++ Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_2/mysol_11/src/main.ts
@@ -1,1 +1,19 @@
 
+const addId = <T>(obj: T) => {
+  const id = Math.random().toString(16);
+  return {
+    ...obj,
+    id
+  }
+};
+
+const user = {
+  name: 'James'
+};
+
+const result = addId(user);
+
+const appDiv = document.getElementById('app');
+if (appDiv) {
+  appDiv.innerHTML = result.id;
+}
