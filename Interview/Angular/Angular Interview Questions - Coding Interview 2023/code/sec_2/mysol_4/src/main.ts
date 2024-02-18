da80ead7d812497fa67a6631abb7d31c43d7a064
Jin Wang <specter01wj@gmail.com>
2024-02-17 20:56:10 -0500

    v9

--- Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_2/mysol_4/src/main.ts
+++ Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_2/mysol_4/src/main.ts
@@ -1,1 +1,14 @@
+const doSomething = (): void => {
+  console.log('doSomething');
+};
 
+let vAny: any = 10;
+let vUnknown: unknown = 10;
+
+let s1: string = vAny;
+let s2: string = vUnknown as string;
+
+const appDiv = document.getElementById('app');
+if (appDiv) {
+  appDiv.innerHTML = s2;
+}
