e8eba3a1f9a1bcf71fd0f4ca232707b02c80c680
Jin Wang <specter01wj@gmail.com>
2024-02-17 21:35:57 -0500

    v12

--- Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_2/mysol_5/src/main.ts
+++ Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_2/mysol_5/src/main.ts
@@ -1,1 +1,28 @@
 
+export interface UserInterface {
+  id: string; 
+  name: string; 
+  age: number;
+  getMessage(): string;
+}
+
+export interface ProfileInterface {
+  name: string; 
+  profileUrl: string; 
+  isActive: boolean;
+}
+
+const transformUserToProfile = (user: UserInterface, isActive: boolean = true): ProfileInterface => {
+  return {
+    name: user.name,
+    profileUrl: `/profiles/${user.name}`,
+    isActive
+  }
+}
+
+let tt1 = transformUserToProfile({id: '1001', name: 'James', age: 43, getMessage: () => 'King'}, false);
+
+const appDiv = document.getElementById('app');
+if (appDiv) {
+  appDiv.innerHTML = JSON.stringify(tt1);
+}
