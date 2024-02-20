d007d611475463544242d6f410a41fb917b901bd
Jin Wang <specter01wj@gmail.com>
2024-02-20 00:09:50 -0500

    v2

--- Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_3/mysol_2/src/main.ts
+++ Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_3/mysol_2/src/main.ts
@@ -1,5 +1,29 @@
+import { Observable, filter, of } from "rxjs";
+
 export interface UserInterface {
   id: string;
   name: string;
   age: number;
+  isActive: boolean;
 }
+
+const getActiveUsers = (users$: Observable<UserInterface[]>): Observable<UserInterface[]> => {
+  return users$.pipe(filter((users) => users.every((user) => user.isActive)));
+}
+
+const mockUsers: UserInterface[] = [
+  { id: '1', name: 'John Doe', age: 30, isActive: true },
+  { id: '2', name: 'Jane Doe', age: 25, isActive: false },
+  { id: '3', name: 'Jim Beam', age: 40, isActive: true },
+];
+
+const mockUsers$ = of(mockUsers);
+
+
+getActiveUsers(mockUsers$).subscribe((item) => {
+  const appDiv = document.getElementById('app');
+  if (appDiv) {
+    appDiv.innerHTML = item.toString();
+  }
+})
+
