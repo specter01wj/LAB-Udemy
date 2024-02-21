630fea3ed09c8b63cd95563cc24b55b0c848d092
Jin Wang <specter01wj@gmail.com>
2024-02-21 00:42:02 -0500

    v4

--- Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_3/mysol_3/src/main.ts
+++ Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_3/mysol_3/src/main.ts
@@ -10,12 +10,24 @@ const normalizeUsers = (users$: Observable<UserInterface[]>): Observable<string[
   return users$.pipe(
     map((users) => users.map((user) => user.name)), 
     catchError(err => {
+      console.log('err', err);
       return of([]);
     })
   );
 }
 
+const mockUsers: UserInterface[] = [
+  { id: '1', name: 'John Doe', age: 30 },
+];
 
+const mockUsers$ = of(mockUsers);
+
+normalizeUsers(mockUsers$).subscribe((item) => {
+  const appDiv = document.getElementById('app');
+  if (appDiv) {
+    appDiv.innerHTML = item.toString();
+  }
+})
 
 
 
