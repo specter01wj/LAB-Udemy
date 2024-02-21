4b16575fc150610358f936bcac5c9e62a62ada32
Jin Wang <specter01wj@gmail.com>
2024-02-21 01:34:20 -0500

    v13

--- Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_3/mysol_5/src/main.ts
+++ Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_3/mysol_5/src/main.ts
@@ -1,5 +1,17 @@
+import { BehaviorSubject } from "rxjs";
+
 export interface UserInterface {
   id: string;
   name: string;
   age: number;
 }
+
+const users$ = new BehaviorSubject<UserInterface[]>([]);
+
+users$.subscribe((item) => {
+  const appDiv = document.getElementById('app');
+  if (appDiv) {
+    appDiv.innerHTML = item.toString();
+  }
+})
+
