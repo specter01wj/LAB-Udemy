581f6c6d4c2f1e60cedbd13ce3ca51fa16ccf6f1
Jin Wang <specter01wj@gmail.com>
2024-02-21 01:54:58 -0500

    v19

--- Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_3/mysol_6/src/main.ts
+++ Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_3/mysol_6/src/main.ts
@@ -1,2 +1,21 @@
+import { Observable, filter, of } from "rxjs";
+
+const foo$ = of('1');
+
+foo$.subscribe((item) => {
+  const appDiv = document.getElementById('app1');
+  if (appDiv) {
+    appDiv.innerHTML = JSON.stringify(item);
+  }
+});
+
+foo$.toPromise().then((item) => {
+  const appDiv = document.getElementById('app2');
+  if (appDiv) {
+    appDiv.innerHTML = JSON.stringify(item);
+  } 
+});
+
+
 
 
