3707304e57028f41128706844207a733b118f5b3
Jin Wang <specter01wj@gmail.com>
2024-02-21 02:27:21 -0500

    v30

--- Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_3/mysol_8/src/main.ts
+++ Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_3/mysol_8/src/main.ts
@@ -1,4 +1,9 @@
-import { concat, concatMap, delay, flatMap, from, mergeMap, of, switchMap } from "rxjs";
+import { Observable, concat, concatMap, delay, exhaustMap, flatMap, from, mergeMap, of, switchMap } from "rxjs";
+
+export interface UserInterface {
+  id: string;
+  name: string;
+}
 
 const example = (operator: any) => () => {
   from([0, 1, 2, 3, 4])
@@ -14,3 +19,25 @@ const example = (operator: any) => () => {
 // example(flatMap)();
 // example(concatMap)();
 example(switchMap)();
+// example(exhaustMap)();
+
+/* users: UserInterface[] = [
+  { id: '1', name: 'John Doe' },
+  { id: '2', name: 'James' },
+];
+
+const foo$ = this.getUser('1').pipe(
+  switchMap((user) => this.getUserDetails(user))
+);
+
+foo$.subscribe((foo) => console.log('foo', foo));
+
+getUser(id: string): Observable<UserInterface> {
+  const user = this.users.find((user) => user.id === id)!;
+  return of(user);
+}
+
+getUserDetails(user: UserInterface): Observable<UserDetailsInterface> {
+  return of({ id: user.id, age: 30 },)
+}
+ */
