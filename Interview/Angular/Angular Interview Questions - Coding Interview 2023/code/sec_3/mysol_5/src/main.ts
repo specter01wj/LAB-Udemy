bafcd4c0d596eb6a7779d62fed5b9768207d3f15
Jin Wang <specter01wj@gmail.com>
2024-02-21 01:41:11 -0500

    v15

--- Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_3/mysol_5/src/main.ts
+++ Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_3/mysol_5/src/main.ts
@@ -1,4 +1,4 @@
-import { BehaviorSubject } from "rxjs";
+import { BehaviorSubject, Subject } from "rxjs";
 
 export interface UserInterface {
   id: string;
@@ -8,14 +8,24 @@ export interface UserInterface {
 
 const users$ = new BehaviorSubject<UserInterface[]>([]);
 
+const subject$ = new Subject();
+
 setTimeout(() => {
-  users$.next([{ id: '1', name: 'John Doe', age: 30 },])
+  users$.next([{ id: '1', name: 'John Doe', age: 30 },]);
+  subject$.next([{ id: '2', name: 'Jin', age: 34 },]);
 }, 2000);
 
 users$.subscribe((item) => {
-  const appDiv = document.getElementById('app');
+  const appDiv = document.getElementById('app1');
   if (appDiv) {
     appDiv.innerHTML = JSON.stringify(item);
   }
-})
+});
+
+subject$.subscribe((item) => {
+  const appDiv = document.getElementById('app2');
+  if (appDiv) {
+    appDiv.innerHTML = JSON.stringify(item);
+  }
+});
 
