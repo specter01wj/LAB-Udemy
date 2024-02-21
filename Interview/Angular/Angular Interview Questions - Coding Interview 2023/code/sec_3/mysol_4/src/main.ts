a7ef16bbe849064689be8f1a6b2210b9e3423df2
Jin Wang <specter01wj@gmail.com>
2024-02-21 01:21:07 -0500

    v10

--- Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_3/mysol_4/src/main.ts
+++ Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_3/mysol_4/src/main.ts
@@ -1,2 +1,19 @@
+import { Observable, combineLatest, map, of } from "rxjs";
+
+const foo$ = of('foo');
+const bar$ = of('bar');
+const baz$ = of('baz');
+
+combineLatest([foo$, bar$, baz$]).subscribe((res) => {
+  const appDiv = document.getElementById('app');
+  if (appDiv) {
+    appDiv.innerHTML = res.toString();
+  }
+});
+
+
+
+
+
 
 
