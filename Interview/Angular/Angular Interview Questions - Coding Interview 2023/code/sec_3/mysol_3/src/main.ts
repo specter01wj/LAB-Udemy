cfe450bbb9ec502f2603b1534279d418755f891f
Jin Wang <specter01wj@gmail.com>
2024-02-21 00:37:58 -0500

    v3

--- Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_3/mysol_3/src/main.ts
+++ Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_3/mysol_3/src/main.ts
@@ -1,5 +1,21 @@
+import { Observable, catchError, map, of } from "rxjs";
+
 export interface UserInterface {
   id: string;
   name: string;
   age: number;
 }
+
+const normalizeUsers = (users$: Observable<UserInterface[]>): Observable<string[]> => {
+  return users$.pipe(
+    map((users) => users.map((user) => user.name)), 
+    catchError(err => {
+      return of([]);
+    })
+  );
+}
+
+
+
+
+
