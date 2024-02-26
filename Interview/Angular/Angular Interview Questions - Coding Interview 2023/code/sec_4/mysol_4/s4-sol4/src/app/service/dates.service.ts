445c160ededbd48dd5fb3e064b9dfa18e244ec67
Jin Wang <specter01wj@gmail.com>
2024-02-26 14:14:59 -0500

    v6

--- Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_4/mysol_4/s4-sol4/src/app/service/dates.service.ts
+++ Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_4/mysol_4/s4-sol4/src/app/service/dates.service.ts
@@ -6,4 +6,22 @@ import { Injectable } from '@angular/core';
 export class DatesService {
 
   constructor() { }
+
+  getToday(): Date {
+    const startOfDateTimestamp = new Date().setHours(0, 0, 0, 0);
+    return new Date(startOfDateTimestamp);
+  }
+
+  getTomorrow(): Date {
+    const today = this.getToday();
+    return new Date(today.setDate(today.getDate() + 1));
+  }
+
+  getYesterday(): Date {
+    const today = this.getToday();
+    return new Date(today.setDate(today.getDate() - 1));
+  }
+
+
+
 }
