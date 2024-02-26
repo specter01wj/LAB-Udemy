67e20be241f03ffa25983c4b18dcc51d6a562e64
Jin Wang <specter01wj@gmail.com>
2024-02-26 01:50:02 -0500

    v7

--- Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_4/mysol_3/s4-sol3/src/app/app.component.ts
+++ Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_4/mysol_3/s4-sol3/src/app/app.component.ts
@@ -9,9 +9,14 @@ export class AppComponent {
   title = 's4-sol3';
   initVal: string = 'Inital Value';
   initVal_2: string = 'Inital Value 2';
+  initVal_3: string = 'Inital Value 3';
 
   changeVal(event: Event): void {
     this.initVal = (event.target as HTMLInputElement).value;
+  }
+
+  modelChange(value: string): void {
+    this.initVal_3 = value;
   }
 
 }
