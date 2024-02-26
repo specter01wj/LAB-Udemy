42e9baa6a6933e70717c909bacc82f147d32e4bd
Jin Wang <specter01wj@gmail.com>
2024-02-26 14:17:17 -0500

    v7

--- Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_4/mysol_4/s4-sol4/src/app/app.component.ts
+++ Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_4/mysol_4/s4-sol4/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DatesService } from './service/dates.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 's4-sol4';
+
+  constructor(private datesService: DatesService) { }
+
+  ngOnInit(): void {
+      console.log(this.datesService.getToday(),
+      this.datesService.getTomorrow(),
+      this.datesService.getYesterday());
+  }
+
 }
