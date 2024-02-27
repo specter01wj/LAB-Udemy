8b73c17e2d9358d7382af4b0fc516c4cc6ff6b24
Jin Wang <specter01wj@gmail.com>
2024-02-27 00:46:43 -0500

    v6

--- Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_4/mysol_7/s4-sol7/src/app/app-routing.module.ts
+++ Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_4/mysol_7/s4-sol7/src/app/app-routing.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { UsersComponent } from './users/users/users.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: UsersComponent
-  }
 ];
 
 @NgModule({
--- Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_4/mysol_7/s4-sol7/src/app/users/users.module.ts
+++ Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_4/mysol_7/s4-sol7/src/app/users/users.module.ts
@@ -2,14 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsersComponent } from './users/users.component';
 
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: UsersComponent
+  }
+];
 
 @NgModule({
   declarations: [
     UsersComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ],
   exports: [
     UsersComponent
