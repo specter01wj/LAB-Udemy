a0fa472860cc1bf7525ece3983fe3c255133ca76
Jin Wang <specter01wj@gmail.com>
2024-02-27 11:09:39 -0500

    v6

--- Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_4/mysol_9/s4-sol9/src/app/app.component.ts
+++ Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_4/mysol_9/s4-sol9/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { ChildComponent } from './child/child.component';
 
 @Component({
@@ -6,13 +6,26 @@ import { ChildComponent } from './child/child.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 's4-sol9';
 
   @ViewChild(ChildComponent, {static: true}) child?: ChildComponent;
+  @ViewChild('button', {static: true}) buttonRef?: ElementRef<HTMLButtonElement>;
+
+  @ViewChildren(ChildComponent) children?: QueryList<ChildComponent>;
+  @ViewChildren('button') buttonsRef?: QueryList<ElementRef<HTMLButtonElement>>;
 
   increment(): void {
     this.child?.increment();
+  }
+
+  ngAfterViewInit(): void {
+    if (this.buttonRef?.nativeElement) {
+      this.buttonRef.nativeElement.innerHTML = 'James';
+    }
+
+    this.children?.forEach((child) => console.log(child));
+    this.buttonsRef?.forEach((button) => console.log(button));
   }
 
 }
