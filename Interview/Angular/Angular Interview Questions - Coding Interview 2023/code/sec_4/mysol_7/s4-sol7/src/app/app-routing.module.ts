9371691071189461202c89c51a82db316f9bd5b5
Jin Wang <specter01wj@gmail.com>
2024-02-27 00:33:41 -0500

    v3

--- Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_4/mysol_7/s4-sol7/src/app/app-routing.module.ts
+++ Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_4/mysol_7/s4-sol7/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsersComponent } from './users/users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: UsersComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
