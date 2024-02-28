06953800fb9f902f972bdab105de814806562bbd
Jin Wang <specter01wj@gmail.com>
2024-02-28 01:24:48 -0500

    v1

--- Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_4/mysol_10/s4-sol10/src/app/app.component.ts
+++ Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_4/mysol_10/s4-sol10/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 's4-sol10';
+
+  constructor() {
+    console.log('constructor');
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit');
+  }
+
 }
