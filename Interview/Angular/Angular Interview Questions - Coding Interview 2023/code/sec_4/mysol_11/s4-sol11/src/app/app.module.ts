e576b33074195e3813d5b456bdf355c859aabaf5
Jin Wang <specter01wj@gmail.com>
2024-02-28 02:03:31 -0500

    v8

--- Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_4/mysol_11/s4-sol11/src/app/app.module.ts
+++ Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_4/mysol_11/s4-sol11/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { UsersModule } from './users/users.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
--- Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_4/mysol_11/s4-sol11/src/app/users/users/users.component.ts
+++ Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_4/mysol_11/s4-sol11/src/app/users/users/users.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss'
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit, OnDestroy {
+  data$ = interval(1000);
+  dataSubscription?: Subscription;
+
+  ngOnInit(): void {
+      this.dataSubscription = this.data$.subscribe((data) => {
+        console.log('data:', data);
+      });
+  }
+
+
+  ngOnDestroy(): void {
+    this.dataSubscription?.unsubscribe();
+  }
 
 }
