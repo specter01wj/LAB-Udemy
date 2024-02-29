3ae6868c6984b58ff78c09c7cf123eac482d7e02
Jin Wang <specter01wj@gmail.com>
2024-02-29 13:21:40 -0500

    v9

--- Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_4/mysol_14/s4-sol14/src/app/app.component.ts
+++ Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_4/mysol_14/s4-sol14/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { User } from './model/user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 's4-sol14';
+  user$ = new BehaviorSubject<User[]>([]);
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<User[]>('http://localhost:3004/users')
+      .subscribe({
+        next: (users) => {
+          this.user$.next(users);
+        }
+      })
+  }
+
+
 }
