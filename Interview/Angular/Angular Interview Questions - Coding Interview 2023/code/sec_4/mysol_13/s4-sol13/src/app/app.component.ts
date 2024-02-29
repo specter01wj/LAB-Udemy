0020137d0bdd0d8ec9fc02a352bee5d5b269d342
Jin Wang <specter01wj@gmail.com>
2024-02-29 11:28:36 -0500

    v3

--- Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_4/mysol_13/s4-sol13/src/app/app.component.ts
+++ Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_4/mysol_13/s4-sol13/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 's4-sol13';
+  currentPage$ = of(1);
 }
