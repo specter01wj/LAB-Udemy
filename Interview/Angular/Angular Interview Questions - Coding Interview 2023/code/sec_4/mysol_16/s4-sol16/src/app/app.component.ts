025b4ad78b640b09238635edccf04e4da9421dac
Jin Wang <specter01wj@gmail.com>
2024-03-03 20:30:17 -0500

    v5

--- Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_4/mysol_16/s4-sol16/src/app/app.component.ts
+++ Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_4/mysol_16/s4-sol16/src/app/app.component.ts
@@ -1,20 +1,38 @@
-import { animate, style, transition, trigger } from '@angular/animations';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
-const enterTransition = transition(':enter', [
+/* const enterTransition = transition(':enter', [
   style({
     opacity: 0,
   }),
   animate('1s ease-in', style({ opacity: 1 })),
 ]);
 
+const exitTransition = transition(':leave', [
+  style({
+    opacity: 1,
+  }),
+  animate('1s ease-out', style({ opacity: 0 })),
+]);
+
 const fadeIn = trigger('fadeIn', [enterTransition]);
+const fadeOut = trigger('fadeOut', [exitTransition]); */
+
+const fadeInOut = trigger('fadeInOut', [
+  state('open', style({ opacity: 1 })),
+  state('close', style({ opacity: 0 })),
+  transition('open => close', [animate('1s ease-out')]),
+  transition('close => open', [animate('1s ease-in')]),
+]);
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  animations: [fadeIn]
+  // animations: [fadeIn, fadeOut]
+  animations: [fadeInOut]
 })
 export class AppComponent {
   title = 's4-sol16';
