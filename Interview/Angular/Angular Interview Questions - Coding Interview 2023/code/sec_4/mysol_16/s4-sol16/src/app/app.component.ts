1681ce2ec12249307a23fda743ba49c3121ecd1b
Jin Wang <specter01wj@gmail.com>
2024-03-03 20:20:10 -0500

    v4

--- Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_4/mysol_16/s4-sol16/src/app/app.component.ts
+++ Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_4/mysol_16/s4-sol16/src/app/app.component.ts
@@ -1,9 +1,20 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
+
+const enterTransition = transition(':enter', [
+  style({
+    opacity: 0,
+  }),
+  animate('1s ease-in', style({ opacity: 1 })),
+]);
+
+const fadeIn = trigger('fadeIn', [enterTransition]);
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  animations: [fadeIn]
 })
 export class AppComponent {
   title = 's4-sol16';
