d0207f17cad58519b47bd72d4d7ec3c842ee4a1d
Jin Wang <specter01wj@gmail.com>
2024-03-06 02:13:41 -0500

    v10

--- Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_4/mysol_21/s4-sol21/src/app/app.component.ts
+++ Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_4/mysol_21/s4-sol21/src/app/app.component.ts
@@ -10,4 +10,9 @@ export class AppComponent {
 
   currentDate = new Date();
 
+  person = {
+    name: "James",
+    surname: "Wang"
+  };
+
 }
--- Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_4/mysol_21/s4-sol21/src/app/app.module.ts
+++ Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_4/mysol_21/s4-sol21/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FullNamePipe } from './shared/full-name.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FullNamePipe
   ],
   imports: [
     BrowserModule,
