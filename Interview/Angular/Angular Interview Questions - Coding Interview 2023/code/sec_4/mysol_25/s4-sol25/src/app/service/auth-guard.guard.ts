18217f3b3fa688313a0b973999b307df73757eca
Jin Wang <specter01wj@gmail.com>
2024-03-07 02:05:41 -0500

    v9

--- Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_4/mysol_25/s4-sol25/src/app/service/auth-guard.guard.ts
+++ Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_4/mysol_25/s4-sol25/src/app/service/auth-guard.guard.ts
@@ -1,16 +1,34 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
+import { Observable, map, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuardService implements CanActivate {
 
-  canActivate(): boolean {
-    if (localStorage.getItem('token')) {
-      return true;
-    }
-    return false;
+
+  constructor(private router: Router) {}
+
+
+  // canActivate(): boolean {
+  //   if (localStorage.getItem('token')) {
+  //     return true;
+  //   }
+  //   return false;
+  // }
+
+  canActivate(): Observable<boolean> {
+      return of(true).pipe(
+        map((isLoggedIn) => {
+          if (!isLoggedIn) {
+            this.router.navigateByUrl('/');
+            return false;
+          }
+
+          return true;
+        })
+      );
   }
 
 
