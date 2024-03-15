a0ba7225938c24f3d18702a97bb6f9680076f209
Jin Wang <specter01wj@gmail.com>
2024-03-15 00:32:28 -0400

    v6

--- Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_4/mysol_28/s4-sol28/src/app/app.component.ts
+++ Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_4/mysol_28/s4-sol28/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, NgZone, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 's4-sol28';
+
+  @ViewChild('element', {static: true}) element: any;
+  position: any;
+
+  constructor(private zone: NgZone) {}
+
+
+  mouseDown(event: any) {
+    this.element = event.target;
+    this.zone.runOutsideAngular(() => {
+      window.document.addEventListener('mousemove', this.mouseMove.bind(this));
+    });
+  }
+
+  mouseMove(event: any) {
+    event.preventDefault();
+    this.element.setAttribute('x', event.clientX);
+    this.element.setAttribute('y', event.clientY);
+  }
+
+  mouseUp(event: any) {
+    this.zone.run(() => {
+      this.position = {
+        x: this.element.getAttribute('x'),
+        y: this.element.getAttribute('y'),
+      }
+    });
+    window.document.removeEventListener('mousemove', this.mouseMove);
+  }
+
+
+
 }
