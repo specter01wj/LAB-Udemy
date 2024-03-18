830896daf93669b8f2914396de8067473b49408c
Jin Wang <specter01wj@gmail.com>
2024-03-17 21:19:13 -0400

    v4

--- Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_4/mysol_29/s4-sol29/src/app/app.component.ts
+++ Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_4/mysol_29/s4-sol29/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +9,11 @@ import { FormBuilder } from '@angular/forms';
 export class AppComponent {
   title = 's4-sol29';
 
+  registerForm = this.fb.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+  });
 
   constructor(private fb: FormBuilder) {}
 
