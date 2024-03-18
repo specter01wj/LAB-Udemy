c04dcba80f8f2ea15ded12f7deb796eb2f5fa054
Jin Wang <specter01wj@gmail.com>
2024-03-17 21:07:34 -0400

    v3

--- Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_4/mysol_29/s4-sol29/src/app/app.component.ts
+++ Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_4/mysol_29/s4-sol29/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 's4-sol29';
+
+
+  constructor(private fb: FormBuilder) {}
+
+
+
+
 }
