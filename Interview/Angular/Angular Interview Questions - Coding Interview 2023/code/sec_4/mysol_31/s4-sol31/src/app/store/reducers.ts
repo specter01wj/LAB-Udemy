4e95b6f10093dd6343e4c7f120c32da45182b59e
Jin Wang <specter01wj@gmail.com>
2024-03-18 20:07:30 -0400

    v6

--- Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_4/mysol_31/s4-sol31/src/app/store/reducers.ts
+++ Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_4/mysol_31/s4-sol31/src/app/store/reducers.ts
@@ -1,7 +1,16 @@
+import { createReducer, on } from "@ngrx/store";
 import { UsersState } from "../model/users-state";
+import * as UsersActions from './actions';
 
 export const initialState: UsersState = {
   isLoading: false,
   data: [],
   error: null
 }
+
+export const reducers = createReducer(
+  initialState,
+  on(UsersActions.getUsers, (state) => ({
+    ...state, isLoading: true
+  }))
+);
