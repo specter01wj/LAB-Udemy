164285f67bd874b48e1bf1901a581007e7d85be0
Jin Wang <specter01wj@gmail.com>
2024-03-20 00:52:58 -0400

    v3

--- Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_4/mysol_32/s4-sol32/src/app/app.component.ts
+++ Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_4/mysol_32/s4-sol32/src/app/app.component.ts
@@ -1,5 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, ViewRef, inject } from '@angular/core';
 import { UsersService } from './service/users.service';
+import { ActivatedRoute } from '@angular/router';
+import { Subject, filter, map, takeUntil } from 'rxjs';
+
+const getPageParams = () => {
+  return inject(ActivatedRoute).queryParams.pipe(
+    filter((params) => params['page']),
+    map((params) => params['page']),
+  );
+};
+
+const onDestroy = () => {
+  const destroy$ = new Subject<void>();
+  const viewRef = inject(ChangeDetectorRef) as ViewRef;
+
+  viewRef.onDestroy(() => {
+    destroy$.next();
+    destroy$.complete();
+  });
+  return destroy$;
+}
 
 @Component({
   selector: 'app-root',
@@ -8,11 +28,17 @@ import { UsersService } from './service/users.service';
 })
 export class AppComponent implements OnInit {
   title = 's4-sol5';
+  userService = inject(UsersService);
+  page$ = getPageParams();
+  destroy$ = onDestroy();
 
-  constructor(private userService: UsersService) {}
+  // constructor(private userService: UsersService) {}
 
   ngOnInit(): void {
-      this.userService.getUsers().subscribe({
+    this.userService
+      .getUsers()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
         next: (users) => {
           console.log('users', users);
         }
