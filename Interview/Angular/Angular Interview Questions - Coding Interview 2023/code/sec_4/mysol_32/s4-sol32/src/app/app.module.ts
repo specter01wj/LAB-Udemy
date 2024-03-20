321631224d8afbb05ab976c4fa09f6f89a86efbf
Jin Wang <specter01wj@gmail.com>
2024-03-20 00:42:35 -0400

    v2

--- Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_4/mysol_32/s4-sol32/src/app/app.module.ts
+++ Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_4/mysol_32/s4-sol32/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { UsersModule } from './users/users.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    UsersModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Interview/Angular/Angular Interview Questions - Coding Interview 2023/code/sec_4/mysol_32/s4-sol32/src/app/users/users.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UsersComponent } from './users/users.component';
+
+
+
+@NgModule({
+  declarations: [
+    UsersComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    UsersComponent
+  ]
+})
+export class UsersModule { }
