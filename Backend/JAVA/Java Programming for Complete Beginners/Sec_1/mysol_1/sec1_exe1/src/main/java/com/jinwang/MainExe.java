41a433f26b7186d01f2a2735acdacc0171d4155a
Jin Wang <specter01wj@gmail.com>
2026-02-11 00:25:19 -0500

    v5

---
+++ Backend/JAVA/Java Programming for Complete Beginners/Sec_1/mysol_1/sec1_exe1/src/main/java/com/jinwang/MainExe.java
@@ -0,0 +1,14 @@
+package com.jinwang;
+
+public class MainExe {
+
+    public static void main(String[] args) {
+
+        System.out.println("=== Using Variables ===");
+        UsingVariables.run();
+
+        System.out.println("\n=== Using Strings ===");
+        UsingStrings.run();
+    }
+
+}
