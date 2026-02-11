acb69ac7533e38ecd419a9904304466fb1111c57
Jin Wang <specter01wj@gmail.com>
2026-02-11 00:27:53 -0500

    v6

---
+++ Backend/JAVA/Java Programming for Complete Beginners/Sec_1/mysol_1/sec1_exe1/src/main/java/com/jinwang/UsingVariables.java
@@ -0,0 +1,27 @@
+package com.jinwang;
+
+public class UsingVariables {
+
+    public static void run() {
+
+        int aValue = 42;
+        short aShort = 93;
+        long aLong = 8304;
+        double aDouble = 3.4553;
+        float aFloat = 5.234f;
+
+        char aChar = 'b';
+        boolean aBoolean = true;
+        byte aByte = 127;
+
+        System.out.println(aValue);
+        System.out.println(aShort);
+        System.out.println(aLong);
+        System.out.println(aDouble);
+        System.out.println(aFloat);
+        System.out.println(aChar);
+        System.out.println(aBoolean);
+        System.out.println(aByte);
+    }
+
+}
