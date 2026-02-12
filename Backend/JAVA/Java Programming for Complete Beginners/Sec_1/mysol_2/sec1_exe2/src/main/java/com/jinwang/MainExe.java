50297ff731f7bad03524404073b4c724a748816e
Jin Wang <specter01wj@gmail.com>
2026-02-12 02:24:00 -0500

    v7

--- Backend/JAVA/Java Programming for Complete Beginners/Sec_1/mysol_2/sec1_exe2/src/main/java/com/jinwang/MainExe.java
+++ Backend/JAVA/Java Programming for Complete Beginners/Sec_1/mysol_2/sec1_exe2/src/main/java/com/jinwang/MainExe.java
@@ -1,4 +1,17 @@
 package com.jinwang;
 
 public class MainExe {
+
+    public static void main(String[] args) {
+
+        System.out.println("=== While Loops ===");
+        WhileLoops.run();
+
+        System.out.println("\n=== For Loops ===");
+        ForLoops.run();
+
+        System.out.println("\n=== If Statements ===");
+        IfStatements.run();
+    }
+
 }
