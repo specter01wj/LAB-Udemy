966d2d61dc731bfb4824e3387d51c54c5951ceb8
Jin Wang <specter01wj@gmail.com>
2026-02-12 02:24:33 -0500

    v8

--- Backend/JAVA/Java Programming for Complete Beginners/Sec_1/mysol_2/sec1_exe2/src/main/java/com/jinwang/ForLoops.java
+++ Backend/JAVA/Java Programming for Complete Beginners/Sec_1/mysol_2/sec1_exe2/src/main/java/com/jinwang/ForLoops.java
@@ -1,4 +1,12 @@
 package com.jinwang;
 
 public class ForLoops {
+
+    public static void run() {
+
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("The value of my variable is: %d%n", i);
+        }
+    }
+
 }
