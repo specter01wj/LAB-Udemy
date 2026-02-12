5eaef0f0609e202f76c47c8c17e98ac9d16d779b
Jin Wang <specter01wj@gmail.com>
2026-02-12 02:24:06 -0500

    v8

---
+++ Backend/JAVA/Java Programming for Complete Beginners/Sec_1/mysol_2/sec1_exe2/src/main/java/com/jinwang/WhileLoops.java
@@ -0,0 +1,15 @@
+package com.jinwang;
+
+public class WhileLoops {
+
+    public static void run() {
+
+        int number = 23;
+
+        while (number < 34) {
+            System.out.println("Okay " + number);
+            number += 2;
+        }
+    }
+
+}
