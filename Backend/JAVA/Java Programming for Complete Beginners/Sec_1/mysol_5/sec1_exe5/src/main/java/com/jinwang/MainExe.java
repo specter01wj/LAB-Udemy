9e3c7945665cd35af70810ea8dc6255440aa8acb
Jin Wang <specter01wj@gmail.com>
2026-02-22 18:35:11 -0500

    v1

---
+++ Backend/JAVA/Java Programming for Complete Beginners/Sec_1/mysol_5/sec1_exe5/src/main/java/com/jinwang/MainExe.java
@@ -0,0 +1,23 @@
+package com.jinwang;
+
+public class MainExe {
+
+    public static void main(String[] args) {
+
+        System.out.println("=== Constructor Demo ===");
+        ConstructorDemo.run();
+
+        System.out.println("\n=== Static Demo ===");
+        StaticDemo.run();
+
+        System.out.println("\n=== Inheritance Demo ===");
+        InheritanceDemo.run();
+
+        System.out.println("\n=== Polymorphism Demo ===");
+        PolymorphismDemo.run();
+
+        System.out.println("\n=== Encapsulation Demo ===");
+        EncapsulationDemo.run();
+    }
+
+}
