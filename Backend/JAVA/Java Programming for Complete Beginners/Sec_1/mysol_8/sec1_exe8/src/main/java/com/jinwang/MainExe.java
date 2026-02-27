46635529653a63b3b5a7b3c0842d7197ea52d669
Jin Wang <specter01wj@gmail.com>
2026-02-26 23:53:03 -0500

    v6

--- Backend/JAVA/Java Programming for Complete Beginners/Sec_1/mysol_8/sec1_exe8/src/main/java/com/jinwang/MainExe.java
+++ Backend/JAVA/Java Programming for Complete Beginners/Sec_1/mysol_8/sec1_exe8/src/main/java/com/jinwang/MainExe.java
@@ -5,11 +5,6 @@ public class MainExe {
     public static void main(String[] args) {
 
         System.out.println("=== Equality Demo ===");
-
-
-
-
-        
         EqualityDemo.run();
 
         System.out.println("\n=== Ternary Demo ===");
