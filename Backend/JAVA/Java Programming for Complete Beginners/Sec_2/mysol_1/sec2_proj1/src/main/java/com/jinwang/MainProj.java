3490723ca3a184b6eda84ee3eeb74d49c7116002
Jin Wang <specter01wj@gmail.com>
2026-02-27 00:00:11 -0500

    v9

--- Backend/JAVA/Java Programming for Complete Beginners/Sec_2/mysol_1/sec2_proj1/src/main/java/com/jinwang/MainProj.java
+++ Backend/JAVA/Java Programming for Complete Beginners/Sec_2/mysol_1/sec2_proj1/src/main/java/com/jinwang/MainProj.java
@@ -1,4 +1,12 @@
 package com.jinwang;
 
 public class MainProj {
+
+    public static void main(String[] args) {
+
+        GuessTheWord game = new GuessTheWord();
+        game.start();
+        game.end();
+    }
+
 }
