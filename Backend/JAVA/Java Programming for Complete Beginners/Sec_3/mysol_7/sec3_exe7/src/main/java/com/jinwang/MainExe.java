6215346b8f673520930c83c7a0d540faa52878f1
Jin Wang <specter01wj@gmail.com>
2026-03-11 02:59:29 -0400

    v4

--- Backend/JAVA/Java Programming for Complete Beginners/Sec_3/mysol_7/sec3_exe7/src/main/java/com/jinwang/MainExe.java
+++ Backend/JAVA/Java Programming for Complete Beginners/Sec_3/mysol_7/sec3_exe7/src/main/java/com/jinwang/MainExe.java
@@ -1,4 +1,19 @@
 package com.jinwang;
 
 public class MainExe {
+
+    public static void main(String[] args) {
+
+        System.out.println("=== Serialization Demo ===");
+
+        WritingDemo.run();
+        ReadingDemo.run();
+
+        System.out.println();
+        System.out.println("=== Serialization Multiple Objects Demo ===");
+
+        WritingMultiDemo.run();
+        ReadingMultiDemo.run();
+    }
+
 }
