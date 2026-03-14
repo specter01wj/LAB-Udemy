5c3b4ee35a577ab966b0d4510f65e7a9633ed446
Jin Wang <specter01wj@gmail.com>
2026-03-14 12:41:29 -0400

    v6

--- Backend/JAVA/Java Programming for Complete Beginners/Sec_3/mysol_7/sec3_exe7/src/main/java/com/jinwang/serializationmulti/WritingMultiDemo.java
+++ Backend/JAVA/Java Programming for Complete Beginners/Sec_3/mysol_7/sec3_exe7/src/main/java/com/jinwang/serializationmulti/WritingMultiDemo.java
@@ -14,11 +14,6 @@ public class WritingMultiDemo {
 
         Vehicle[] vehicles = {
                 new Vehicle(805, "Toyota"),
-
-
-
-
-                
                 new Vehicle(505, "Ford"),
                 new Vehicle(203, "Chevrolet")
         };
