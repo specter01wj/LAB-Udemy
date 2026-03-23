fbe56d9c363df4272635cdb2b2001b6dc4d5935c
Jin Wang <specter01wj@gmail.com>
2026-03-23 00:37:33 -0400

    v3

--- Backend/JAVA/Java Programming for Complete Beginners/Sec_4/mysol_1/sec4_exe1/src/main/java/com/jinwang/MainExe.java
+++ Backend/JAVA/Java Programming for Complete Beginners/Sec_4/mysol_1/sec4_exe1/src/main/java/com/jinwang/MainExe.java
@@ -1,4 +1,12 @@
 package com.jinwang;
 
 public class MainExe {
+
+    public static void main(String[] args) {
+
+        ThreadExtendDemo.run();
+        RunnableDemo.run();
+        AnonymousThreadDemo.run();
+    }
+
 }
