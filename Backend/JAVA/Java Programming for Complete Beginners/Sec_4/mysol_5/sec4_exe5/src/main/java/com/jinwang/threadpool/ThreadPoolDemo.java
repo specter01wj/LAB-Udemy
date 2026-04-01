508230af42e4d0cd4b3d6c7755a919addaf1095e
Jin Wang <specter01wj@gmail.com>
2026-03-31 23:59:41 -0400

    v7

--- Backend/JAVA/Java Programming for Complete Beginners/Sec_4/mysol_5/sec4_exe5/src/main/java/com/jinwang/threadpool/ThreadPoolDemo.java
+++ Backend/JAVA/Java Programming for Complete Beginners/Sec_4/mysol_5/sec4_exe5/src/main/java/com/jinwang/threadpool/ThreadPoolDemo.java
@@ -21,11 +21,6 @@ class Task implements Runnable {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
-
-
-
-
-            
         }
 
         System.out.println("Task " + id + " finished.");
