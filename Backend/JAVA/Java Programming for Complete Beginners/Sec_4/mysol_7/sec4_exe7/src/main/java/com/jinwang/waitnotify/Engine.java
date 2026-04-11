d4ddc91d50291211408311f2e1da5c5889de80ab
Jin Wang <specter01wj@gmail.com>
2026-04-11 12:59:57 -0400

    v7

---
+++ Backend/JAVA/Java Programming for Complete Beginners/Sec_4/mysol_7/sec4_exe7/src/main/java/com/jinwang/waitnotify/Engine.java
@@ -0,0 +1,31 @@
+package com.jinwang.waitnotify;
+
+import java.util.Scanner;
+
+public class Engine {
+
+    public void releaseEnergy() throws InterruptedException {
+
+        synchronized (this) {
+            System.out.println("Engine started. Waiting...");
+            wait(); // release lock + wait
+            System.out.println("Resumed.");
+        }
+    }
+
+    public void consumeFuel() throws InterruptedException {
+
+        try (Scanner scanner = new Scanner(System.in)) {
+
+            Thread.sleep(2000);
+
+            synchronized (this) {
+                System.out.println("Press ENTER to continue...");
+                scanner.nextLine();
+                System.out.println("ENTER pressed.");
+
+                notify(); // wake up waiting thread
+            }
+        }
+    }
+}
