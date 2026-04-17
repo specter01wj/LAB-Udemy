b0abb5ca811b73f2c05ee9096e712b3fed09db71
Jin Wang <specter01wj@gmail.com>
2026-04-17 19:44:18 -0400

    v1

--- Backend/JAVA/Java Programming for Complete Beginners/Sec_4/mysol_7/sec4_exe7/src/main/java/com/jinwang/waitnotify/Engine.java
+++ Backend/JAVA/Java Programming for Complete Beginners/Sec_4/mysol_7/sec4_exe7/src/main/java/com/jinwang/waitnotify/Engine.java
@@ -22,6 +22,7 @@ public class Engine {
             synchronized (this) {
                 System.out.println("Press ENTER to continue...");
                 scanner.nextLine();
+                
                 System.out.println("ENTER pressed.");
 
                 notify(); // wake up waiting thread
