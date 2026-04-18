a7c4e3cc1db9a1fcd69300bd62037b4f3fb18782
Jin Wang <specter01wj@gmail.com>
2026-04-18 01:14:14 -0400

    v6

--- Backend/JAVA/Java Programming for Complete Beginners/Sec_4/mysol_7/sec4_exe7/src/main/java/com/jinwang/waitnotify/Engine.java
+++ Backend/JAVA/Java Programming for Complete Beginners/Sec_4/mysol_7/sec4_exe7/src/main/java/com/jinwang/waitnotify/Engine.java
@@ -10,11 +10,6 @@ public class Engine {
             System.out.println("Engine started. Waiting...");
             wait(); // release lock + wait
             System.out.println("Resumed.");
-
-
-
-
-            
         }
     }
 
