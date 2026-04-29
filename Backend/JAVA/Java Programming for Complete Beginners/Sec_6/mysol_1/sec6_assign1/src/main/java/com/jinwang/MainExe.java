d27083722ea0cdc165597096462537f5806ab1d1
Jin Wang <specter01wj@gmail.com>
2026-04-29 15:32:26 -0400

    v9

--- Backend/JAVA/Java Programming for Complete Beginners/Sec_6/mysol_1/sec6_assign1/src/main/java/com/jinwang/MainExe.java
+++ Backend/JAVA/Java Programming for Complete Beginners/Sec_6/mysol_1/sec6_assign1/src/main/java/com/jinwang/MainExe.java
@@ -8,29 +8,21 @@ public class MainExe {
     public static void main(String[] args) {
 
         SavingsAccount savings = new SavingsAccount("S001", "John", 1000, 0.05);
-
         CheckingAccount checking = new CheckingAccount("C001", "Alice", 500, 200);
 
         // Savings account
-
         savings.deposit(200);
-
         savings.displayAccountInfo();
 
         savings.withdraw(300);
-
         savings.displayAccountInfo();
 
         // Checking account
-
         checking.deposit(100);
-
         checking.displayAccountInfo();
 
         checking.withdraw(700);
-
         checking.displayAccountInfo();
-
     }
 
 }
