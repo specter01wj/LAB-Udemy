b1804b806caf8fb187284fd572bade26f68054f9
Jin Wang <specter01wj@gmail.com>
2026-04-30 00:31:12 -0400

    v7

--- Backend/JAVA/Java Programming for Complete Beginners/Sec_6/mysol_3/sec6_exe2/src/main/java/com/jinwang/MainExe.java
+++ Backend/JAVA/Java Programming for Complete Beginners/Sec_6/mysol_3/sec6_exe2/src/main/java/com/jinwang/MainExe.java
@@ -5,19 +5,13 @@ public class MainExe {
     public static void main(String[] args) {
 
         Animal[] animals = {
-
                 new Dog("Dog"),
-
                 new Cat("Cat"),
-
                 new Bird("Bird")
-
         };
 
         for (Animal animal : animals) {
-
             animal.makeSound();
-
         }
 
     }
