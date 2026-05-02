fa10c17547ad8e07aa11215043293bce8dd1f053
Jin Wang <specter01wj@gmail.com>
2026-05-02 12:53:02 -0400

    v6

--- Backend/JAVA/Java Programming for Complete Beginners/Sec_6/mysol_6/sec6_exe5/src/main/java/com/jinwang/MainExe.java
+++ Backend/JAVA/Java Programming for Complete Beginners/Sec_6/mysol_6/sec6_exe5/src/main/java/com/jinwang/MainExe.java
@@ -1,4 +1,16 @@
 package com.jinwang;
 
 public class MainExe {
+
+    public static void main(String[] args) {
+
+        RatingSystem system = new RatingSystem();
+
+        system.addRating(new MovieRating("Inception", 5));
+        system.addRating(new MovieRating("Avatar", 4));
+        system.addRating(new MovieRating("Inception", 5)); // duplicate
+
+        system.displayRatings();
+    }
+
 }
