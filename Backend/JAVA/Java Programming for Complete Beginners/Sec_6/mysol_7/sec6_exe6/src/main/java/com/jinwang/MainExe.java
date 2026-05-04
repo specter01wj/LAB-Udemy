77c8711322501001ae17ad54e5b82914fee681d9
Jin Wang <specter01wj@gmail.com>
2026-05-03 23:38:33 -0400

    v10

--- Backend/JAVA/Java Programming for Complete Beginners/Sec_6/mysol_7/sec6_exe6/src/main/java/com/jinwang/MainExe.java
+++ Backend/JAVA/Java Programming for Complete Beginners/Sec_6/mysol_7/sec6_exe6/src/main/java/com/jinwang/MainExe.java
@@ -3,7 +3,7 @@ package com.jinwang;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main {
+public class MainExe {
 
     public static void main(String[] args) throws InterruptedException {
 
