85a15b254eaec8237f1d07cab6e30e2e46b0d853
Jin Wang <specter01wj@gmail.com>
2026-05-03 23:36:29 -0400

    v6

--- Backend/JAVA/Java Programming for Complete Beginners/Sec_6/mysol_7/sec6_exe6/src/main/java/com/jinwang/MainExe.java
+++ Backend/JAVA/Java Programming for Complete Beginners/Sec_6/mysol_7/sec6_exe6/src/main/java/com/jinwang/MainExe.java
@@ -1,4 +1,34 @@
 package com.jinwang;
 
-public class MainExe {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        List<BankAccount> accounts = new ArrayList<>();
+        accounts.add(new BankAccount(1, 1000));
+        accounts.add(new BankAccount(2, 1000));
+
+        Thread t1 = new Thread(new DepositTask(accounts));
+        Thread t2 = new Thread(new WithdrawTask(accounts));
+        Thread t3 = new Thread(new DepositTask(accounts));
+        Thread t4 = new Thread(new WithdrawTask(accounts));
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+
+        t1.join();
+        t2.join();
+        t3.join();
+        t4.join();
+
+        for (BankAccount acc : accounts) {
+            System.out.println("Account " + acc.getAccountNumber()
+                    + " balance: " + acc.getBalance());
+        }
+    }
 }
