4ca82add4f942d77b5150a783939e417bc2587b5
Jin Wang <specter01wj@gmail.com>
2026-05-15 18:40:30 -0400

    v1

--- Backend/JAVA/Java Programming for Complete Beginners/Sec_6/mysol_7/sec6_exe6/src/main/java/com/jinwang/WithdrawTask.java
+++ Backend/JAVA/Java Programming for Complete Beginners/Sec_6/mysol_7/sec6_exe6/src/main/java/com/jinwang/WithdrawTask.java
@@ -15,6 +15,7 @@ public class WithdrawTask implements Runnable {
     @Override
     public void run() {
         for (int i = 0; i < 10; i++) {
+            
             BankAccount acc = accounts.get(random.nextInt(accounts.size()));
             double amount = 50 + random.nextInt(151);
             acc.withdraw(amount);
